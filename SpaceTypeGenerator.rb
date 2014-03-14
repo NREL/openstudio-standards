9469d411168408e2bef940f9a1f73f48c4e73ae6
Dan Macumber <dan.macumber@gmail.com>
2014-03-14 14:43:48 -0600

    Reverted back to old schedule files, names in the spreadsheet need to be updated

--- SpaceTypeGenerator.rb
+++ SpaceTypeGenerator.rb
@@ -27,7 +27,15 @@ def initialize(path_to_standards_json, path_to_master_schedules_library)
 
   #load up the osm with all the reference building schedules
   path_to_master_schedules_library = OpenStudio::Path.new(path_to_master_schedules_library)
-  @schedule_library = OpenStudio::Model::Model::load(path_to_master_schedules_library).get  
+  
+  # TODO: remove this
+  path_to_office_schedules_library = OpenStudio::Path.new(path_to_master_schedules_library.to_s.gsub('Master_Schedules.osm', 'Jan2014_OfficeSchedules.osm'))
+  
+  vt = OpenStudio::OSVersion::VersionTranslator.new
+  @schedule_library = vt.loadModel(path_to_master_schedules_library).get  
+  
+  vt = OpenStudio::OSVersion::VersionTranslator.new
+  @alt_schedule_library = vt.loadModel(path_to_office_schedules_library).get 
 end
 
 def generate_space_type(template, clim, building_type, spc_type, model = nil)
@@ -49,9 +57,16 @@ def generate_space_type(template, clim, building_type, spc_type, model = nil)
     
     #get the correct space type from the library file
     sch = @schedule_library.getObjectByTypeAndName("OS_Schedule_Ruleset".to_IddObjectType, sch_name)
+    
     if sch.empty?
       puts "schedule called '#{sch_name}' not found in master schedule library"
-      exit
+      
+      # TODO: remove check in alternate file
+      sch = @alt_schedule_library.getObjectByTypeAndName("OS_Schedule_Ruleset".to_IddObjectType, sch_name)
+      if sch.empty?
+        puts "schedule called '#{sch_name}' not found in alternate schedule library"
+        exit
+      end
     end
     #clone the space type from the library model into the space type model
     clone_of_sch = sch.get.to_Schedule.get.clone(model)
