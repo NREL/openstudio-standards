9d4e1de5a72815334725f6f4a85f438643ff92f5
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-06 10:22:27 -0700

    Attempt 6

--- openstudio-standards/test/bin/docker-run.sh
+++ openstudio-standards/test/bin/docker-run.sh
@@ -6,9 +6,14 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # install dependencies and run default rake task
-cd ~/openstudio-standards/openstudio-standards
+pwd
+echo "Home from docker-run.sh:" $HOME
+cd /openstudio-standards/openstudio-standards
 bundle install 
 bundle exec rake
+cd ..
+pwd
+ls
 cp ~/openstudio-standards/test/reports/* $CIRCLE_TEST_REPORTS/minitest
 
 
