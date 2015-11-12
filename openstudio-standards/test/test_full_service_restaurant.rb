1090f48c44d0eefda5f8ad91502602fbf6d60ff9
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-11 21:58:26 -0700

    Updates tests to use dynamically created tests

--- openstudio-standards/test/test_full_service_restaurant.rb
+++ openstudio-standards/test/test_full_service_restaurant.rb
@@ -1,31 +1,14 @@
 require_relative 'minitest_helper'
 
 class TestFullServiceRestaurant < CreateDOEPrototypeBuildingTest
-  def test_case
-    # RetailStandalone, LargeHotel, RetailStripmall
-    bldg_types = ['FullServiceRestaurant']
-    vintages = ['90.1-2010']
-    # vintages = ['DOE Ref 1980-2004', 'DOE Ref Pre-1980','90.1-2004'] 
-    climate_zones =['ASHRAE 169-2006-2A']
-    # vintages = ['DOE Ref 1980-2004', 'DOE Ref Pre-1980', '90.1-2007', '90.1-2010', '90.1-2013', '90.1-2004'] 
-    # climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
-                     # 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
-                     # 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
-                     # 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A', 'ASHRAE 169-2006-8A'] 
-    
-    all_failures = []
-
-    # Create the models
-    all_failures += create_models(bldg_types, vintages, climate_zones)
-
-    # Run the models
-    # all_failures += run_models(bldg_types, vintages, climate_zones)
-
-    # Compare the results to the legacy idf results
-    # all_failures += compare_results(bldg_types, vintages, climate_zones)
-
-    # Assert if there are any errors
-    puts "There were #{all_failures.size} failures"
-    assert(all_failures.size == 0, "FAILURES: #{all_failures.join("\n")}")
-  end
+  
+  building_types = ['FullServiceRestaurant']
+  templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
+  climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+  create_models = true
+  run_models = false
+  compare_results = false
+  
+  TestFullServiceRestaurant.create_run_model_tests(building_types, templates, climate_zones, create_models, run_models, compare_results)
+     
 end
