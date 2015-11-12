51b6554607293c3b2b24c92ad8cde71733534b6b
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-11 21:41:29 -0700

    Adds tests for other building types that should be working

---
+++ openstudio-standards/test/test_small_office.rb
@@ -0,0 +1,14 @@
+require_relative 'minitest_helper'
+
+class TestSmallOffice < CreateDOEPrototypeBuildingTest
+  
+  building_types = ['SmallOffice']
+  templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
+  climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+  create_models = true
+  run_models = false
+  compare_results = false
+  
+  TestSmallOffice.create_run_model_tests(building_types, templates, climate_zones, create_models, run_models, compare_results)
+  
+end
