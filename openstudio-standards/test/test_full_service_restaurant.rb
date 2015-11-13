c5658e8fadf87da09c7edf16a765314a65b898fb
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-13 12:05:47 -0700

    Updates all prototype tests to run and compare

--- openstudio-standards/test/test_full_service_restaurant.rb
+++ openstudio-standards/test/test_full_service_restaurant.rb
@@ -1,14 +1,18 @@
 require_relative 'minitest_helper'
+require_relative 'create_doe_prototype_helper'
 
 class TestFullServiceRestaurant < CreateDOEPrototypeBuildingTest
   
   building_types = ['FullServiceRestaurant']
   templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
   climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
-  create_models = true
-  run_models = false
-  compare_results = false
   
-  TestFullServiceRestaurant.create_run_model_tests(building_types, templates, climate_zones, create_models, run_models, compare_results)
+  create_models = true
+  run_models = true
+  compare_results = true
+  
+  debug = false
+  
+  TestFullServiceRestaurant.create_run_model_tests(building_types, templates, climate_zones, create_models, run_models, compare_results, debug)
      
 end
