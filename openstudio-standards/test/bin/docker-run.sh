66b44c449f8ad6565c2bf61f70de5717a50a1b16
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-13 16:18:35 -0700

    Increase the timeout to 6 hrs for the full test suite.

--- openstudio-standards/test/bin/docker-run.sh
+++ openstudio-standards/test/bin/docker-run.sh
@@ -6,10 +6,6 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # install dependencies and run default rake task
-pwd
-ls /
-ls /root
-echo "Home from docker-run.sh:" $HOME
 cd /openstudio-standards/openstudio-standards
 bundle install 
 bundle exec rake
