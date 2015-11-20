cc600cf3e51d67cf4be41e9f074a555a3ff6202a
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-20 14:15:23 -0700

    Renable full tests for all building types.

--- openstudio-standards/test/test_medium_office.rb
+++ openstudio-standards/test/test_medium_office.rb
@@ -3,16 +3,16 @@ require_relative 'create_doe_prototype_helper'
 
 class TestMediumOffice < CreateDOEPrototypeBuildingTest
   
-  # building_types = ['MediumOffice']
-  # templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
-  # climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+  building_types = ['MediumOffice']
+  templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
+  climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
   
-  # create_models = true
-  # run_models = true
-  # compare_results = true
+  create_models = true
+  run_models = true
+  compare_results = true
   
-  # debug = false
+  debug = false
   
-  # TestMediumOffice.create_run_model_tests(building_types, templates, climate_zones, create_models, run_models, compare_results, debug)
+  TestMediumOffice.create_run_model_tests(building_types, templates, climate_zones, create_models, run_models, compare_results, debug)
   
 end
--- openstudio-standards/test/test_quick_service_restaurant.rb
+++ openstudio-standards/test/test_quick_service_restaurant.rb
@@ -4,8 +4,8 @@ require_relative 'create_doe_prototype_helper'
 class TestQuickServiceRestaurant < CreateDOEPrototypeBuildingTest
 
   building_types = ['QuickServiceRestaurant']
-  templates = ['DOE Ref Pre-1980',]#'DOE Ref 1980-2004','90.1-2010']
-  climate_zones = ['ASHRAE 169-2006-2A',]#'ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+  templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
+  climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
   
   create_models = true
   run_models = true
