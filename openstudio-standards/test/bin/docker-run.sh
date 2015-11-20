6a0d4084d7eefc2f621580860ec990ba10c5e43e
Andrew Parker <Andrew.Parker@nrel.gov>
2015-11-20 10:52:33 -0700

    Define explicit test groups instead of dynamic assignment since tests vary significantly in length, and no test group may take more than 2 hrs (CircleCI limit).

--- openstudio-standards/test/bin/docker-run.sh
+++ openstudio-standards/test/bin/docker-run.sh
@@ -9,18 +9,38 @@ eval "$(rbenv init -)"
 cd /openstudio-standards/openstudio-standards
 bundle install
 
+# Run a specific set of tests on each node.
+# Test groups are defined in the Rakefile.
+# Each group must have a total runtime less
+# than 2 hrs.
+case $CIRCLE_NODE_INDEX in
+  0)
+    rake test:measures
+    ;;
+  1)
+    rake test:gem_group_1
+    ;;
+  2)
+    rake test:gem_group_2
+    ;;
+  3)
+    rake test:gem_group_3
+    ;;
+  *)
+esac
+    
 # Loop through the test files and run
 # every nth test, where n is determined
 # by the total number of CI nodes and
 # the index of this particular node.
 # Note: this command is running
 # ON EACH NODE.
-i=0
-files=()
-for testfile in $(find test/ -name "test_*.rb" | sort); do
-  if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
-  then
-    ruby $testfile
-  fi
-  ((i=i+1))
-done
+# i=0
+# files=()
+# for testfile in $(find test/ -name "test_*.rb" | sort); do
+  # if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+  # then
+    # ruby $testfile
+  # fi
+  # ((i=i+1))
+# done
