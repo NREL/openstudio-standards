4ee70a53eab1ec9a301696aeffbf05464fe0e2a9
phylroy <phylroy.lopez@gmail.com>
2015-11-25 18:35:39 -0500

    Added include path to /lib/openstudio-standards.rb so it these tests can be run from Netbeans.

--- measures/btap_equest_converter/tests/btap_equest_converter_test.rb
+++ measures/btap_equest_converter/tests/btap_equest_converter_test.rb
@@ -1,3 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../../../openstudio-standards/lib', __FILE__)
 require 'openstudio'
 require 'openstudio/ruleset/ShowRunnerOutput'
 require 'minitest/autorun'
