f68a0d9b1ff1a707ad8ef9cc827abcbefbcf51f7
phylroy <phylroy.lopez@gmail.com>
2015-11-27 12:36:07 -0500

    Small change to test git email notifications.

--- openstudio-standards/lib/openstudio-standards/btap/btap.rb
+++ openstudio-standards/lib/openstudio-standards/btap/btap.rb
@@ -17,6 +17,7 @@
 # *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 # **********************************************************************/
 
+
 require 'openstudio/energyplus/find_energyplus'
 require 'fileutils'
 require 'singleton'
