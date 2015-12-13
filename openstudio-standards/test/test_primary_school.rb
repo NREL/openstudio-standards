3322d6a34c58a90d219c863f9794e50c9473f6c1
Andrew Parker <Andrew.Parker@nrel.gov>
2015-12-12 19:48:30 -0700

    Decrease the number of climate zones for the schools tests so they'll finish

--- openstudio-standards/test/test_primary_school.rb
+++ openstudio-standards/test/test_primary_school.rb
@@ -5,7 +5,7 @@ class TestPrimarySchool < CreateDOEPrototypeBuildingTest
 
   building_types = ['PrimarySchool']
   templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2004','90.1-2007','90.1-2010','90.1-2013']
-  climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+  climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-5A']
   
   create_models = true
   run_models = true
