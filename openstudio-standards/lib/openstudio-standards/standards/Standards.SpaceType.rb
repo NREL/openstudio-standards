8ab5ea14bc026b0e649cb8afe8332c69c96fe442
David Goldwasser <david.goldwasser@nrel.gov>
2016-01-28 14:19:10 -0700

    Adding methods to construction from PTool repo

--- openstudio-standards/lib/openstudio-standards/standards/Standards.SpaceType.rb
+++ openstudio-standards/lib/openstudio-standards/standards/Standards.SpaceType.rb
@@ -35,11 +35,49 @@ class OpenStudio::Model::SpaceType
     # lookup space type properties
     space_type_properties = self.model.find_object(@standards["space_types"], search_criteria)
 
+    # switch to use this but update test in standards and measures to load this outside of the method
+    #space_type_properties = self.model.find_object(self.model.standards["space_types"], search_criteria)
+
     return space_type_properties
 
   end
-  
-  private
+
+
+  # this returns standards data for selected space type and template
+  # @param [string] target template for lookup
+  # @return [hash] hash of internal loads for different load types
+  def get_standards_data(template)
+
+    if self.standardsBuildingType.is_initialized
+      standards_building_type = self.standardsBuildingType.get
+    else
+      standards_building_type = nil
+    end
+    if self.standardsSpaceType.is_initialized
+      standards_space_type = self.standardsSpaceType.get
+    else
+      standards_space_type = nil
+    end
+
+    # lookup if space type is residential in space types standards
+
+    # standard.Model has find_climate_zone_set method. pass in climate zone and template
+
+    # populate search hash
+    search_criteria = {
+        "template" => template,
+        "climate_zone_set" => climate_zone_set,
+        "is_residential" => standards_building_type,
+    }
+
+    # lookup space type properties
+
+    # switch to use this but update test in standards and measures to load this outside of the method
+    space_type_properties = self.model.find_object(self.model.standards["construction_properties"], search_criteria)
+
+    return space_type_properties
+
+  end
 
 
 end
