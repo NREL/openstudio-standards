e5c0370f099d78563e6b28be0d386e51d508c7ec
David Goldwasser <david.goldwasser@nrel.gov>
2016-02-04 00:25:33 -0700

    adding method for const prop to model
    similar to what is in space type, but this one runs off of the model
    and adds an argument for Noresidential vs. Residential.

--- openstudio-standards/lib/openstudio-standards/standards/Standards.Model.rb
+++ openstudio-standards/lib/openstudio-standards/standards/Standards.Model.rb
@@ -1725,6 +1725,37 @@ class OpenStudio::Model::Model
     return result
 
   end
+
+  # this returns standards data for selected construction
+  # @param [string] target template for lookup
+  # @param [string] intended_surface_type template for lookup
+  # @param [string] standards_construction_type template for lookup
+  # @param [string] building_category template for lookup
+  # @return [hash] hash of construction properties
+  def get_construction_properties(template,intended_surface_type,standards_construction_type,building_category = 'Nonresidential')
+
+    # todo - remove loading standards before merge
+    standards = self.load_openstudio_standards_json
+
+    # get climate_zone_set
+    climate_zone = self.get_building_climate_zone_and_building_type['climate_zone']
+    climate_zone_set = self.find_climate_zone_set(climate_zone, template,standards)
+
+    # populate search hash
+    search_criteria = {
+        "template" => template,
+        "climate_zone_set" => climate_zone_set,
+        "intended_surface_type" => intended_surface_type,
+        "standards_construction_type" => standards_construction_type,
+        "building_category" => building_category,
+    }
+
+    # switch to use this but update test in standards and measures to load this outside of the method
+    construction_properties = self.find_object(standards["construction_properties"], search_criteria)
+
+    return construction_properties
+
+  end
   
   private
 
