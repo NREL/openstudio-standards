b57cab179552a932514cc256024a846a8fa14874
Julien Marrec <julien.marrec@gmail.com>
2016-02-16 15:52:05 +0100

    Added a unit test for the PRM (on my dummy office building created not so long ago) to the rake file. Added helper to set the weather file based on the climate zone and in relative path, so that it can run on several machine/servers. Will see if that works...

--- openstudio-standards/test/create_performance_rating_method_helper.rb
+++ openstudio-standards/test/create_performance_rating_method_helper.rb
@@ -13,7 +13,42 @@ def create_baseline_model(model_name, standard, climate_zone, building_type, deb
   model = translator.loadModel(path)
   assert(model.is_initialized, "Could not load test model '#{model_name}.osm' from test_models/performance_rating_method.  Check name for typos.")
   model = model.get
-  
+
+  # Check if there's a need to set the weather
+  # 'ASHRAE 169-2006-1A', 'ASHRAE 169-2006-1B', 'ASHRAE 169-2006-2A', 'ASHRAE 169-2006-2B',
+  # 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A', 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C',
+  # 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B', 'ASHRAE 169-2006-5C', 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A',
+  # 'ASHRAE 169-2006-7B', 'ASHRAE 169-2006-8A', 'ASHRAE 169-2006-8B'
+  if model.weatherFile.empty?
+    epw_name = nil
+    base_rel_path = '../../data/weather/'
+    case climate_zone
+      when 'ASHRAE 169-2006-2A'
+        epw_name = 'USA_TX_Houston-Bush.Intercontinental.AP.722430_TMY3.epw'
+      when 'ASHRAE 169-2006-4A'
+        epw_name = 'USA_MD_Baltimore-Washington.Intl.AP.724060_TMY3.epw'
+      else
+        puts "No Weather file set, and CANNOT locate #{climate_zone}"
+        return false
+    end
+    if epw_name.nil?
+      puts "No Weather file set, and CANNOT locate #{climate_zone}"
+    else
+      rel_path = base_rel_path + epw_name
+      weather_file =  File.expand_path(rel_path, __FILE__)
+      weather = BTAP::Environment::WeatherFile.new(weather_file)
+      #Set Weather file to model.
+      success = weather.set_weather_file(model)
+      if success
+        puts "Set Weather file to '#{weather_file}'"
+      else
+        puts "Failed to set the weather file"
+        return false
+      end
+    end
+  end
+
+
   # Create a directory for the test result
   osm_directory = "#{test_dir}/#{model_name}-#{standard}-#{climate_zone}"
   if !Dir.exists?(osm_directory)
@@ -25,13 +60,13 @@ def create_baseline_model(model_name, standard, climate_zone, building_type, deb
   if debug
     msg_log.setLogLevel(OpenStudio::Debug)
   else
-   msg_log.setLogLevel(OpenStudio::Info)
+    msg_log.setLogLevel(OpenStudio::Info)
   end
-  
+
   # Create the baseline model from the
   # supplied proposed test model
   model.create_performance_rating_method_baseline_building(building_type,standard,climate_zone,osm_directory,debug = false)
-  
+
   # Show the output messages
   msg_log.logMessages.each do |msg|
     # DLM: you can filter on log channel here for now
@@ -42,7 +77,7 @@ def create_baseline_model(model_name, standard, climate_zone, building_type, deb
           msg.logChannel.include?("setFileExtension") || # .ddy extension unexpected
           msg.logChannel.include?("Translator") || # Forward translator and geometry translator
           msg.logMessage.include?("UseWeatherFile") # 'UseWeatherFile' is not yet a supported option for YearDescription
-          
+
       # Report the message in the correct way
       if msg.logLevel == OpenStudio::Info
         puts(msg.logMessage)
@@ -57,7 +92,8 @@ def create_baseline_model(model_name, standard, climate_zone, building_type, deb
   end
 
   # Save the test model
-  model.save(OpenStudio::Path.new("#{osm_directory}/#{model_name}_baseline.osm"), true)  
+  model.save(OpenStudio::Path.new("#{osm_directory}/#{model_name}_baseline.osm"), true)
+
 
   return model
 
