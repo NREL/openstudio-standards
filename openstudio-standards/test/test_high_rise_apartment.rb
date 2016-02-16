02a692cd0bbd80a0960aed68d463b7e619acdffa
Kaiyu Sun <ksun@lbl.gov>
2016-02-16 14:28:58 -0800

    Add error check for HighriseApartment Pre1980 and 1980-2004 in Prototype.Model

--- openstudio-standards/test/test_high_rise_apartment.rb
+++ openstudio-standards/test/test_high_rise_apartment.rb
@@ -4,7 +4,7 @@ require_relative 'create_doe_prototype_helper'
 class TestHighRiseApartment < CreateDOEPrototypeBuildingTest
   
   building_types = ['HighriseApartment']
-  templates = ['90.1-2004']
+  templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004']
   # templates = ['90.1-2007', '90.1-2010', '90.1-2013', '90.1-2004'] 
   # templates = ['90.1-2007', '90.1-2010', '90.1-2004'] 
   # climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
