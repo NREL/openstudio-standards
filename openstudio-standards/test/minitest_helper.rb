9f6e9e19e0f83b140c871f385cd60d03028224e6
Andrew Parker <Andrew.Parker@nrel.gov>
2016-02-20 17:49:38 -0700

    Attempt to get coverage tests to show up correctly

--- openstudio-standards/test/minitest_helper.rb
+++ openstudio-standards/test/minitest_helper.rb
@@ -2,7 +2,21 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'openstudio'
+require 'simplecov'
 require 'coveralls'
+
+# Ignore some of the code in coverage testing
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/.idea/'
+  add_filter '/.yardoc/'
+  add_filter '/data/'
+  add_filter '/doc/'
+  add_filter '/docs/'
+  add_filter '/pkg/'
+  add_filter '/test/'
+end
+
 Coveralls.wear!
 # Require local version instead of installed version for developers
 if require_relative '../lib/openstudio-standards.rb'
