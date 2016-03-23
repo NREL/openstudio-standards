05e0850e0959c45dda833a7a078bd2fca176f3ab
Kaiyu Sun <ksun@lbl.gov>
2016-03-23 10:39:24 -0700

    rerun all the building types

--- openstudio-standards/test/test_quick_service_restaurant.rb
+++ openstudio-standards/test/test_quick_service_restaurant.rb
@@ -6,6 +6,11 @@ class TestQuickServiceRestaurant < CreateDOEPrototypeBuildingTest
   building_types = ['QuickServiceRestaurant']
   templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2010']
   climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+  # templates = ['DOE Ref 1980-2004', 'DOE Ref Pre-1980', '90.1-2007', '90.1-2010', '90.1-2013', '90.1-2004'] 
+  # climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
+                   # 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
+                   # 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
+                   # 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A', 'ASHRAE 169-2006-8A'] 
   
   create_models = true
   run_models = false
