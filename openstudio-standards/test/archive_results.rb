7dd6c40bb29f41ab7b599aea0e67abcd26473fa0
Kaiyu Sun <ksun@lbl.gov>
2016-03-28 19:10:41 -0700

    Fix SHW schedule of QuickServiceRestaurant and FullServiceRestaurant

--- openstudio-standards/test/archive_results.rb
+++ openstudio-standards/test/archive_results.rb
@@ -3,13 +3,14 @@ require 'find'
 
 output_dir = "#{Dir.pwd}/output"
 # list the building types, vintages and climate zones that will be archived.
-building_types = ['RetailStandalone', 'RetailStripmall']
-vintages = ['DOE Ref 1980-2004', '90.1-2010']
-# climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
-                 # 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
-                 # 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
-                 # 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A', 'ASHRAE 169-2006-8A'] 
-climate_zones = ['ASHRAE 169-2006-2A','ASHRAE 169-2006-3B','ASHRAE 169-2006-4A','ASHRAE 169-2006-5A']
+building_types = ['RetailStandalone', 'RetailStripmall', 'FullServiceRestaurant', 'QuickServiceRestaurant', 'LargeHotel',
+                 'SmallHotel', 'HighriseApartment', 'MidriseApartment', 'Outpatient']
+vintages = ['DOE Ref 1980-2004', 'DOE Ref Pre-1980', '90.1-2004', '90.1-2007', '90.1-2010', '90.1-2013']
+climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
+                 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
+                 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
+                 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A', 'ASHRAE 169-2006-8A'] 
+
 # create archive folder
 archive_dir = "#{Dir.pwd}/Archive"
 Dir.mkdir(archive_dir) unless Dir.exist?(archive_dir)
