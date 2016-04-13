5b47ee81d9dc91219a8f5f3fee21f7bf72e913f5
Andrew Parker <Andrew.Parker@nrel.gov>
2016-04-13 15:40:14 -0600

    Adds more tests for models sent by testers

--- openstudio-standards/test/test_create_performance_rating_method_baseline_building.rb
+++ openstudio-standards/test/test_create_performance_rating_method_baseline_building.rb
@@ -6,8 +6,11 @@ class CreatePerformanceRatingMethodBaselineBuildingTest < Minitest::Test
   def test_901_2013_sec_school
   
     # Create the baseline model
-    model = create_baseline_model('SecondarySchool-DOE Ref Pre-1980-ASHRAE 169-2006-2A', '90.1-2013', 'ASHRAE 169-2006-2A', 'SecondarySchool', false)
-  
+    model, errs = create_baseline_model('SecondarySchool-DOE Ref Pre-1980-ASHRAE 169-2006-2A', '90.1-2013', 'ASHRAE 169-2006-2A', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+
     # Conditions expected to be true in the baseline model
     
     # Lighting power densities
@@ -18,14 +21,77 @@ class CreatePerformanceRatingMethodBaselineBuildingTest < Minitest::Test
     lpd_w_per_ft2 = OpenStudio.convert(lpd_w_per_m2,'W/m^2','W/ft^2').get
     #               (expected, actual, tolerance, message to show if it fails) 
     assert_in_delta(1.24, lpd_w_per_ft2, 0.01, "Classroom LPD is wrong.")
-    
-    # Example of a failing assertion
-    # Gym LPD should be 1.24 W/ft2
-    space = model.getSpaceByName("Gym_ZN_1_FLR_1").get
-    lpd_w_per_m2 = space.lightingPowerPerFloorArea
-    lpd_w_per_ft2 = OpenStudio.convert(lpd_w_per_m2,'W/m^2','W/ft^2').get
-    assert_in_delta(1.24, lpd_w_per_ft2, 0.01, "Gymnasium LPD is wrong.")    
-    
+      
   end
 
+  def test_bldg_1_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_1', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+
+  end  
+  
+  def test_bldg_2_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_2', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+ 
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+
+  end
+
+  def test_bldg_3_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_3', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+    
+  end  
+  
+  def test_bldg_4_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_4', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+
+  end  
+  
+  def test_bldg_5_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_5', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+
+  end  
+  
+  def test_bldg_6_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_6', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+
+  end  
+  
+  def test_bldg_7_901_2013
+  
+    # Create the baseline model
+    model, errs = create_baseline_model('bldg_7', '90.1-2013', 'ASHRAE 169-2006-5B', 'SecondarySchool', false)
+
+    # Assert no errors
+    assert(errs.size == 0, "Model created, but had Errors: #{errs.join(',')}")
+    
+  end  
+  
 end
