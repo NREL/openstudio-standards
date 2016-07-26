174993ac679a07a6a3767c4739752c9f2a1373cb
Andrew Parker <Andrew.Parker@nrel.gov>
2016-07-26 12:23:37 -0600

    Make space type info error into an info message.

--- openstudio-standards/lib/openstudio-standards/standards/Standards.SpaceType.rb
+++ openstudio-standards/lib/openstudio-standards/standards/Standards.SpaceType.rb
@@ -31,7 +31,7 @@ class OpenStudio::Model::SpaceType
     space_type_properties = self.model.find_object($os_standards["space_types"], search_criteria)
 
     if space_type_properties.nil?
-      OpenStudio::logFree(OpenStudio::Error, 'openstudio.standards.SpaceType', "Space type properties lookup failed: #{search_criteria}.")
+      OpenStudio::logFree(OpenStudio::Info, 'openstudio.standards.SpaceType', "Space type properties lookup failed: #{search_criteria}.")
       space_type_properties = {}
     end
     
