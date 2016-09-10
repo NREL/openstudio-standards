7767e3743c75b0de3392c804c93ad9e6f00701cf
Andrew Parker <Andrew.Parker@nrel.gov>
2016-09-10 11:51:14 -0600

    Fixes up the PFP terminal fan power calculations

--- openstudio-standards/lib/openstudio-standards/standards/Standards.AirTerminalSingleDuctParallelPIUReheat.rb
+++ openstudio-standards/lib/openstudio-standards/standards/Standards.AirTerminalSingleDuctParallelPIUReheat.rb
@@ -14,10 +14,10 @@ class OpenStudio::Model::AirTerminalSingleDuctParallelPIUReheat
     sec_flow_frac = 0.5
     min_flow_frac = 0.3
     fan_efficacy_w_per_cfm = 0.35
-    # case template
-    # when
-    # else
-    # end
+
+    # Convert efficacy to metric
+    # 1 cfm = 0.0004719 m^3/s
+    fan_efficacy_w_per_m3_per_s = fan_efficacy_w_per_cfm / 0.0004719
 
     # Get the maximum flow rate through the terminal
     max_primary_air_flow_rate = nil
@@ -30,37 +30,32 @@ class OpenStudio::Model::AirTerminalSingleDuctParallelPIUReheat
     # Set the max secondary air flow rate
     max_sec_flow_rate_m3_per_s = max_primary_air_flow_rate * sec_flow_frac
     setMaximumSecondaryAirFlowRate(max_sec_flow_rate_m3_per_s)
+    max_sec_flow_rate_cfm = OpenStudio.convert(max_sec_flow_rate_m3_per_s, 'm^3/s', 'ft^3/min').get
 
     # Set the minimum flow fraction
     # TODO Also compare to min OA requirement
     setMinimumPrimaryAirFlowFraction(min_flow_frac)
 
-    # Set the fan efficacy
+    # Get the fan
     fan = self.fan.to_FanConstantVolume.get
-    fan_rise_pa = fan.pressureRise
-    fan_rise_in_wc = OpenStudio.convert(fan_rise_pa, 'Pa', 'inH_{2}O')
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "=> Pressure Rise = #{fan_rise_pa} Pa")
 
-    max_sec_flow_rate_cfm = OpenStudio.convert(max_sec_flow_rate_m3_per_s, 'm^3/s', 'ft^3/min').get
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "=> Maximum Fan Flow Rate = #{max_sec_flow_rate_cfm} m3/s")
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "=> Maximum Secondary Air Flow Rate = #{maximumSecondaryAirFlowRate.get} m3/s")
+    # Set the impeller efficiency
+    fan.change_impeller_efficiency(fan.baseline_impeller_efficiency(template))
 
-    fan_efficiency = fan.fanEfficiency
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "=> Fan Total Efficiency = #{fan_efficiency}")
+    # Set the motor efficiency, preserving the impeller efficency.
+    # For terminal fans, a bhp lookup of 0.5bhp is always used because
+    # they are assumed to represent a series of small fans in reality.
+    fan.apply_standard_minimum_motor_efficiency(template, fan.brake_horsepower)
 
-    fan_power_w = fan_rise_pa * max_sec_flow_rate_m3_per_s / fan_efficiency
-    fan_efficacy_calc = fan_power_w / max_sec_flow_rate_m3_per_s
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "=> fan efficacy calculated = #{fan_efficacy_calc} W-s/m3")
-
-    fan_efficacy_w_per_m3_per_s = fan_efficacy_w_per_cfm * OpenStudio.convert(1, 'm^3/s', 'cfm').get
-
-    fan_rise_new_pa = fan_efficacy_w_per_m3_per_s * fan_efficiency
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "=> fan pressure rise new = #{fan_rise_new_pa} Pa")
+    # Calculate a new pressure rise to hit the target W/cfm
+    fan_tot_eff = fan.fanEfficiency
+    fan_rise_new_pa = fan_efficacy_w_per_m3_per_s * fan_tot_eff
     fan.setPressureRise(fan_rise_new_pa)
-    fan_power_new_w = fan_rise_new_pa * max_sec_flow_rate_cfm / fan_efficiency
-    fan_efficacy_new_w_per_cfm = fan_power_new_w / max_sec_flow_rate_cfm
 
-    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "For #{name}: fan efficacy set to #{fan_efficacy_new_w_per_cfm.round(2)} W/cfm, fan bhp = #{fan.brake_horsepower} hp, motor efficiency = #{fan.motorEfficiency}.")
+    # Calculate the newly set efficacy
+    fan_power_new_w = fan_rise_new_pa * max_sec_flow_rate_m3_per_s / fan_tot_eff
+    fan_efficacy_new_w_per_cfm = fan_power_new_w / max_sec_flow_rate_cfm
+    OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.AirTerminalSingleDuctParallelPIUReheat', "For #{name}: fan efficacy set to #{fan_efficacy_new_w_per_cfm.round(2)} W/cfm.")
 
     return true
   end
