634aa48070f67964b29c5007ba00616fe381788f
Andrew Parker <Andrew.Parker@nrel.gov>
2016-10-11 11:53:03 -0600

    Fixes #112 to handle radiant htg/clg

--- openstudio-standards/lib/openstudio-standards/standards/Standards.ThermalZone.rb
+++ openstudio-standards/lib/openstudio-standards/standards/Standards.ThermalZone.rb
@@ -731,6 +731,65 @@ class OpenStudio::Model::ThermalZone
       return htd
     end
 
+    # Check if the zone has radiant heating,
+    # and if it does, get heating setpoint schedule
+    # directly from the radiant system to check.
+    equipment.each do |equip|
+      htg_sch = nil
+      if equip.to_ZoneHVACHighTemperatureRadiant.is_initialized
+        equip = equip.to_ZoneHVACHighTemperatureRadiant.get
+        if equip.heatingSetpointTemperatureSchedule.is_initialized
+          htg_sch = equip.heatingSetpointTemperatureSchedule.get
+        end 
+      elsif equip.to_ZoneHVACLowTemperatureRadiantElectric.is_initialized
+        equip = equip.to_ZoneHVACLowTemperatureRadiantElectric.get
+        htg_sch = equip.heatingSetpointTemperatureSchedule.get
+      elsif equip.to_ZoneHVACLowTempRadiantConstFlow.is_initialized
+        equip = equip.to_ZoneHVACLowTempRadiantConstFlow.get
+        htg_coil = equip.heatingCoil
+        if htg_coil.to_CoilHeatingLowTempRadiantConstFlow.is_initialized
+          htg_coil = htg_coil.to_CoilHeatingLowTempRadiantConstFlow.get
+          if htg_coil.heatingHighControlTemperatureSchedule.is_initialized
+            htg_sch = htg_coil.heatingHighControlTemperatureSchedule.get
+          end
+        end
+      elsif equip.to_ZoneHVACLowTempRadiantVarFlow.is_initialized
+        equip = equip.to_ZoneHVACLowTempRadiantVarFlow.get
+        htg_coil = equip.heatingCoil
+        if htg_coil.to_CoilHeatingLowTempRadiantVarFlow.is_initialized
+          htg_coil = htg_coil.to_CoilHeatingLowTempRadiantVarFlow.get
+          if htg_coil.heatingControlTemperatureSchedule.is_initialized
+            htg_sch = htg_coil.heatingControlTemperatureSchedule.get
+          end
+        end    
+      end
+      # Move on if no heating schedule was found
+      next if htg_sch.nil?
+      # Get the setpoint from the schedule
+      if htg_sch.to_ScheduleRuleset.is_initialized
+        htg_sch = htg_sch.to_ScheduleRuleset.get
+        max_c = htg_sch.annual_min_max_value['max']
+        if max_c > temp_c
+          htd = true
+        end
+      elsif htg_sch.to_ScheduleConstant.is_initialized
+        htg_sch = htg_sch.to_ScheduleConstant.get
+        max_c = htg_sch.annual_min_max_value['max']
+        if max_c > temp_c
+          htd = true
+        end
+      elsif htg_sch.to_ScheduleCompact.is_initialized
+        htg_sch = htg_sch.to_ScheduleCompact.get
+        max_c = htg_sch.annual_min_max_value['max']
+        if max_c > temp_c
+          htd = true
+        end
+      else
+        OpenStudio.logFree(OpenStudio::Debug, 'openstudio.Standards.ThermalZone', "Zone #{name} used an unknown schedule type for the heating setpoint; assuming heated.")
+        htd = true
+      end
+    end
+
     # Unheated if no thermostat present
     if thermostat.empty?
       return htd
@@ -811,6 +870,57 @@ class OpenStudio::Model::ThermalZone
     if area_plenum > area_non_plenum
       cld = true
       return cld
+    end
+
+    # Check if the zone has radiant cooling,
+    # and if it does, get cooling setpoint schedule
+    # directly from the radiant system to check.
+    equipment.each do |equip|
+      clg_sch = nil
+      if equip.to_ZoneHVACLowTempRadiantConstFlow.is_initialized
+        equip = equip.to_ZoneHVACLowTempRadiantConstFlow.get
+        clg_coil = equip.heatingCoil
+        if clg_coil.to_CoilCoolingLowTempRadiantConstFlow.is_initialized
+          clg_coil = clg_coil.to_CoilCoolingLowTempRadiantConstFlow.get
+          if clg_coil.coolingLowControlTemperatureSchedule.is_initialized
+            clg_sch = clg_coil.coolingLowControlTemperatureSchedule.get
+          end
+        end
+      elsif equip.to_ZoneHVACLowTempRadiantVarFlow.is_initialized
+        equip = equip.to_ZoneHVACLowTempRadiantVarFlow.get
+        clg_coil = equip.heatingCoil
+        if clg_coil.to_CoilCoolingLowTempRadiantVarFlow.is_initialized
+          clg_coil = clg_coil.to_CoilCoolingLowTempRadiantVarFlow.get
+          if clg_coil.coolingControlTemperatureSchedule.is_initialized
+            clg_sch = clg_coil.coolingControlTemperatureSchedule.get
+          end
+        end    
+      end
+      # Move on if no cooling schedule was found
+      next if clg_sch.nil?
+      # Get the setpoint from the schedule
+      if clg_sch.to_ScheduleRuleset.is_initialized
+        clg_sch = clg_sch.to_ScheduleRuleset.get
+        min_c = clg_sch.annual_min_max_value['min']
+        if min_c < temp_c
+          cld = true
+        end
+      elsif clg_sch.to_ScheduleConstant.is_initialized
+        clg_sch = clg_sch.to_ScheduleConstant.get
+        min_c = clg_sch.annual_min_max_value['min']
+        if min_c < temp_c
+          cld = true
+        end
+      elsif clg_sch.to_ScheduleCompact.is_initialized
+        clg_sch = clg_sch.to_ScheduleCompact.get
+        min_c = clg_sch.annual_min_max_value['min']
+        if min_c < temp_c
+          cld = true
+        end
+      else
+        OpenStudio.logFree(OpenStudio::Debug, 'openstudio.Standards.ThermalZone', "Zone #{name} used an unknown schedule type for the cooling setpoint; assuming cooled.")
+        cld = true
+      end
     end
 
     # Unheated if no thermostat present
