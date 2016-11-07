bfa5e130a82e16dd3f33909d03cb0af1ff1f81b7
khaddad <khaddad.energy.eng@gmail.com>
2016-11-07 14:05:16 -0500

    add reference to NECB 2011

--- openstudio-standards/lib/openstudio-standards/standards/Standards.WaterHeaterMixed.rb
+++ openstudio-standards/lib/openstudio-standards/standards/Standards.WaterHeaterMixed.rb
@@ -129,7 +129,7 @@ class OpenStudio::Model::WaterHeaterMixed
           base_ef, vol_drt = case template
                              when '90.1-2004', '90.1-2007'
                                [0.62, 0.0019]
-                             when '90.1-2010'
+                             when '90.1-2010', 'NECB 2011'
                                [0.67, 0.0019]
                              when '90.1-2013'
                                [0.67, 0.0005]
@@ -152,12 +152,11 @@ class OpenStudio::Model::WaterHeaterMixed
           et = 0.8
           # Calculate the max allowable standby loss (SL)
           cap_adj, vol_drt = case template
-                   when '90.1-2004', '90.1-2007', '90.1-2010'
+                   when '90.1-2004', '90.1-2007', '90.1-2010', 'NECB 2011'
                      [800, 110]
                    when '90.1-2013'
                      [799, 16.6]
                    end
-
           sl_btu_per_hr = (capacity_btu_per_hr / cap_adj + vol_drt * Math.sqrt(volume_gal))
           # Calculate the skin loss coefficient (UA)
           ua_btu_per_hr_per_f = (sl_btu_per_hr * et) / 70
