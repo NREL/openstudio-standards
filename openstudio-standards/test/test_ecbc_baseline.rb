915132ab266f7fe906a917ce44c354e784af6daf
Subhash <subhash.iiit.h@gmail.com>
2016-11-08 15:52:42 +0530

    Updated the test_ecbc_baseline file with the Lighting scenario (open office-lobby)

--- openstudio-standards/test/test_ecbc_baseline.rb
+++ openstudio-standards/test/test_ecbc_baseline.rb
@@ -9,6 +9,14 @@ class BaselineECBCTest < Minitest::Test
     model = create_ecbc_baseline_model('bldg_1', 'ECBC 2007', 'ECBC Warm and Humid', 'MediumOffice', nil, false, true)
    
     # Assertions about the baseline model go here
+   
+    # Open Office - Lobby LPD should be 1.3 W/ft2
+  
+    space = model.getSpaceByName("Lobby 1").get
+    lpd_w_per_m2 = space.lightingPowerPerFloorArea
+    lpd_w_per_ft2 = OpenStudio.convert(lpd_w_per_m2,'W/m^2','W/ft^2').get
+    assert_in_delta(1.3, lpd_w_per_ft2, 0.01, "Open Office Lobby LPD is wrong.") #The measure did this correctly
+    
 
   end 
 
