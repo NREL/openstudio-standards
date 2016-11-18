5aa28b9973dece7b11c1847989e52f2de5260d18
phylroy <phylroy.lopez@gmail.com>
2016-11-18 14:38:19 +0000

    removed non relevant files

--- openstudio-standards/test/tet_tests.rb
+++
@@ -1,8 +0,0 @@
-require  './test_necb_bldg_small_hotel.rb'
-require  './test_necb_bldg_full_service_restaurant.rb'
-require  './test_necb_bldg_retail_standalone.rb'
-require  './test_necb_bldg_small_office.rb'
-require  './test_small_hotel.rb' # 7 min
-
-
-
