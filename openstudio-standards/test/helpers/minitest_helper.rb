ad4c8d59fde85a6c03e49964d3dbe66613d8d109
Andrew Parker <Andrew.Parker@nrel.gov>
2017-03-14 20:54:13 -0600

    Disable Codecov since formatter is failing for some unknown reason

--- openstudio-standards/test/helpers/minitest_helper.rb
+++ openstudio-standards/test/helpers/minitest_helper.rb
@@ -1,10 +1,11 @@
 require 'simplecov'
-require 'codecov'
+# require 'codecov'
 
 # Get the code coverage in html for local viewing
 # and in JSON for CI codecov
 if ENV['CI'] == 'true'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  # SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 else
   SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 end
