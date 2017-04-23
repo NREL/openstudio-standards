e438552a10a71529bb0eda2acc4edae8439330c2
Andrew Parker <Andrew.Parker@nrel.gov>
2017-04-22 19:06:30 -0600

    Adds hydronic systems to the hvac system test

--- openstudio-standards/test/doe_prototype/test_add_hvac_systems.rb
+++ openstudio-standards/test/doe_prototype/test_add_hvac_systems.rb
@@ -11,6 +11,8 @@ class TestAddHVACSystems < Minitest::Test
     # List all the HVAC system types to test
     hvac_systems = [
 
+      ## Forced Air ##
+
       # Gas, Electric, forced air
       ['PTAC', 'NaturalGas', nil, 'Electricity'],
       ['PSZ-AC', 'NaturalGas', nil, 'Electricity'],
@@ -29,28 +31,57 @@ class TestAddHVACSystems < Minitest::Test
       ['VAV Reheat', 'DistrictHeating', 'DistrictHeating', 'Electricity'],
 
       # Ambient Loop, Ambient Loop, forced air
-      # TODO make sure ambient loop configurations work
-      ['Water Source Heat Pumps with ERVs', 'HeatPump', nil, 'HeatPump'],
-      ['Water Source Heat Pumps with DOAS', 'HeatPump', nil, 'HeatPump'],
       ['PVAV Reheat', 'HeatPump', 'HeatPump', 'HeatPump'],
       ['VAV Reheat', 'HeatPump', 'HeatPump', 'HeatPump'],
 
       # Gas, District Chilled Water, forced air
-      ['PTAC', 'NaturalGas', nil, 'DistrictCooling'],
       ['PSZ-AC', 'NaturalGas', nil, 'DistrictCooling'],
       ['PVAV Reheat', 'NaturalGas', 'NaturalGas', 'DistrictCooling'],
       ['VAV Reheat', 'NaturalGas', 'NaturalGas', 'DistrictCooling'],
 
       # Electric, District Chilled Water, forced air
-      ['PTAC', 'Electricity', nil, 'DistrictCooling'],
       ['PSZ-AC', 'Electricity', nil, 'DistrictCooling'],
       ['PVAV Reheat', 'Electricity', 'Electricity', 'DistrictCooling'],
       ['VAV Reheat', 'Electricity', 'Electricity', 'DistrictCooling'],
 
       # District Hot Water, District Chilled Water, forced air
-      ['PTAC', 'DistrictHeating', nil, 'DistrictCooling'],
       ['PVAV Reheat', 'DistrictHeating', 'DistrictHeating', 'DistrictCooling'],
-      ['VAV Reheat', 'DistrictHeating', 'DistrictHeating', 'DistrictCooling']
+      ['VAV Reheat', 'DistrictHeating', 'DistrictHeating', 'DistrictCooling'],
+
+      ## Hydronic ##
+
+      # Gas, Electric, hydronic
+      ['Fan Coil with DOAS', 'NaturalGas', nil, 'Electricity'],
+      ['Water Source Heat Pumps with DOAS', 'NaturalGas', nil, 'Electricity'],
+      ['Fan Coil with DOAS', 'NaturalGas', 'NaturalGas', 'Electricity'],
+
+      # Electric, Electric, hydronic
+      ['Ground Source Heat Pumps with ERVs', 'Electricity', nil, 'Electricity'],
+      ['Ground Source Heat Pumps with DOAS', 'Electricity', nil, 'Electricity'],
+      ['Ground Source Heat Pumps with DOAS', 'Electricity', 'Electricity', 'Electricity'],
+
+      # District Hot Water, Electric, hydronic
+      ['Fan Coil with DOAS', 'DistrictHeating', nil, 'Electricity'],
+      ['Water Source Heat Pumps with DOAS', 'DistrictHeating', 'DistrictHeating', 'Electricity'],
+      ['Fan Coil with DOAS', 'DistrictHeating', 'DistrictHeating', 'Electricity'],
+
+      # Ambient Loop, Ambient Loop, hydronic
+      ['Water Source Heat Pumps with ERVs', 'HeatPump', nil, 'HeatPump'],
+      ['Water Source Heat Pumps with DOAS', 'HeatPump', nil, 'HeatPump'],
+      ['Water Source Heat Pumps with DOAS', 'HeatPump', 'HeatPump', 'HeatPump'],
+
+      # Gas, District Chilled Water, hydronic
+      ['Fan Coil with DOAS', 'NaturalGas', nil, 'DistrictCooling'],
+      ['Fan Coil with DOAS', 'NaturalGas', 'NaturalGas', 'DistrictCooling'],
+
+      # Electric, District Chilled Water, hydronic
+      ['Fan Coil with ERVs', 'Electricity', nil, 'DistrictCooling'],
+      ['Fan Coil with DOAS', 'Electricity', 'Electricity', 'DistrictCooling'],
+
+      # District Hot Water, District Chilled Water, hydronic
+      ['Fan Coil with ERVs', 'DistrictHeating', nil, 'DistrictCooling'],
+      ['Fan Coil with DOAS', 'DistrictHeating', nil, 'DistrictCooling'],
+      ['Fan Coil with DOAS', 'DistrictHeating', 'DistrictHeating', 'DistrictCooling']
     ]
 
     template = '90.1-2013'
@@ -59,7 +90,7 @@ class TestAddHVACSystems < Minitest::Test
     # and run a sizing run to ensure it simulates.
     i = 0
     errs = []
-    hvac_systems.sort.each do |system_type, main_heat_fuel, zone_heat_fuel, cool_fuel|
+    hvac_systems.each do |system_type, main_heat_fuel, zone_heat_fuel, cool_fuel|
       i += 1
       # next if i < 13
       
@@ -68,27 +99,38 @@ class TestAddHVACSystems < Minitest::Test
       type_desc = "#{system_type} #{main_heat_fuel} #{zone_heat_fuel} #{cool_fuel}" 
       puts "running #{type_desc}"
 
-      # Load the test model
-      model = safe_load_model("#{File.dirname(__FILE__)}/models/basic_2_story_office_no_hvac.osm")
-
-      # Assign a weather file
-      model.add_design_days_and_weather_file('MediumOffice', template, 'ASHRAE 169-2006-7A', '')
-
-      # Add the HVAC
-      model.add_hvac_system(template, system_type, main_heat_fuel, zone_heat_fuel, cool_fuel, model.getThermalZones)
-
-      # Save the model
       model_dir = "#{output_dir}/hvac_#{system_type}_#{main_heat_fuel}_#{zone_heat_fuel}_#{cool_fuel}"
-      FileUtils.mkdir output_dir unless Dir.exist? output_dir
-      model.save("#{model_dir}/final.osm", true)
+      # Load the model if already created
+      if File.exist?("#{model_dir}/final.osm")
 
-      # Run the sizing run
-      annual_run_success = model.run_simulation_and_log_errors("#{model_dir}/AR")
+        model = OpenStudio::Model::Model.new
+        sql = safe_load_sql("#{model_dir}/AR/run/eplusout.sql")
+        model.setSqlFile(sql)
 
-      # Log the errors
-      log_messages_to_file("#{model_dir}/openstudio-standards.log", debug=false)
+      # If not created, make and run annual simulation
+      else
 
-      errs << "For #{type_desc} annual run failed" unless annual_run_success
+        # Load the test model
+        model = safe_load_model("#{File.dirname(__FILE__)}/models/basic_2_story_office_no_hvac.osm")
+
+        # Assign a weather file
+        model.add_design_days_and_weather_file('MediumOffice', template, 'ASHRAE 169-2006-7A', '')
+
+        # Add the HVAC
+        model.add_hvac_system(template, system_type, main_heat_fuel, zone_heat_fuel, cool_fuel, model.getThermalZones)
+
+        # Save the model
+        model.save("#{model_dir}/final.osm", true)
+
+        # Run the sizing run
+        annual_run_success = model.run_simulation_and_log_errors("#{model_dir}/AR")
+
+        # Log the errors
+        log_messages_to_file("#{model_dir}/openstudio-standards.log", debug=false)
+
+        errs << "For #{type_desc} annual run failed" unless annual_run_success
+
+      end
 
       # Check the conditioned floor area
       errs << "For #{type_desc} there was no conditioned area." if model.net_conditioned_floor_area == 0
@@ -96,11 +138,14 @@ class TestAddHVACSystems < Minitest::Test
       # Check the unmet hours
       unmet_hrs = model.annual_occupied_unmet_hours
       max_unmet_hrs = 300
-      errs << "For #{type_desc} there were #{unmet_hrs} unmet occupied heating and cooling hours, more than the limit of #{max_unmet_hrs}." if unmet_hrs > max_unmet_hrs
-
+      if unmet_hrs
+        errs << "For #{type_desc} there were #{unmet_hrs} unmet occupied heating and cooling hours, more than the limit of #{max_unmet_hrs}." if unmet_hrs > max_unmet_hrs
+      else
+        errs << "For #{type_desc} could not determine unmet hours; simulation may have failed."
+      end
     end
   
-    assert(errs.size == 0, errs.join(', '))
+    assert(errs.size == 0, errs.join("\n"))
 
     return true
   end
