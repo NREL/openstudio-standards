0a57ecf7f98e9ba4a19fdcd055f869f81ba560ab
phylroy <phylroy.lopez@gmail.com>
2017-05-03 03:25:53 +0000

    added method to determine sha revision for git.

--- openstudio-standards/lib/openstudio-standards/version.rb
+++ openstudio-standards/lib/openstudio-standards/version.rb
@@ -1,3 +1,16 @@
 module OpenstudioStandards
+  def self.git_revision
+    cmd = 'git'
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each { |ext|
+        exe = "#{path}/#{cmd}#{ext}"
+        if File.executable?(exe)
+        return `#{exe} -C #{__dir__} rev-parse --short HEAD` 
+        end
+      }
+    end
+    return 'git-not-found-on-this-system'
+  end
   VERSION = '0.1.13'.freeze
 end
