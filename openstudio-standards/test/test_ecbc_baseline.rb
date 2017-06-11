7f28ab4ffe596d495b6c79914c61402873743482
Subhash <subhash.iiit.h@gmail.com>
2017-06-11 22:12:32 +0530

    Added Daytime - Operation type for ECBC
    Added Daytime - Operation type for ECBC

--- openstudio-standards/test/test_ecbc_baseline.rb
+++ openstudio-standards/test/test_ecbc_baseline.rb
@@ -6,8 +6,8 @@ class BaselineECBCTest < Minitest::Test
   # Create a baseline model for bldg_1
   def test_lpd_bldg1
 
-    model = create_ecbc_baseline_model('bldg_1', 'ECBC 2007', 'ECBC Warm and Humid', 'MediumOffice', nil, false, true)
-   
+    model = create_ecbc_baseline_model('ecbc1', 'ECBC 2007', 'ECBC Warm and Humid', 'MediumOffice', nil, false, true, "Daytime")
+    
     # Assertions about the baseline model go here
    
     # Open Office - Lobby LPD should be 1.3 W/ft2
@@ -17,7 +17,7 @@ class BaselineECBCTest < Minitest::Test
     lpd_w_per_ft2 = OpenStudio.convert(lpd_w_per_m2,'W/m^2','W/ft^2').get
     assert_in_delta(1.3, lpd_w_per_ft2, 0.01, "Open Office Lobby LPD is wrong.") #The measure did this correctly
     
-
   end 
 
 end
+
