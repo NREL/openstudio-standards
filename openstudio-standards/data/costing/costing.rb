6f7b3742f4206a6951e2a5b33b942ca45afa7fe6
phylroy <phylroy.lopez@gmail.com>
2017-08-10 21:36:03 +0000

    added costing algorithms...success it works!

--- openstudio-standards/data/costing/costing.rb
+++ openstudio-standards/data/costing/costing.rb
@@ -7,7 +7,66 @@ require 'aes'
 require 'geocoder'
 
 class CostingDatabase
-
+# A list of the table and fields in the Excel database. Please keep up to date.
+#RSMeansLocations
+# province-state
+# city
+# latitude
+# longitude
+# source
+#RSMeansLocalFactors
+# province-state
+# city
+# division
+# code_prefixes
+# material
+# installation
+# total	source
+#SpaceTypeRules
+# template
+# building_type
+# space_type
+# min_stories
+# max_stories
+# spandrel
+# ext_wall_type
+# ext_floor_type
+# ext_roof_type
+# ext_doors_type
+# ground_contact_wall_type
+# ground_contact_floor_type
+# ground_contact_roof_type
+# ext_fixed_window_type
+# ext_operable_window_type
+# ext_glass_door
+# ext_skylight
+# ext_tubular_domes
+# ext_tubular_diffusers
+#ConstructionsOpaque
+# surface_type
+# construction_type_name
+# author
+# intended_surface_type
+# standards_construction_type
+# type_index
+# zone
+# rsi_k_m2_per_w
+# u_w_per_m2_k
+# description
+# material_opaque_id_layers
+# material_descriptions
+#MaterialsOpaque
+# materials_opaque_id
+# source
+# type
+# material_type
+# catalog_id
+# id
+# description
+# unit
+# quantity
+# material_mult
+# labour_mult	op_mult
 
   def apply_baseline_constructions_based_on_rsi(model)
     #Scan for spacetypes and determine contructions used.
@@ -22,6 +81,8 @@ class CostingDatabase
     end
   end
 
+
+  #Enter in [latitude, logitude] for each loc and this method will return the distance.
   def distance (loc1, loc2)
     rad_per_deg = Math::PI/180 # PI / 180
     rkm = 6371 # Earth radius in kilometers
@@ -39,77 +100,160 @@ class CostingDatabase
   end
 
 
-# Method to obtain the unit costs from the New Construtions library via the RS-means API.
-# You will need to place your secret hash into a file named rs_means_auth in your home folder as ruby sees it.
-# Your hash will need to be updated as your swagger session expires (within an hour) otherwise you will get a
-# 401 not authorized error. The hash_id (the weird long piece of text) is the only thing required in the file.
+  # Method to obtain the unit costs from the New Construtions library via the RS-means API.
+  # You will need to place your secret hash into a file named rs_means_auth in your home folder as ruby sees it.
+  # Your hash will need to be updated as your swagger session expires (within an hour) otherwise you will get a
+  # 401 not authorized error. The hash_id (the weird long piece of text) is the only thing required in the file.
   def get_rsmeans_costs(rs_type, rs_catalog_id, rs_id)
-    puts "Trying #{rs_catalog_id}, #{rs_id}"
-    auth = File.read("#{Dir.home}/rs_means_auth").strip
-    auth = {:Authorization => "bearer #{auth}"}
-    path = "https://dataapi-sb.gordian.com/v1/costdata/#{rs_type.downcase.strip}/catalogs/#{rs_catalog_id.strip}/costlines/#{rs_id.strip}"
-    begin
-      values = JSON.parse(RestClient.get(path, auth).body)
-      #puts JSON.pretty_generate(values)
-      return values
-    rescue Exception => e
-      puts e
-      if e.to_s.strip == "401 Unauthorized"
-        raise("Authenication failed with RSMeans. Ensure you have created your secret hash from the website and saved it in your home folder as rs_means_auth")
-      end
-      @not_found_in_rsmeans_api << " #{rs_catalog_id}, #{rs_id}"
-    end
-    return values
+
   end
 
 
-#This will convert a sheet in a given workbook into an array of hashes with the headers as symbols.
+  #This will convert a sheet in a given workbook into an array of hashes with the headers as symbols.
   def convert_workbook_sheet_to_array_of_hashes(xlsx_path, sheet_name)
     #Load Constructions data sheet from workbook and convert to a csv object.
     data = Roo::Spreadsheet.open(xlsx_path).sheet(sheet_name).to_csv
-    csv = CSV.new(data, {headers: true, header_converters: :symbol})
+    csv = CSV.new(data, {headers: true})
     return csv.to_a.map {|row| row.to_hash}
   end
 
-  def get_costs_for_materials(materials)
-    new_materials = Array.new
-    materials.each do |material|
 
-      material['rs_means_api'] = get_rsmeans_costs(material[:type], material[:catalog_id], material[:id])
-      material['btap_total_cost_op'] = material['rs_means_api']['baseCosts'][:materialOpCost].to_f * material[:material_mult].to_f +
-          material['rs_means_api']['baseCosts'][:labourOpCost].to_f * material[:labour_mult].to_f +
-          material['rs_means_api']['baseCosts'][:equipmentOpCost].to_f
+  def get_costing_for_constructions_for_all_regions()
 
-      material['btap_total_cost'] = material['rs_means_api']['baseCosts'][:materialCost].to_f * material[:material_mult].to_f +
-          material['rs_means_api']['baseCosts'][:labourCost].to_f * material[:labour_mult].to_f +
-          material['rs_means_api']['baseCosts'][:equipmentCost].to_f
-      new_materials << material
+
+    @costing_database = JSON.parse(File.read('costing.json'))
+    @costing_database['constructions_costs']= Array.new
+    counter = 0
+    @costing_database['raw']['RSMeansLocations'].each do |location|
+      puts location
+      @costing_database["raw"]['ConstructionsOpaque'].each do |construction|
+        #puts "Getting cost for Construction type #{construction["construction_type_name"]} at RSI #{construction['rsi_k_m2_per_w']}"
+        total_with_op = 0.0
+        materials_string = ''
+        construction['material_opaque_id_layers'].split(',').reject {|c| c.empty?}.each do |material_index|
+
+          material = @costing_database["raw"]['MaterialsOpaque'].find {|material| material['materials_opaque_id'].to_s == material_index.to_s}
+          if material.nil?
+            puts "material error..could not find material #{material_index} in #{@costing_database["raw"]['MaterialsOpaque']}"
+            raise()
+          else
+            rs_means_data = @costing_database['rsmean_api_data'].select {|data| data['id'].to_s == material['id']}.first
+            if rs_means_data.nil?
+              #puts "This material id #{material['id']} was not found in the rs-means api. Skipping. This construction will be inaccurate. "
+              next
+            else
+              regional_material, regional_installation = get_regional_cost_factors(location['province-state'], location['city'], material)
+              #Get RSMeans cost information from lookup.
+              material_cost = rs_means_data['baseCosts']['materialOpCost'].to_f * material['quantity'].to_f * material['material_mult'].to_f
+              labour_cost = rs_means_data['baseCosts']['labourOpCost'].to_f * material['labour_mult'].to_f
+              equipment_cost = rs_means_data['baseCosts']['equipmentOpCost'].to_f
+              total_with_op += (material_cost * regional_material / 100.0) + (labour_cost * regional_installation / 100.0) + equipment_cost
+              materials_string += "\n#{material['description']}"
+            end
+          end
+        end
+        new_construction = {'province-state' => location['province-state'],
+                            'city' => location['city'],
+                            "construction_type_name" => construction["construction_type_name"],
+                            'intended_surface_type'	=> construction["intended_surface_type"],
+                            'standards_construction_type' => construction["standards_construction_type"],
+                            'rsi_k_m2_per_w ' => construction['rsi_k_m2_per_w'].to_f,
+                            'zone' => construction['zone'],
+                            'materials_string' => materials_string,
+                            'total_with_op' => total_with_op }
+
+
+        @costing_database['constructions_costs'] << new_construction
+        counter += 1
+      end
     end
-    return new_materials
+    puts counter
+    #For debugging
+    File.open("costing.json", "w") do |f|
+      f.write(JSON.pretty_generate(@costing_database))
+    end
+    File.open("just_costing.json", "w") do |f|
+      f.write(JSON.pretty_generate(@costing_database['constructions_costs']))
+    end
+
+
+  end
+
+  def get_regional_cost_factors(provincestate, city, material)
+    @costing_database['raw']['RSMeansLocalFactors'].select {|code| code['province-state'] == provincestate and code['city'] == city}.each do |code|
+      id = material['id'].to_s
+      prefixes = code["code_prefixes"].split(',')
+      prefixes.each do |prefix|
+        # puts " #{id} == #{prefix}"
+        if id.start_with?(prefix.strip)
+          return code["material"].to_f, code["installation"].to_f
+        end
+      end
+    end
+    raise("Could not find regional adjustment factor for rs-means material #{material['id']}")
   end
 
 
-  def generate_encrypted_costing_database()
+  def generate_encrypted_materials_database()
     @not_found_in_rsmeans_api = Array.new
     @costing_database = Hash.new()
-# Path to the xlsx file
+    # Path to the xlsx file
     xlsx_path = "#{File.dirname(__FILE__)}/national_average_cost_information.xlsm"
 
+    #Get Raw Data from files.
+    @costing_database['raw'] = {}
+    @costing_database['rs_mean_errors']=[]
+    @costing_database['raw']['SpaceTypeRules'] = convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'SpaceTypeRules')
+    @costing_database['raw']['RSMeansLocations'] = convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'RSMeansLocations')
+    @costing_database['raw']['RSMeansLocalFactors'] = convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'RSMeansLocalFactors')
+    @costing_database['raw']['MaterialsOpaque'] = convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'MaterialsOpaque')
+    @costing_database['raw']['ConstructionsOpaque'] = convert_workbook_sheet_to_array_of_hashes("#{File.dirname(__FILE__)}/national_average_cost_information.xlsm", 'ConstructionsOpaque')
+    @costing_database['rsmean_api_data']= Array.new
+    @costing_database['constructions_costs']= Array.new
+
+    #Get RSMeans Materials data and store errors if encountered
+    [@costing_database['raw']['MaterialsOpaque']].each do |materials|
+      lookup_list = materials.map {|material| {'type' => material['type'], 'catalog_id' => material['catalog_id'], 'id' => material['id']}}.uniq
+      start = Time.now
+      lookup_list.each do |material|
+
+        puts "Trying to look up #{material}"
+        auth = File.read("#{Dir.home}/rs_means_auth").strip
+        auth = {:Authorization => "bearer #{auth}"}
+        path = "https://dataapi-sb.gordian.com/v1/costdata/#{material['type'].downcase.strip}/catalogs/#{material['catalog_id'].strip}/costlines/#{material['id'].strip}"
+        value = nil
+        begin
+          api_return = JSON.parse(RestClient.get(path, auth).body)
+          JSON.pretty_generate(api_return)
+          puts api_return
+          @costing_database['rsmean_api_data'] << api_return
+        rescue Exception => e
+          puts e
+          if e.to_s.strip == "401 Unauthorized"
+            raise("Authenication failed with RSMeans. Ensure you have created your secret hash from the website and saved it in your home folder as rs_means_auth")
+          elsif e.to_s.strip == "404 Not Found"
+            material['error'] = e
+            @costing_database['rs_mean_errors'] << material
+          else
+            raise("Unknown Error Occured #{e}")
+          end
+        end
+      end
+      puts "Elapsed time in sec #{Time.now - start}"
+    end
 
 
-    @costing_database[:constructions_opaque] = convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'constructions-opaque')
-    @costing_database[:materials_opaque] = get_costs_for_materials(convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'materials-opaque'))
-    @costing_database[:materials_glazing] = get_costs_for_materials(convert_workbook_sheet_to_array_of_hashes(xlsx_path, 'materials-glazing'))
-
-
-        key = AES.key
-#Write public cost information to a json file. This will be used by the standards and measures. To create
-#create the openstudio construction names and costing objects.
+    key = AES.key
+    #Write public cost information to a json file. This will be used by the standards and measures. To create
+    #create the openstudio construction names and costing objects.
     File.open("costing_e.json", "w") do |f|
       f.write(encrypt_hash(key, @costing_database))
     end
+    #For debugging
+    File.open("costing.json", "w") do |f|
+      f.write(JSON.pretty_generate(@costing_database))
+    end
     puts "the decryption key is:#{key}"
-
   end
 
 
@@ -124,7 +268,7 @@ class CostingDatabase
       puts "Could not decrypt string, perhaps key is invalid? #{detail}"
     end
   end
-
 end
 
-CostingDatabase.new.generate_encrypted_costing_database()
+#CostingDatabase.new.generate_encrypted_materials_database()
+CostingDatabase.new.get_costing_for_constructions_for_all_regions()
