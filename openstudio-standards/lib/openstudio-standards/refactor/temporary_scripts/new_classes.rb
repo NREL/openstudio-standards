3d704bd081c5bc533113717cd6281ec56851a712
Andrew Parker <Andrew.Parker@nrel.gov>
2017-11-09 20:01:17 -0700

    Removes temporary scripts used during refactor

--- openstudio-standards/lib/openstudio-standards/refactor/temporary_scripts/new_classes.rb
+++
@@ -1,7 +0,0 @@
-class A90_1_2004_Model < A90_1_Model
-class A90_1_2007_Model < A90_1_Model
-class A90_1_2010_Model < A90_1_Model
-class A90_1_2013_Model < A90_1_Model
-class DOERef1980_2004_Model < A90_1_Model
-class DOERefPre1980_Model < A90_1_Model
-class NRELZNEReady2017_Model < A90_1_Model
