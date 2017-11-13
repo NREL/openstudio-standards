c59644eb61d77f3a1e9787b372a056dbd0407784
Andrew Parker <Andrew.Parker@nrel.gov>
2017-11-13 14:39:57 -0700

    Re-implements Xcel EDA system type customization

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2013/ashrae_90_1_2013.Model.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2013/ashrae_90_1_2013.Model.rb
@@ -20,43 +20,66 @@ class A90_1_2010_Model < A90_1_Model
   # 5_or_6, 7_or_8, 9_or_10
   def model_prm_baseline_system_number(model, climate_zone, area_type, fuel_type, area_ft2, num_stories, custom)
     sys_num = nil
-    
-    # TODO refactor: figure out this weird template switching case
-    # For a custom scenario, use the lookup method from
-    # a different standard instead of the specified standard.
+
     # Customization - Xcel EDA Program Manual 2014
     # Table 3.2.2 Baseline HVAC System Types
-    # if custom == 'Xcel Energy CO EDA'
-      # instvartemplate = '90.1-2010'
-      # OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.Model', 'Custom; per Xcel EDA Program Manual 2014 Table 3.2.2 Baseline HVAC System Types, the 90.1-2010 lookup for HVAC system types shall be used.')
-      # sys_num = model_prm_baseline_system_number(model, climate_zone, area_type, fuel_type, area_ft2, num_stories, custom)
-      # return sys_num
-    # end
-    
-    # Set the area limit
-    limit_ft2 = 25_000
+    if custom == 'Xcel Energy CO EDA'
+      OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.Model', 'Custom; per Xcel EDA Program Manual 2014 Table 3.2.2 Baseline HVAC System Types, the 90.1-2010 lookup for HVAC system types shall be used.')
 
-    case area_type
-    when 'residential'
-      sys_num = '1_or_2'
-    when 'nonresidential'
-      # nonresidential and 3 floors or less and <25,000 ft2
-      if num_stories <= 3 && area_ft2 < limit_ft2
-        sys_num = '3_or_4'
-      # nonresidential and 4 or 5 floors or 5 floors or less and 25,000 ft2 to 150,000 ft2
-      elsif ((num_stories == 4 || num_stories == 5) && area_ft2 < limit_ft2) || (num_stories <= 5 && (area_ft2 >= limit_ft2 && area_ft2 <= 150_000))
-        sys_num = '5_or_6'
-      # nonresidential and more than 5 floors or >150,000 ft2
-      elsif num_stories >= 5 || area_ft2 > 150_000
-        sys_num = '7_or_8'
+      # Set the area limit
+      limit_ft2 = 25_000
+
+      case area_type
+        when 'residential'
+          sys_num = '1_or_2'
+        when 'nonresidential'
+          # nonresidential and 3 floors or less and <25,000 ft2
+          if num_stories <= 3 && area_ft2 < limit_ft2
+            sys_num = '3_or_4'
+            # nonresidential and 4 or 5 floors or 5 floors or less and 25,000 ft2 to 150,000 ft2
+          elsif ((num_stories == 4 || num_stories == 5) && area_ft2 < limit_ft2) || (num_stories <= 5 && (area_ft2 >= limit_ft2 && area_ft2 <= 150_000))
+            sys_num = '5_or_6'
+            # nonresidential and more than 5 floors or >150,000 ft2
+          elsif num_stories >= 5 || area_ft2 > 150_000
+            sys_num = '7_or_8'
+          end
+        when 'heatedonly'
+          sys_num = '9_or_10'
+        when 'retail'
+          # Should only be hit by Xcel EDA
+          sys_num = '3_or_4'
       end
-    when 'heatedonly'
-      sys_num = '9_or_10'
-    when 'retail'
-      sys_num = '3_or_4'
-    end
+
+      return sys_num
+
+    else
     
-    return sys_num
+      # Set the area limit
+      limit_ft2 = 25_000
+
+      case area_type
+      when 'residential'
+        sys_num = '1_or_2'
+      when 'nonresidential'
+        # nonresidential and 3 floors or less and <25,000 ft2
+        if num_stories <= 3 && area_ft2 < limit_ft2
+          sys_num = '3_or_4'
+        # nonresidential and 4 or 5 floors or 5 floors or less and 25,000 ft2 to 150,000 ft2
+        elsif ((num_stories == 4 || num_stories == 5) && area_ft2 < limit_ft2) || (num_stories <= 5 && (area_ft2 >= limit_ft2 && area_ft2 <= 150_000))
+          sys_num = '5_or_6'
+        # nonresidential and more than 5 floors or >150,000 ft2
+        elsif num_stories >= 5 || area_ft2 > 150_000
+          sys_num = '7_or_8'
+        end
+      when 'heatedonly'
+        sys_num = '9_or_10'
+      when 'retail'
+        sys_num = '3_or_4'
+      end
+
+      return sys_num
+
+    end
   end
   
   # Change the fuel type based on climate zone, depending on the standard.
