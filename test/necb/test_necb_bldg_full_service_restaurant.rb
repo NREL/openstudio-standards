a4f86ccf9260d7da2be6dd9a0ab748bb6afedb9f
phylroy <phylroy.lopez@gmail.com>
2018-02-06 13:59:50 +0000

    changed format of test for buildings for FSR.

--- test/necb/test_necb_bldg_full_service_restaurant.rb
+++ test/necb/test_necb_bldg_full_service_restaurant.rb
@@ -4,8 +4,13 @@ require_relative '../helpers/compare_models_helper'
 require_relative './regression_helper'
 
 class TestNECBFullServiceRestaurant < Minitest::Test
-  create_model_and_regression_test('FullServiceRestaurant',
-                                   ['CAN_AB_Calgary.Intl.AP.718770_CWEC2016.epw'],
-                                   ['NECB 2011']
-  )
+
+  def test_necb2011_full_service_restaurant()
+    result, msg = create_model_and_regression_test('FullServiceRestaurant',
+                                                   'CAN_AB_Calgary.Intl.AP.718770_CWEC2016.epw',
+                                                   'NECB 2011'
+    )
+    assert(result, msg)
+  end
+
 end
