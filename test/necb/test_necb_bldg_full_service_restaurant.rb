01132ad96167bbb1b794feb1e724e71000a96ca1
Phylroy Lopez <phylroy.lopez@gmail.com>
2018-02-06 19:57:11 -0500

    Nrcan regression (#300)
    * Modified test for regression work.
    
    * Added regresssion test files.
    
    * Added regression test files.
    
    * changed format of test for buildings for FSR.
    
    * added regression files for tyson to complete.
    
    * regression testing
    
    * Regression testing
    
    * Added remaining regression expected result osm files.
    
    * added error handling to entire test

--- test/necb/test_necb_bldg_full_service_restaurant.rb
+++ test/necb/test_necb_bldg_full_service_restaurant.rb
@@ -1,19 +1,29 @@
 require_relative '../helpers/minitest_helper'
 require_relative '../helpers/create_doe_prototype_helper'
+require_relative '../helpers/compare_models_helper'
+require_relative './regression_helper'
 
-##FullServiceRestaurant
-class TestNECBFullServiceRestaurant < CreateDOEPrototypeBuildingTest
-  building_types = ['FullServiceRestaurant']
-  templates = [ 'NECB 2011']
-  climate_zones = ['NECB HDD Method']
-  epw_files = [
-      'CAN_AB_Calgary.Intl.AP.718770_CWEC2016.epw'
-] 
-  create_models = true
-  run_models = false
-  compare_results = false
-  debug = false
-  TestNECBFullServiceRestaurant.create_run_model_tests(building_types, templates, climate_zones, epw_files, create_models, run_models, compare_results, debug)
+class TestNECBFullServiceRestaurant < Minitest::Test
+
+  def setup()
+    @building_type = 'FullServiceRestaurant'
+    @gas_location = 'CAN_AB_Calgary.Intl.AP.718770_CWEC2016.epw'
+    @electric_location = 'CAN_QC_Kuujjuaq.AP.719060_CWEC2016.epw'
+  end
+
+  def test_regression_natural_gas()
+    result, msg = create_model_and_regression_test(@building_type,
+                                                   @gas_location,
+                                                   'NECB 2011'
+    )
+    assert(result, msg)
+  end
+
+  def test_regression_electric()
+    result, msg = create_model_and_regression_test(@building_type,
+                                                   @electric_location,
+                                                   'NECB 2011'
+    )
+    assert(result, msg)
+  end
 end
-
-
--- test/necb/test_necb_bldg_midrise_apartment_office.rb
+++ test/necb/test_necb_bldg_midrise_apartment_office.rb
@@ -1,24 +1,31 @@
 require_relative '../helpers/minitest_helper'
 require_relative '../helpers/create_doe_prototype_helper'
+require_relative '../helpers/compare_models_helper'
+require_relative './regression_helper'
 
+class TestNECBMidriseApartment < Minitest::Test
 
+  def setup()
+    @building_type = 'MidriseApartment'
+    @gas_location = 'CAN_AB_Calgary.Intl.AP.718770_CWEC2016.epw'
+    @electric_location = 'CAN_QC_Kuujjuaq.AP.719060_CWEC2016.epw'
+  end
 
+  def test_regression_natural_gas()
+    result, msg = create_model_and_regression_test(@building_type,
+                                                   @gas_location,
+                                                   'NECB 2011'
+    )
+    assert(result, msg)
+  end
 
-
-
-#MediumOffice
-class TestNECBMidriseApartment < CreateDOEPrototypeBuildingTest
-  building_types = ['MidriseApartment']
-
-  templates = [ 'NECB 2011']
-  climate_zones = ['NECB HDD Method']
-  epw_files = [
-      'CAN_AB_Calgary.Intl.AP.718770_CWEC2016.epw'
-  ]
-  create_models = true
-  run_models = false
-  compare_results = false
-  debug = false
-  TestNECBMidriseApartment.create_run_model_tests(building_types, templates, climate_zones, epw_files, create_models, run_models, compare_results, debug)
-  # TestNECBMidriseApartment.compare_test_results(building_types, templates, climate_zones, file_ext="")
+  def test_regression_electric()
+    result, msg = create_model_and_regression_test(@building_type,
+                                                   @electric_location,
+                                                   'NECB 2011'
+    )
+    assert(result, msg)
+  end
 end
+
+
