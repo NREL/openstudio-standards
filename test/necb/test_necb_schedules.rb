53388f5bc4b6a20b4c1fc61aaa0cf20e11150c5e
Chris Kirney <chris.kirney@canada.ca>
2018-05-24 15:53:37 +0000

    Adding reference file to compare against and code which includes testing for occupancy, lighting, electrical equipment, and SHW schedules.

--- test/necb/test_necb_schedules.rb
+++ test/necb/test_necb_schedules.rb
@@ -18,10 +18,8 @@ class NECB2011ScheduleTests < Minitest::Test
     #Create only above ground geometry (Used for infiltration tests) 
     length = 100.0; width = 100.0 ; num_above_ground_floors = 1; num_under_ground_floors = 0; floor_to_floor_height = 3.8 ; plenum_height = 1; perimeter_zone_depth = 4.57; initial_height = 10.0
     BTAP::Geometry::Wizards::create_shape_rectangle(@model,length, width, num_above_ground_floors,num_under_ground_floors, floor_to_floor_height, plenum_height,perimeter_zone_depth, initial_height )
-#    standard = Standard.build('NECB2015')
 
-    header_output = ""
-    output = ""
+    output_array = []
     #Iterate through all spacetypes/buildingtypes. 
     Templates.each do |template|
       #Get spacetypes from googledoc.
@@ -32,7 +30,6 @@ class NECB2011ScheduleTests < Minitest::Test
       }
       # lookup space type properties
       standard.model_find_objects(standard.standards_data["space_types"], search_criteria).each do |space_type_properties|
-        header_output = ""
         # Create a space type
         st = OpenStudio::Model::SpaceType.new(@model)
         st.setStandardsBuildingType(space_type_properties['building_type'])
@@ -53,35 +50,86 @@ class NECB2011ScheduleTests < Minitest::Test
         space = @model.getSpaces[0]
         space_area = space.floorArea #m2
 
+        space_sched_array = []
+        header_info = {
+            "Space_Type" => st.name,
+            "StandardsSpaceType" => st.standardsSpaceType.get,
+            "StandardsBuildingType" => st.standardsBuildingType.get
+        }
+
+        unless header_info.nil?
+          space_sched_array << header_info
+        end
+
         #People / Occupancy
+        occ_scheds = []
         total_occ_dens = []
         occ_sched = []
         st.people.each {|people_def| total_occ_dens << people_def.peoplePerFloorArea ; occ_sched << people_def.numberofPeopleSchedule.get}
         assert(total_occ_dens.size <= 1 , "#{total_occ_dens.size} people definitions given. Expecting <= 1.")
 
-        occ_array = []
         unless occ_sched[0].nil?
-          occ_sched[0].to_ScheduleRuleset.get.scheduleRules.each do |occ_day|
+          occ_sched[0].to_ScheduleRuleset.get.scheduleRules.sort.each do |occ_day|
             sched_entry = {
-                "schedule_name" => occ_day.daySchedule.name.get,
-                "schedule_times" => occ_day.daySchedule.times,
-                "schedule_values" => occ_day.daySchedule.values
+                "ScheduleName" => occ_day.daySchedule.name.get,
+                "ScheduleTimes" => occ_day.daySchedule.times,
+                "ScheduleValues" => occ_day.daySchedule.values
             }
-            occ_array << sched_entry
+            occ_scheds << sched_entry
           end
+          occ_entry = {
+              "ScheduleType" => occ_sched[0].name.get,
+              "Schedules" => occ_scheds
+          }
+          space_sched_array << occ_entry
         end
 
         #Lights
+        lpd_scheds = []
         lpd_sched = []
-        st.lights.each {|light| lpd_sched << light.schedule.get.name}
+        st.lights.each {|light| lpd_sched << light.schedule.get}
         assert(lpd_sched.size <= 1 , "#{lpd_sched.size} light definitions given. Expecting <= 1.")
 
+        unless lpd_sched[0].nil?
+          lpd_sched[0].to_ScheduleRuleset.get.scheduleRules.sort.each do |lpd_day|
+            sched_entry = {
+                "ScheduleName" => lpd_day.daySchedule.name.get,
+                "ScheduleTimes" => lpd_day.daySchedule.times,
+                "ScheduleValues" => lpd_day.daySchedule.values
+            }
+            lpd_scheds << sched_entry
+          end
+          lpd_entry = {
+              "ScheduleType" => lpd_sched[0].name.get,
+              "Schedules" => lpd_scheds
+          }
+          space_sched_array << lpd_entry
+        end
+
         #Equipment -Electric
+        elec_equip_scheds = []
         elec_equip_sched = []
-        st.electricEquipment.each {|elec_equip| elec_equip_sched << elec_equip.schedule.get.name}
+        st.electricEquipment.each {|elec_equip| elec_equip_sched << elec_equip.schedule.get}
         assert( elec_equip_sched.size <= 1 , "#{elec_equip_sched.size} electric definitions given. Expecting <= 1." )
 
+        unless elec_equip_sched[0].nil?
+          elec_equip_sched[0].to_ScheduleRuleset.get.scheduleRules.sort.each do |elec_day|
+            sched_entry = {
+                "ScheduleName" => elec_day.daySchedule.name.get,
+                "ScheduleTimes" => elec_day.daySchedule.times,
+                "ScheduleValues" => elec_day.daySchedule.values
+            }
+            elec_equip_scheds << sched_entry
+          end
+          elec_equip_entry = {
+              "ScheduleType" => elec_equip_sched[0].name.get,
+              "Schedules" => elec_equip_scheds
+          }
+          space_sched_array << elec_equip_entry
+        end
+
         #Hot Water Equipment
+        shw_scheds = []
         hw_equip_power = []
         hw_equip_sched = []
         st.hotWaterEquipment.each {|equip| hw_equip_power << equip.powerPerFloorArea.get ; hw_equip_sched << equip.schedule.get.name}
@@ -95,279 +143,42 @@ class NECB2011ScheduleTests < Minitest::Test
         area_per_occ = 0.0
         area_per_occ = 1/total_occ_dens[0].to_f unless total_occ_dens[0].nil?
         water_fixture = standard.model_add_swh_end_uses_by_space(@model, st.standardsBuildingType.get, 'NECB HDD Method', shw_loop, st.standardsSpaceType.get, space.name.get)
-        if water_fixture.nil?
-          shw_watts_per_person = 0.0
-          shw__fraction_schedule = 0.0
-          shw_target_temperature_schedule = "NA"
-        else
+        unless water_fixture.nil?
           shw__fraction_schedule = water_fixture.flowRateFractionSchedule.get.name
-          shw_fraction_schedule_rules = water_fixture.flowRateFractionSchedule.get.to_ScheduleRuleset.get.scheduleRules
-          shw_peak_flow = water_fixture.waterUseEquipmentDefinition.getPeakFlowRate.value # m3/s
-          shw_peak_flow_per_area = shw_peak_flow / space_area #m3/s/m2
-          # # Watt per person =             m3/s/m3        * 1000W/kW * (specific heat * dT) * m2/person
-          shw_watts_per_person = shw_peak_flow_per_area * 1000 * (4.19 * 44.4) * 1000 * area_per_occ
-          shw_target_temperature_schedule = water_fixture.waterUseEquipmentDefinition.targetTemperatureSchedule.get.to_ScheduleRuleset.get.defaultDaySchedule.values
-        end
-
-        occ_sched = st.defaultScheduleSet.get.numberofPeopleSchedule.get
-        scheds = @model.getScheduleRulesets
-        occ_sched_ruleset = nil
-        scheds.each do |sched|
-          if sched.name.get.to_s == spacetype_occ_sched.name.get.to_s
-            sched.scheduleRules.each do |sched_day|
-              sched_times = []
-              sched_values = []
-              sched_times = sched_day.daySchedule.times
-              sched_values = sched_day.daySchedule.values
-              puts "Schedule!"
-            end
+          water_fixture.flowRateFractionSchedule.get.to_ScheduleRuleset.get.scheduleRules.sort.each do |shw_day|
+            sched_entry = {
+                "ScheduleName" => shw_day.daySchedule.name.get,
+                "ScheduleTimes" => shw_day.daySchedule.times,
+                "ScheduleValues" => shw_day.daySchedule.values
+            }
+            shw_scheds << sched_entry
           end
-        end
-=begin
-        sched_times = []
-        sched_values = []
-        sched_day = occ_sched_ruleset[0].daySchedule
-        sched_values = sched_day.values
-        sched_times = sched_day.times
-=end
-
-        #        spacetype_occ_sched_name = st.defaultScheduleSet.get.numberofPeopleSchedule.get.name
-#        spacetype_occ_sched = st.defaultScheduleSet.get.numberofPeopleSchedule.get
-#        puts spacetype_occ_sched
-#        if occ_sched_ruleset == spacetype_occ_sched
-#          sched_rules_2 = occ_sched_ruleset.scheduleRules
-#          sched_rules = spacetype_occ_sched.scheduleRule.get
-#          puts "Why doesn't this work!??!!"
-#        end
-#        spcaetype_occ_scheddays = st.defaultScheduleSet.get.numberofPeopleSchedule.get.scheduleRulesets
-#        spacetype_occ_scheddays = spacetype_occ_sched.scheduleRules.get
-
-=begin
-        scheds = @model.getSchedules
-        @model.getSchedules.each do |sched|
-          schedname = sched.name.get
-          puts "Schedule name = #{schedname}, looking for #{spacetype_occ_sched}"
-          if sched.name.get.to_s == spacetype_occ_sched.to_s
-            schedday = sched.scheduleTypeLimits.get
-            schedday.each do |sched_limits|
-              sched_limits_name = sched_limits.name
-            end
-            puts "Worked?"
-          end
-        end
-        puts spacetype_occ_sched
-
-        #        spacetype_lighting_sched = defaultScheduleSet.lightingSchedule.get.name
-=end
-
-        puts "Got this far!"
-
-=begin
-        #Lights
-        total_lpd = []
-        lpd_sched = []
-        occSensLPDfactor = 1.0
-        if template == "NECB2011"
-          # NECB2011 space types that require a reduction in the LPD to account for
-          # the requirement of an occupancy sensor (8.4.4.6(3) and 4.2.2.2(2))
-          reduceLPDSpaces = ["Classroom/lecture/training", "Conf./meet./multi-purpose", "Lounge/recreation",
-            "Conf./meet./multi-purpose", "Washroom-sch-A",
-            "Washroom-sch-B", "Washroom-sch-C", "Washroom-sch-D", "Washroom-sch-E", "Washroom-sch-F", "Washroom-sch-G",
-            "Washroom-sch-H", "Washroom-sch-I", "Dress./fitt. - performance arts", "Locker room", "Retail - dressing/fitting"]
-          space_type_name = st.standardsSpaceType.get
-          if reduceLPDSpaces.include?(space_type_name)
-            occSensLPDfactor = 0.9
-          elsif ( (space_type_name=='Storage area' && space_area < 100) || 
-               (space_type_name=='Storage area - refrigerated' && space_area < 100) || 
-               (space_type_name=='Office - enclosed' && space_area < 25) )
-            # Do nothing! In this case, we use the duplicate space type name appended with " - occsens"!
-          end
-        end
-        st.lights.each {|light| total_lpd << light.powerPerFloorArea.get * occSensLPDfactor ; lpd_sched << light.schedule.get.name}
-        assert(total_lpd.size <= 1 , "#{total_lpd.size} light definitions given. Expecting <= 1.")
-        
-        #People / Occupancy
-        total_occ_dens = []
-        occ_sched = []
-        st.people.each {|people_def| total_occ_dens << people_def.peoplePerFloorArea ; occ_sched << people_def.numberofPeopleSchedule.get.name}
-        assert(total_occ_dens.size <= 1 , "#{total_occ_dens.size} people definitions given. Expecting <= 1.")
-
-        #Equipment -Gas
-        gas_equip_power = []
-        gas_equip_sched = []
-        st.gasEquipment.each {|gas_equip| gas_equip_power << gas_equip.powerPerFloorArea.get ; gas_equip_sched << gas_equip.schedule.get.name}
-        assert( gas_equip_power.size <= 1 , "#{gas_equip_power.size} gas definitions given. Expecting <= 1." ) 
-        
-        #Equipment -Electric
-        elec_equip_power = []
-        elec_equip_sched = []
-        st.electricEquipment.each {|elec_equip| elec_equip_power << elec_equip.powerPerFloorArea.get ; elec_equip_sched << elec_equip.schedule.get.name}
-        assert( elec_equip_power.size <= 1 , "#{elec_equip_power.size} electric definitions given. Expecting <= 1." ) 
-        
-        #Equipment - Steam
-        steam_equip_power = []
-        steam_equip_sched = []
-        st.steamEquipment.each {|steam_equip| steam_equip_power << steam_equip.powerPerFloorArea.get ; steam_equip_sched << steam_equip.schedule.get.name}
-        assert( steam_equip_power.size <= 1 , "#{steam_equip_power.size} steam definitions given. Expecting <= 1." ) 
-        
-        #Hot Water Equipment
-        hw_equip_power = []
-        hw_equip_sched = []
-        st.hotWaterEquipment.each {|equip| hw_equip_power << equip.powerPerFloorArea.get ; hw_equip_sched << equip.schedule.get.name}
-        assert( hw_equip_power.size <= 1 , "#{hw_equip_power.size} hw definitions given. Expecting <= 1." ) 
-        
-        #Other Equipment
-        other_equip_power = []
-        other_equip_sched = []
-        st.otherEquipment.each {|equip| other_equip_power << equip.powerPerFloorArea.get ; other_equip_sched << equip.schedule.get.name}
-        assert( other_equip_power.size <= 1 , "#{other_equip_power.size} other equipment definitions given. Expecting <= 1." ) 
-            
-        #SHW
-        shw_loop = OpenStudio::Model::PlantLoop.new(@model)
-        shw_peak_flow_per_area = []
-        shw_heating_target_temperature = []
-        shw__schedule = ""
-        area_per_occ = 0.0
-        area_per_occ = 1/total_occ_dens[0].to_f unless total_occ_dens[0].nil?
-        water_fixture = standard.model_add_swh_end_uses_by_space(@model, st.standardsBuildingType.get, 'NECB HDD Method', shw_loop, st.standardsSpaceType.get, space.name.get)
-        if water_fixture.nil?
-          shw_watts_per_person = 0.0
-          shw__fraction_schedule = 0.0
-          shw_target_temperature_schedule = "NA"
-        else
-          shw__fraction_schedule = water_fixture.flowRateFractionSchedule.get.name
-          shw_peak_flow = water_fixture.waterUseEquipmentDefinition.getPeakFlowRate.value # m3/s
-          shw_peak_flow_per_area = shw_peak_flow / space_area #m3/s/m2
-          # # Watt per person =             m3/s/m3        * 1000W/kW * (specific heat * dT) * m2/person
-          shw_watts_per_person = shw_peak_flow_per_area * 1000 * (4.19 * 44.4) * 1000 * area_per_occ
-          shw_target_temperature_schedule = water_fixture.waterUseEquipmentDefinition.targetTemperatureSchedule.get.to_ScheduleRuleset.get.defaultDaySchedule.values
+          shw_entry = {
+              "ScheduleType" => shw__fraction_schedule,
+              "Schedules" => shw_scheds
+          }
+          space_sched_array << shw_entry
         end
 
-        header_output << "SpaceType,"
-        output << "#{st.name},"
-        #standardsSpaceType
-        header_output << "StandardsSpaceType,"
-        output << "#{st.standardsSpaceType.get},"
-        #standardsBuildingType
-        header_output << "standardsBuildingType,"
-        output << "#{st.standardsBuildingType.get},"
-          
-        #lights
-        if total_lpd[0].nil?
-          total_lpd[0] = 0.0
-          lpd_sched[0] = "NA"
+        unless space_sched_array.empty?
+          output_array << space_sched_array
         end
-        header_output << "Lighting Power Density (W/m2),"
-        output << "#{total_lpd[0].round(4)},"
-        header_output << "Lighting Schedule,"
-        output << "#{lpd_sched[0]},"
-        
-        #people
-        if total_occ_dens[0].nil?
-          total_occ_dens[0] = 0.0
-          occ_sched[0] = "NA"
-        else
-          total_occ_dens[0] = 1/total_occ_dens[0].to_f
-        end
-        header_output << "Occupancy Density (m2/person),"
-        output << "#{total_occ_dens[0].round(4)},"
-        header_output << "Occupancy Schedule Name,"
-        output << "#{occ_sched[0]},"
-  
-        #equipment - Elec
-        if elec_equip_power[0].nil?
-          elec_equip_power[0] = 0.0
-          elec_equip_sched[0] = "NA"
-        end
-        header_output << "Elec Equip Power Density (W/m2),"
-        output << "#{elec_equip_power[0].round(4)},"
-        header_output << "Elec Equip Schedule,"
-        output << "#{elec_equip_sched[0]}," 
-        
-        #equipment - Gas
-        if gas_equip_power[0].nil?
-          gas_equip_power[0] = 0.0
-          gas_equip_sched[0] = "NA"
-        end
-        header_output << "Gas Equip Power Density (W/m2),"
-        output << "#{gas_equip_power[0].round(4)},"
-        header_output << "Gas Equip Schedule Name,"
-        output << "#{gas_equip_sched[0]}," 
-        
-        #equipment - steam
-        if steam_equip_power[0].nil?
-          steam_equip_power[0] = 0.0
-          steam_equip_sched[0] = "NA"
-        end
-        header_output << "Steam Equip Power Density (W/m2),"
-        output << "#{steam_equip_power[0].round(4)},"
-        header_output << "Steam Equip Schedule,"
-        output << "#{steam_equip_sched[0]},"
-        
-        #equipment - hot water
-        if hw_equip_power[0].nil?
-          hw_equip_power[0] = 0.0
-          hw_equip_sched[0] = "NA"
-        end
-        header_output << "HW Equip Power Density (W/m2),"
-        output << "#{hw_equip_power[0].round(4)},"
-        header_output << "HW Equip Schedule,"
-        output << "#{hw_equip_sched[0]},"
-            
-        #SHW
-        header_output << "SHW Watt/Person (W/person),"
-        output << "#{shw_watts_per_person.round(0)},"
-        header_output << "SHW Fraction Schedule,"
-        output << "#{shw__fraction_schedule},"
-        header_output << "SHW Temperature Setpoint Schedule Values (C),"
-        output << "#{shw_target_temperature_schedule},"
-          
-  
-        #Outdoor Air / Ventilation
-        dsoa = st.designSpecificationOutdoorAir.get
-        header_output << "outdoorAirMethod,"         
-        output << "#{dsoa.outdoorAirMethod },"
-        header_output << "OutdoorAirFlowperFloorArea (#{dsoa.getOutdoorAirFlowperFloorArea.units.print}) ,"
-        output << "#{dsoa.getOutdoorAirFlowperFloorArea.value.round(4)},"
-          
-        header_output << "OutdoorAirFlowperPerson  (#{dsoa.getOutdoorAirFlowperPerson.units.print}) ,"
-        output << "#{dsoa.getOutdoorAirFlowperPerson.value.round(4)},"
-          
-        header_output << "OutdoorAirFlowRate (#{dsoa.getOutdoorAirFlowRate.units.print}) ,"
-        output << "#{dsoa.getOutdoorAirFlowRate.value.round(4)},"
-          
-        header_output << "OutdoorAirFlowAirChangesperHour (#{dsoa.getOutdoorAirFlowAirChangesperHour.units.print}) ,"
-        output << "#{dsoa.getOutdoorAirFlowAirChangesperHour.value.round(4)},"
-          
-        header_output << "outdoorAirFlowRateFractionSchedule,"
-        if dsoa.outdoorAirFlowRateFractionSchedule.empty?
-          output << "NA,"
-        else
-          output << "#{dsoa.outdoorAirFlowRateFractionSchedule.get.name},"
-        end
-        #End line
-        header_output << "\n"
-        output << "\n"
-=end
+
         #remove space_type (This speeds things up a bit. 
         st.remove
         shw_loop.remove
-        water_fixture.remove unless water_fixture.nil? 
-            
+        water_fixture.remove unless water_fixture.nil?
       end #loop spacetypes
-      puts template
     end #loop Template
     #Write test report file. 
-    test_result_file = File.join(File.dirname(__FILE__),'data','space_type_test_results.csv')
-    File.open(test_result_file, 'w') {|f| f.write(header_output + output) }
-      
+    test_result_file = File.join(File.dirname(__FILE__),'data','schedule_test_results.json')
+    File.open(test_result_file, 'w') {|f| f.write(JSON.pretty_generate(output_array)) }
+
     #Test that the values are correct by doing a file compare.
-    expected_result_file = File.join(File.dirname(__FILE__),'data','space_type_expected_results.csv')
+    expected_result_file = File.join(File.dirname(__FILE__),'data','schedule_test_expected_results.json')
     b_result = FileUtils.compare_file(expected_result_file , test_result_file )
     assert( b_result, 
-      "Spacetype test results do not match expected results! Compare/diff the output with the stored values here #{expected_result_file} and #{test_result_file}"
-    )  
-  end 
-  
+      "Schedule test results do not match expected results! Compare/diff the output with the stored values here #{expected_result_file} and #{test_result_file}"
+    )
+  end
 end
-
