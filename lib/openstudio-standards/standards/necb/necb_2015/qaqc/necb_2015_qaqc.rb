83e7f9e44b7290ce7d08d8d880a8ca011e066ea3
padmassun <padmassun@gmail.com>
2018-06-05 15:22:14 +0000

    sub NECB2011 with NECB2015 & Activated ext-opaque compliance [no changes from 2011]

--- lib/openstudio-standards/standards/necb/necb_2015/qaqc/necb_2015_qaqc.rb
+++ lib/openstudio-standards/standards/necb/necb_2015/qaqc/necb_2015_qaqc.rb
@@ -1,28 +1,91 @@
 class NECB2015
 
+
+  def load_qaqc_database_new()
+    # # Combine the data from the JSON files into a single hash
+    # files = Dir.glob("#{File.dirname(__FILE__)}/qaqc_data/*.json").select {|e| File.file? e}
+    # @qaqc_data = {}
+    # @qaqc_data["tables"] = []
+    # files.each do |file|
+    #   #puts "loading qaqc data from #{file}"
+    #   data = JSON.parse(File.read(file))
+    #   if not data["tables"].nil? and data["tables"].first["data_type"] =="table"
+    #     @qaqc_data["tables"] << data["tables"].first
+    #   else
+    #     @qaqc_data[data.keys.first] = data[data.keys.first]
+    #   end
+    # end
+    # #needed for compatibility of qaqc database format
+    # @qaqc_data['tables'].each do |table|
+    #   @qaqc_data[table['name']] = table
+    # end
+    # return @qaqc_data
+    super()
+
+    # replace 2011 to 2015 for all references in the tables.
+    # puts JSON.pretty_generate( @standards_data['tables'] )
+    @qaqc_data['tables'].each do |table|
+      if table.has_key?('refs')
+        # check if the reference is an array
+        if table['refs'].is_a?(Array)
+          table['refs'].each {|item|
+            item.gsub!('NECB2011', 'NECB2015')
+          }
+          # if the reference is a hash (e.g. see space.json compliance), then
+          # replace the 2011 in the value with 2015
+        elsif table['refs'].is_a?(Hash)
+          table['refs'].keys.each  {|key|
+            table['refs'][key].gsub!('NECB2011', 'NECB2015') unless table['refs'][key].nil?
+          }
+        end
+      end
+    end
+
+    # Combine the data from the JSON files into a single hash
+    top_dir = File.expand_path('../../..', File.dirname(__FILE__))
+    standards_data_dir = "#{top_dir}/data/"
+    files = Dir.glob("#{File.dirname(__FILE__)}/data/*.json").select {|e| File.file? e}
+    files.each do |file|
+      #puts "loading standards data from #{file}"
+      data = JSON.parse(File.read(file))
+      if not data["tables"].nil? and data["tables"].first["data_type"] =="table"
+        @qaqc_data["tables"] << data["tables"].first
+      else
+        @qaqc_data[data.keys.first] = data[data.keys.first]
+      end
+    end
+
+    #needed for compatibility of standards database format
+    @qaqc_data['tables'].each do |table|
+      @qaqc_data[table['name']] = table
+    end
+    return @qaqc_data
+  end
+
+
   def necb_envelope_compliance(qaqc)
     puts "\nUsing necb_envelope_compliance in NECB2015 Class\n"
     # Envelope
     necb_section_name = "NECB2015-Section 3.2.1.4"
-    #store hdd in short form
+    # store hdd in short form
     hdd = qaqc[:geography][:hdd]
-    #calculate fdwr based on hdd.
-    # [fdwr] *maximum* allowable total vertical fenestration and door area to 
+    # calculate fdwr based on hdd.
+    # [fdwr] *maximum* allowable total vertical fenestration and door area to
     # gross wall area ratio
-    fdwr = 0 
+    fdwr = 0
     # if hdd < 4000 [NECB 2011]
     if hdd <= 4000
       fdwr = 0.40
-    # elsif hdd >= 4000 and hdd <=7000 [NECB 2011]
+      # elsif hdd >= 4000 and hdd <=7000 [NECB 2011]
     elsif hdd > 4000 and hdd <7000
       fdwr = (2000-0.2 * hdd)/3000
-    # elsif hdd >7000   [NECB 2011]
+      # elsif hdd >7000   [NECB 2011]
     elsif hdd >=7000
       fdwr = 0.20
     end
     #hardset srr to 0.05
     srr = 0.05
-    
+
     # perform test. result must be equal to.
     necb_section_test(
         qaqc,
@@ -61,7 +124,7 @@ class NECB2015
 
     necb_infiltration_compliance(qaqc)
 
-    # necb_exterior_opaque_compliance(qaqc)
+    necb_exterior_opaque_compliance(qaqc)
 
     # necb_exterior_fenestration_compliance(qaqc)
 
@@ -74,7 +137,7 @@ class NECB2015
     # necb_economizer_compliance(qaqc)
 
     # necb_hrv_compliance(qaqc, model)
-    
+
     necb_vav_fan_power_compliance(qaqc)
 
     sanity_check(qaqc)
