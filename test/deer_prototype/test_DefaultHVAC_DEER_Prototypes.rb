659f6793f4938b23c2bd553b830b83eb6e02618e
Dahlhausen <mdahlhau@nrel.gov>
2018-06-06 14:49:34 -0600

    add tests

--- test/deer_prototype/test_DefaultHVAC_DEER_Prototypes.rb
+++ test/deer_prototype/test_DefaultHVAC_DEER_Prototypes.rb
@@ -1,7 +1,7 @@
 require_relative '../helpers/minitest_helper'
 require_relative '../helpers/create_deer_prototype_helper'
 
-class TestAllDEERPrototypes < CreateDEERPrototypeBuildingTest
+class TestDefaultHVACDEERPrototypes < CreateDEERPrototypeBuildingTest
   
   building_types = [
       'Asm',
@@ -216,6 +216,33 @@ class TestAllDEERPrototypes < CreateDEERPrototypeBuildingTest
       'WRf' => ['DXGF']
   }
   
+  building_to_hvac_system_defaults = {
+      'Asm' => ['DXGF'],
+      'ECC' => ['SVVG'],
+      'EPr' => ['DXGF'],
+      'ERC' => ['DXHP'],
+      'ESe' => ['DXGF'],
+      'EUn' => ['SVVG'],
+      'Gro' => ['DXGF'],
+      'Hsp' => ['SVVG'],
+      'Nrs' => ['FPFC'],
+      'Htl' => ['SVVG'],
+      'Mtl' => ['DXHP'],
+      'MBT' => ['DXGF'],
+      'MFm' => ['DXGF'],
+      'MLI' => ['DXGF'],
+      'OfL' => ['SVVG'],
+      'OfS' => ['DXGF'],
+      'RFF' => ['DXGF'],
+      'RSD' => ['DXGF'],
+      'Rt3' => ['SVVG'],
+      'RtL' => ['DXGF'],
+      'RtS' => ['DXGF'],
+      'SCn' => ['DXGF'],
+      'SUn' => ['Unc'], # listed as cNCGF in DEER database
+      'WRf' => ['DXGF']
+  }
+  
   templates = ['DEER Pre-1975', 'DEER 1985', 'DEER 1996', 'DEER 2003', 'DEER 2007', 'DEER 2011', 'DEER 2014', 'DEER 2015', 'DEER 2017']
   climate_zones = ['CEC T24-CEC1', 'CEC T24-CEC2', 'CEC T24-CEC3', 'CEC T24-CEC4',
                   'CEC T24-CEC5', 'CEC T24-CEC6', 'CEC T24-CEC7', 'CEC T24-CEC8',
@@ -230,8 +257,8 @@ class TestAllDEERPrototypes < CreateDEERPrototypeBuildingTest
   
   # Create a new set of tests for each building type because HVAC systems aren't all the same
   building_types.each do |building_type|
-    hvacs = building_to_hvac_systems[building_type]
-    TestAllDEERPrototypes.create_run_model_tests(building_types, templates, hvacs, climate_zones, create_models, run_models, compare_results, debug)
+    hvacs = building_to_hvac_system_defaults[building_type]
+    TestDefaultHVACDEERPrototypes.create_run_model_tests(building_types, templates, hvacs, climate_zones, create_models, run_models, compare_results, debug)
   end
 
 end
