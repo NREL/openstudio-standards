6a371af715f9f1b149e623ee12fcb6add22be907
Nicholas Long <nicholas.long@nrel.gov>
2018-07-06 15:30:57 -0600

    set random version to hack openstudio cli

--- openstudio-standards/lib/openstudio-standards/version.rb
+++ openstudio-standards/lib/openstudio-standards/version.rb
@@ -1,3 +1,5 @@
 module OpenstudioStandards
-  VERSION = '0.5.0.ambient'.freeze
+  # set a version higher than the installed version of OpenStudio standards that is shipped with OpenStudio CLI
+  # This allows OpenStudio CLI to see it as newer, thus using it.
+  VERSION = '0.5.0-ambient'.freeze
 end
