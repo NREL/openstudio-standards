fbd7f2931d076ff51ea463c804652be21292dccc
Andrew Parker <Andrew.Parker@nrel.gov>
2018-07-06 13:07:34 -0600

    Add door and attic infiltration to DOE prototypes

--- lib/openstudio-standards/prototypes/common/buildings/Prototype.RetailStandalone.rb
+++ lib/openstudio-standards/prototypes/common/buildings/Prototype.RetailStandalone.rb
@@ -13,36 +13,37 @@ module RetailStandalone
 
     # Add the door infiltration for template 2004,2007,2010,2013
     case template
-      when '90.1-2004', '90.1-2007', '90.1-2010', '90.1-2013'
-        entry_space = model.getSpaceByName('Front_Entry').get
-        infiltration_entry = OpenStudio::Model::SpaceInfiltrationDesignFlowRate.new(model)
-        infiltration_entry.setName('Entry door Infiltration')
+    when '90.1-2004'
+      entry_space = model.getSpaceByName('Front_Entry').get
+      infiltration_entry = OpenStudio::Model::SpaceInfiltrationDesignFlowRate.new(model)
+      infiltration_entry.setName('Entry door Infiltration')
+      infiltration_per_zone = 1.418672682
+      infiltration_entry.setDesignFlowRate(infiltration_per_zone)
+      infiltration_entry.setSchedule(model.add_schedule('RetailStandalone INFIL_Door_Opening_SCH'))
+      infiltration_entry.setSpace(entry_space)
+
+      # temporal solution for CZ dependent door infiltration rate.  In fact other standards need similar change as well
+    when '90.1-2007', '90.1-2010', '90.1-2013'
+      entry_space = model.getSpaceByName('Front_Entry').get
+      infiltration_entry = OpenStudio::Model::SpaceInfiltrationDesignFlowRate.new(model)
+      infiltration_entry.setName('Entry door Infiltration')
+      case climate_zone
+      when 'ASHRAE 169-2006-1A','ASHRAE 169-2006-1B','ASHRAE 169-2006-2A', 'ASHRAE 169-2006-2B'
         infiltration_per_zone = 1.418672682
-        infiltration_entry.setDesignFlowRate(infiltration_per_zone)
-        infiltration_entry.setSchedule(model_add_schedule(model, 'RetailStandalone INFIL_Door_Opening_SCH'))
-        infiltration_entry.setSpace(entry_space)
-    end
-
-    # Update the zone sizing SAT
-    if template == 'DOE Ref 1980-2004' || template == 'DOE Ref Pre-1980'
-      model.getSizingZones.each do |sizing_zone|
-        sizing_zone.setZoneCoolingDesignSupplyAirTemperature(14)
+        infiltration_entry.setSchedule(model.add_schedule('RetailStandalone INFIL_Door_Opening_SCH'))
+      else
+        infiltration_per_zone = 0.937286742
+        infiltration_entry.setSchedule(model.add_schedule('RetailStandalone INFIL_Door_Opening_SCH_2013'))
       end
+      infiltration_entry.setDesignFlowRate(infiltration_per_zone)
+      infiltration_entry.setSpace(entry_space)
     end
 
-    # Add economizer max fraction schedules
-    case template
-      when '90.1-2004', '90.1-2007', '90.1-2010', '90.1-2013', 'NREL ZNE Ready 2017'
-        econ_eff_sch = model_add_schedule(model, 'RetailStandalone PSZ_Econ_MaxOAFrac_Sch')
-        model.getAirLoopHVACs.each do |air_loop|
-          oa_sys = air_loop.airLoopHVACOutdoorAirSystem
-          if oa_sys.is_initialized
-            oa_sys = oa_sys.get
-            oa_controller = oa_sys.getControllerOutdoorAir
-            oa_controller.setMaximumFractionofOutdoorAirSchedule(econ_eff_sch)
-          end
-        end
-    end
+    # add these additional coefficient inputs
+    infiltration_entry.setConstantTermCoefficient(1.0)
+    infiltration_entry.setTemperatureTermCoefficient(0.0)
+    infiltration_entry.setVelocityTermCoefficient(0.0)
+    infiltration_entry.setVelocitySquaredTermCoefficient(0.0)
 
     OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', 'Finished building type specific adjustments')
 
--- lib/openstudio-standards/prototypes/common/buildings/Prototype.SmallOffice.rb
+++ lib/openstudio-standards/prototypes/common/buildings/Prototype.SmallOffice.rb
@@ -4,7 +4,61 @@
 # building types.
 module SmallOffice
   def model_custom_hvac_tweaks(building_type, climate_zone, prototype_input, model)
+    # add extra infiltration for entry door
+    add_door_infiltration(template, climate_zone, model)
+    OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', 'Added door infiltration')
+
+    # add extra infiltration for attic
+    add_attic_infiltration(template, climate_zone, model)
+    OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', 'Added attic infiltration')
+
     return true
+  end
+
+  def add_door_infiltration(template, climate_zone, model)
+    # add extra infiltration for entry door in m3/s (there is no attic in 'DOE Ref Pre-1980')
+    unless template == 'DOE Ref 1980-2004' || template == 'DOE Ref Pre-1980'
+      entry_space = model.getSpaceByName('Perimeter_ZN_1').get
+      infiltration_entrydoor = OpenStudio::Model::SpaceInfiltrationDesignFlowRate.new(model)
+      infiltration_entrydoor.setName('entry door Infiltration')
+      infiltration_per_zone_entrydoor = 0
+      if template == '90.1-2004'
+        infiltration_per_zone_entrydoor = 0.129785425
+        infiltration_entrydoor.setSchedule(model.add_schedule('OfficeSmall INFIL_Door_Opening_SCH'))
+      elsif template == '90.1-2007' || template == '90.1-2010'|| template == '90.1-2013'
+        case climate_zone
+        when 'ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A', 'ASHRAE 169-2006-1B', 'ASHRAE 169-2006-2B'
+          infiltration_per_zone_entrydoor = 0.129785425
+          infiltration_entrydoor.setSchedule(model.add_schedule('OfficeSmall INFIL_Door_Opening_SCH'))
+        else
+          infiltration_per_zone_entrydoor = 0.076455414
+          infiltration_entrydoor.setSchedule(model.add_schedule('OfficeSmall INFIL_Door_Opening_SCH_2013'))
+        end
+      end
+      infiltration_entrydoor.setDesignFlowRate(infiltration_per_zone_entrydoor)
+      infiltration_entrydoor.setConstantTermCoefficient(1.0)
+      infiltration_entrydoor.setTemperatureTermCoefficient(0.0)
+      infiltration_entrydoor.setVelocityTermCoefficient(0.0)
+      infiltration_entrydoor.setVelocitySquaredTermCoefficient(0.0)
+      infiltration_entrydoor.setSpace(entry_space)
+    end
+  end
+
+  def add_attic_infiltration(template, climate_zone, model)
+    # add extra infiltration for attic in m3/s (there is no attic in 'DOE Ref Pre-1980')
+    unless template == 'DOE Ref 1980-2004' || template == 'DOE Ref Pre-1980'
+      entry_space = model.getSpaceByName('Attic').get
+      infiltration_attic = OpenStudio::Model::SpaceInfiltrationDesignFlowRate.new(model)
+      infiltration_attic.setName('attic Infiltration')
+      infiltration_per_zone_attic = 0.2001
+      infiltration_attic.setSchedule(model.add_schedule('Always On'))
+      infiltration_attic.setDesignFlowRate(infiltration_per_zone_attic)
+      infiltration_attic.setConstantTermCoefficient(1.0)
+      infiltration_attic.setTemperatureTermCoefficient(0.0)
+      infiltration_attic.setVelocityTermCoefficient(0.0)
+      infiltration_attic.setVelocitySquaredTermCoefficient(0.0)
+      infiltration_attic.setSpace(entry_space)
+    end
   end
 
   def model_custom_swh_tweaks(model, building_type, climate_zone, prototype_input)
