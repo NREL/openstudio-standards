0ea1ad73fe3524e772be310da73b44e804948b59
Andrew Parker <Andrew.Parker@nrel.gov>
2018-07-08 08:06:02 -0600

    Changes the compare_results of the create_doe_prototype_helper to perform a regression test comparison of the osm files

--- test/helpers/create_doe_prototype_helper.rb
+++ test/helpers/create_doe_prototype_helper.rb
@@ -1,4 +1,5 @@
 #require "#{File.dirname(__FILE__)}/btap"
+require_relative 'compare_models_helper'
 
 
 
@@ -86,11 +87,11 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
     when 'NECB2011'
 
       method_name = "test_#{building_type}-#{template}-#{climate_zone}-#{File.basename(epw_file.to_s,'.epw')}".gsub(' ','_').gsub('.','_')
-      
+
     else
       method_name = "test_#{building_type}-#{template}-#{climate_zone}".gsub(' ','_')
     end
-    
+
 
     define_method(method_name) do
             
@@ -109,8 +110,8 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
     else
       model_name = "#{building_type}-#{template}-#{climate_zone}"
     end
-      
-      
+
+
       run_dir = "#{@test_dir}/#{model_name}"
       if !Dir.exists?(run_dir)
         Dir.mkdir(run_dir)
@@ -118,14 +119,18 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
       full_sim_dir = "#{run_dir}/AnnualRun"
       idf_path_string = "#{run_dir}/#{model_name}.idf"
       idf_path = OpenStudio::Path.new(idf_path_string)            
-      osm_path_string = "#{run_dir}/final.osm"
-      output_path = OpenStudio::Path.new(run_dir)
+      osm_path_string = "#{run_dir}/#{model_name}.osm"
+      osm_path = OpenStudio::Path.new(osm_path_string)
+      truth_osm_path_string = "#{Dir.pwd}/regression_models/#{model_name}.osm"
+      truth_osm_path = OpenStudio::Path.new(truth_osm_path_string)
 
       model = nil
-            
+
+      # Make a standard
+      prototype_creator = Standard.build("#{template}_#{building_type}")
+
       # Create the model, if requested
       if create_models
-        prototype_creator = Standard.build("#{template}_#{building_type}")
         model = prototype_creator.model_create_prototype_model(climate_zone, epw_file, run_dir)
         @current_model = model
         if model.is_a?(FalseClass)
@@ -153,6 +158,9 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
           ]
           BTAP::Reports::set_output_variables(model,"Hourly", output_variable_array)
 
+          # Save the model
+          model.save(osm_path, true)
+
           # Convert the model to energyplus idf
           forward_translator = OpenStudio::EnergyPlus::ForwardTranslator.new
           idf = forward_translator.translateModel(model)
@@ -172,206 +180,43 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
             
         # Load the model from disk if not already in memory
         if model.nil?
-          model = standard.safe_load_model(osm_path_string)
-          forward_translator = OpenStudio::EnergyPlus::ForwardTranslator.new
-          idf = forward_translator.translateModel(model)
-          idf.save(idf_path,true)
+          model = prototype_creator.safe_load_model(osm_path_string)
         end
 
         # Run the annual simulation
-        standard = Standard.build("#{template}")
-        standard.model_run_simulation_and_log_errors(model, full_sim_dir)
+        prototype_creator.model_run_simulation_and_log_errors(model, full_sim_dir)
 
       end           
             
                       
             
-      # Compare the results against the legacy idf files if requested
+      # Compare the model against the regression model, if requested.
       if compare_results
-            
-        acceptable_error_percentage = 0 # Max % error for any end use/fuel type combo
 
-        # Load the legacy idf results JSON file into a ruby hash
-        temp = File.read("#{Dir.pwd}/data/legacy_idf_results.json")
-        legacy_idf_results = JSON.parse(temp)
+        # Load the model from disk if not already in memory
+        if model.nil?
+          model = prototype_creator.safe_load_model(osm_path_string)
+        end
 
-        # List of all fuel types
-        fuel_types = ['Electricity', 'Natural Gas', 'Additional Fuel', 'District Cooling', 'District Heating', 'Water']
-
-        # List of all end uses
-        end_uses = ['Heating', 'Cooling', 'Interior Lighting', 'Exterior Lighting', 'Interior Equipment', 'Exterior Equipment', 'Fans', 'Pumps', 'Heat Rejection','Humidification', 'Heat Recovery', 'Water Systems', 'Refrigeration', 'Generators']
-              
-        sql_path_string = "#{@test_dir}/#{model_name}/AnnualRun/EnergyPlus/eplusout.sql"
-        sql_path = OpenStudio::Path.new(sql_path_string)
-        sql_path_string_2 = "#{@test_dir}/#{model_name}/AnnualRun/run/eplusout.sql"
-        sql_path_2 = OpenStudio::Path.new(sql_path_string_2)        
-        sql = nil
-        if OpenStudio.exists(sql_path)
-          sql = OpenStudio::SqlFile.new(sql_path)
-        elsif OpenStudio.exists(sql_path_2)
-          sql = OpenStudio::SqlFile.new(sql_path_2)
+        # Load the truth model from disk and compare to the newly-created model
+        if File.exist?(truth_osm_path_string)
+          truth_model = prototype_creator.safe_load_model(truth_osm_path_string)
+          diffs = compare_osm_files(truth_model, model)
         else
-          OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', "Could not find sql file, could not compare results.")
+          diffs = []
+          diffs << "ERROR: could not find regression model at #{truth_osm_path_string}, did not compare models."
         end
 
-        # Create a hash of hashes to store the results from each file
-        results_hash = Hash.new{|h,k| h[k]=Hash.new(&h.default_proc) }
-
-        # Get the osm values for all fuel type/end use pairs
-        # and compare to the legacy idf results
-        csv_rows = []
-        total_legacy_energy_val = 0
-        total_osm_energy_val = 0
-        total_legacy_water_val = 0
-        total_osm_water_val = 0
-        total_cumulative_energy_err = 0
-        total_cumulative_water_err = 0
-        fuel_types.each do |fuel_type|
-          end_uses.each do |end_use|
-            next if end_use == 'Exterior Equipment'
-            # Get the legacy results number
-            legacy_val = legacy_idf_results.dig(building_type, template, climate_zone, fuel_type, end_use)
-            # Combine the exterior lighting and exterior equipment
-            if end_use == 'Exterior Lighting'
-              legacy_exterior_equipment = legacy_idf_results.dig(building_type, template, climate_zone, fuel_type, 'Exterior Equipment')
-              unless legacy_exterior_equipment.nil?
-                legacy_val += legacy_exterior_equipment
-              end
+        # Write the diffs to a file
+        if diffs.size > 0
+          diff_file_path = "#{run_dir}/regression.log"
+          File.open(diff_file_path, 'w') do |file|
+            diffs.each do |d|
+              file.puts d
             end
-
-            if legacy_val.nil?
-              OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', "#{fuel_type} #{end_use} legacy idf value not found")
-              legacy_val = 0
-              next
-            end
-
-            # Add the energy to the total
-            if fuel_type == 'Water'
-              total_legacy_water_val += legacy_val
-            else
-              total_legacy_energy_val += legacy_val
-            end
-
-            # Select the correct units based on fuel type
-            units = 'GJ'
-            if fuel_type == 'Water'
-              units = 'm3'
-            end
-
-            # End use breakdown query
-            energy_query = "SELECT Value FROM TabularDataWithStrings WHERE (ReportName='AnnualBuildingUtilityPerformanceSummary') AND (ReportForString='Entire Facility') AND (TableName='End Uses') AND (ColumnName='#{fuel_type}') AND (RowName = '#{end_use}') AND (Units='#{units}')"
-
-            # Get the end use value
-            osm_val = sql.execAndReturnFirstDouble(energy_query)
-            if osm_val.is_initialized
-              osm_val = osm_val.get
-            else
-              OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', "No sql value found for #{fuel_type}-#{end_use}")
-              osm_val = 0
-            end
-
-            # Combine the exterior lighting and exterior equipment
-            if end_use == 'Exterior Lighting'
-              # End use breakdown query
-              energy_query = "SELECT Value FROM TabularDataWithStrings WHERE (ReportName='AnnualBuildingUtilityPerformanceSummary') AND (ReportForString='Entire Facility') AND (TableName='End Uses') AND (ColumnName='#{fuel_type}') AND (RowName = 'Exterior Equipment') AND (Units='#{units}')"
-
-              # Get the end use value
-              osm_val_2 = sql.execAndReturnFirstDouble(energy_query)
-              if osm_val_2.is_initialized
-                osm_val_2 = osm_val_2.get
-              else
-                OpenStudio::logFree(OpenStudio::Warn, 'openstudio.model.Model', "No sql value found for #{fuel_type}-Exterior Equipment.")
-                osm_val_2 = 0
-              end
-              osm_val += osm_val_2
-            end
-
-            # Add the energy to the total
-            if fuel_type == 'Water'
-              total_osm_water_val += osm_val
-            else
-              total_osm_energy_val += osm_val
-            end
-
-            # Add the absolute error to the total
-            abs_err = (legacy_val-osm_val).abs
-                  
-            if fuel_type == 'Water'
-              total_cumulative_water_err += abs_err
-            else                    
-              total_cumulative_energy_err += abs_err
-            end                  
-                  
-            # Calculate the error and check if less than
-            # acceptable_error_percentage
-            percent_error = nil
-            write_to_file = false
-            if osm_val > 0 && legacy_val > 0
-              percent_error = ((osm_val - legacy_val)/legacy_val) * 100
-              if percent_error.abs >= acceptable_error_percentage
-                OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "#{fuel_type}-#{end_use} Error = #{percent_error.round}% (#{osm_val}, #{legacy_val})")
-                write_to_file = true
-              end
-            elsif osm_val > 0 && legacy_val.abs < 1e-6
-              # The osm has a fuel/end use that the legacy idf does not
-              percent_error = 9999
-              OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "#{fuel_type}-#{end_use} Error = osm has extra fuel/end use that legacy idf does not (#{osm_val})")
-              write_to_file = true
-            elsif osm_val.abs < 1e-6 && legacy_val > 0
-              # The osm has a fuel/end use that the legacy idf does not
-              percent_error = 9999
-              OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "#{fuel_type}-#{end_use} Error = osm is missing a fuel/end use that legacy idf has (#{legacy_val})")
-              write_to_file = true
-            else
-              # Both osm and legacy are == 0 for this fuel/end use, no error
-              percent_error = 0
-            end
-
-            # ALWAYS OUTPUT THE VALUES
-            write_to_file = true
-            if write_to_file
-              csv_rows << "#{building_type},#{template},#{climate_zone},#{fuel_type},#{end_use},#{legacy_val.round(2)},#{osm_val.round(2)},#{percent_error.round},#{abs_err.round}"
-            end
-                  
-          end # Next end use
-        end # Next fuel type
-
-        # Calculate the overall energy error
-        total_percent_error = nil
-        if total_osm_energy_val > 0 && total_legacy_energy_val > 0
-          # If both
-          total_percent_error = ((total_osm_energy_val - total_legacy_energy_val)/total_legacy_energy_val) * 100
-          OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "Total Energy Error = #{total_percent_error.round}%")
-        elsif total_osm_energy_val > 0 && total_legacy_energy_val == 0
-          # The osm has a fuel/end use that the legacy idf does not
-          total_percent_error = 9999
-          OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "Total Energy Error = osm has extra fuel/end use that legacy idf does not (#{total_osm_energy_val})")
-        elsif total_osm_energy_val == 0 && total_legacy_energy_val > 0
-          # The osm has a fuel/end use that the legacy idf does not
-          total_percent_error = 9999
-          OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "Total Energy Error = osm is missing a fuel/end use that legacy idf has (#{total_legacy_energy_val})")
-        else
-          # Both osm and legacy are == 0 for, no error
-          total_percent_error = 0
-          OpenStudio::logFree(OpenStudio::Info, 'openstudio.model.Model', "Total Energy Error = both idf and osm don't use any energy.")
-        end
-
-        tot_abs_energy_err = ((total_osm_energy_val - total_legacy_energy_val)/total_legacy_energy_val) * 100
-        tot_cumulative_energy_err = (total_cumulative_energy_err/total_legacy_energy_val) * 100
-              
-        if tot_cumulative_energy_err.abs > 20
-          OpenStudio::logFree(OpenStudio::Warn, 'openstudio.model.Model', "Total Energy cumulative error = #{tot_cumulative_energy_err.round}%.")
-        end
-              
-        csv_rows << "#{building_type},#{template},#{climate_zone},Total Energy,Total Energy,#{total_legacy_energy_val.round(2)},#{total_osm_energy_val.round(2)},#{total_percent_error.round},#{tot_abs_energy_err.round},#{tot_cumulative_energy_err.round}"
-              
-        # Append the comparison results
-        File.open(@results_csv_file, 'a') do |file|
-          csv_rows.each do |csv_row|
-            file.puts csv_row
           end
         end
- 
+
       end
             
       # Calculate run time
@@ -380,7 +225,7 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
       # Report out errors
       log_file_path = "#{run_dir}/openstudio-standards.log"
       messages = log_messages_to_file(log_file_path, debug)
-      errors = get_logs(OpenStudio::Error)         
+      errors = get_logs(OpenStudio::Error)
             
       # Copy errors to combined log file
       File.open(@combined_results_log, 'a') do |file|
@@ -389,6 +234,9 @@ class CreateDOEPrototypeBuildingTest < Minitest::Test
           file.puts message
         end
       end
+
+      # Add any regression differences to the errors
+      errors += diffs
             
       # Assert if there were any errors
       assert(errors.size == 0, errors.reverse)
