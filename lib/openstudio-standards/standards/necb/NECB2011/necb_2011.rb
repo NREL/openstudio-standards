54fbb8577a65d8fdc60cf328c5c9c463302eaeb0
padmassun <padmassun@gmail.com>
2018-08-15 14:56:57 +0000

    Added remove_duplicate_materials_and_constructions method

--- lib/openstudio-standards/standards/necb/NECB2011/necb_2011.rb
+++ lib/openstudio-standards/standards/necb/NECB2011/necb_2011.rb
@@ -258,6 +258,8 @@ class NECB2011 < Standard
     model_add_daylighting_controls(model) # to be removed after refactor.
     # Add output variables for debugging
     model_request_timeseries_outputs(model) if debug
+    # Remove duplicate materials and constructions
+    model  = BTAP::FileIO::remove_duplicate_materials_and_constructions(model)
     return model
   end
 
