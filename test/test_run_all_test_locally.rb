13bc8c4a66c666259da24531be9225d5c6c12be5
padmassun <padmassun@gmail.com>
2018-08-15 17:28:00 +0000

    fix typo

--- test/test_run_all_test_locally.rb
+++ test/test_run_all_test_locally.rb
@@ -6,7 +6,7 @@ require 'open3'
 
 TestListFile = File.join(File.dirname(__FILE__), 'circleci_tests.txt')
 TestOutputFolder = File.join(File.dirname(__FILE__), 'local_test_output')
-ProcessorsUsed = ( 40).round
+ProcessorsUsed = ( Parallel.processor_count * 2 / 3 ).floor
 
 class String
   # colorization
