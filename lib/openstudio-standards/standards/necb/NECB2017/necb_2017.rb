e6780b589e009b8f4b2b16c1f95415060b322428
phylroy <phylroy.lopez@gmail.com>
2018-09-17 21:30:08 +0000

    conductance for 2017 turned on

--- lib/openstudio-standards/standards/necb/NECB2017/necb_2017.rb
+++ lib/openstudio-standards/standards/necb/NECB2017/necb_2017.rb
@@ -11,12 +11,12 @@ class NECB2017 < NECB2015
   end
 
   def load_standards_database_new()
-    #load NECB2015 data.
+    #load NECB2011 data.
     super()
-    #replace template to 2017 for all tables.
+    #replace template to 2015 for all tables.
     #puts JSON.pretty_generate( @standards_data['tables'] )
-    @standards_data['tables'].each do |table|
-      table['table'].each do |row|
+    @standards_data['tables'].each do |key,value|
+      value['table'].each do |row|
         ["lighting_standard", "ventilation_standard", "template"].each do |item|
           row[item].gsub!('NECB2015', 'NECB2017') unless row[item].nil?
         end
@@ -27,7 +27,7 @@ class NECB2017 < NECB2015
       embedded_files_relative('data/', /.*\.json/).each do |file|
         data = JSON.parse(EmbeddedScripting.getFileAsString(file))
         if not data["tables"].nil? and data["tables"].first["data_type"] =="table"
-          @standards_data["tables"] << data["tables"].first
+          @standards_data["tables"] = [*@standards_data["tables"],  *data["tables"] ].to_h
         else
           @standards_data[data.keys.first] = data[data.keys.first]
         end
@@ -36,19 +36,15 @@ class NECB2017 < NECB2015
       files = Dir.glob("#{File.dirname(__FILE__)}/data/*.json").select {|e| File.file? e}
       files.each do |file|
         data = JSON.parse(File.read(file))
-        if not data["tables"].nil? and data["tables"].first["data_type"] =="table"
-          @standards_data["tables"] << data["tables"].first
+        puts file
+        if not data["tables"].nil?
+          @standards_data["tables"] = [*@standards_data["tables"],  *data["tables"] ].to_h
         else
           @standards_data[data.keys.first] = data[data.keys.first]
         end
       end
     end
 
-
-    #needed for compatibility of standards database format
-    @standards_data['tables'].each do |table|
-      @standards_data[table['name']] = table
-    end
     return @standards_data
   end
 
