fe9fe4f56d8ae27475ec0522b599761d0ee60d4c
Andrew Parker <Andrew.Parker@nrel.gov>
2018-09-22 14:23:48 -0600

    Removes deprecated OS:ProgramControl object from compare_models() check

--- test/helpers/compare_models_helper.rb
+++ test/helpers/compare_models_helper.rb
@@ -32,7 +32,8 @@ def compare_osm_files(model_true, model_compare, look_for_renamed_objects = fals
     'OS:AvailabilityManagerAssignmentList', # Names appear to be created non-deteministically
     'OS:Schedule:Rule', # Names appear to be created non-deteministically
     'OS:Rendering:Color', # Rendering colors don't matter
-    'OS:Output:Meter' # Output meter objects may be different and don't affect results
+    'OS:Output:Meter', # Output meter objects may be different and don't affect results
+    'OS:ProgramControl' # Deprecated object no longer translated to EnergyPlus
   ]
 
   # Fill model object lists with all object types to be compared
