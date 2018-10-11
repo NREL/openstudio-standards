f81b901203a1c6995ec852a9a473c655663a9389
Dan Macumber <dan.macumber@gmail.com>
2018-10-10 21:22:58 -0600

    Do not unset GEM_PATH set by bundle, ignore bundle

--- lib/openstudio-standards.rb
+++ lib/openstudio-standards.rb
@@ -312,24 +312,27 @@ module OpenstudioStandards
     # blank out bundler and gem path modifications, will be re-setup by new call
     new_env = {}
     new_env["BUNDLER_ORIG_MANPATH"] = nil
-    new_env["GEM_PATH"] = nil
-    new_env["GEM_HOME"] = nil
     new_env["BUNDLER_ORIG_PATH"] = nil
     new_env["BUNDLER_VERSION"] = nil
     new_env["BUNDLE_BIN_PATH"] = nil
-    new_env["BUNDLE_GEMFILE"] = nil
     new_env["RUBYLIB"] = nil
     new_env["RUBYOPT"] = nil
     
-    bundle_gemfile = ENV['BUNDLE_GEMFILE']
-    bundle_path = ENV['BUNDLE_PATH']    
-    if bundle_gemfile.nil? || bundle_path.nil?
+    # DLM: preserve GEM_HOME and GEM_PATH set by current bundle because we are not supporting bundle
+    # requires to ruby gems will work, will fail if we require a native gem
+    #new_env["GEM_PATH"] = nil
+    #new_env["GEM_HOME"] = nil
+    
+    # DLM: for now, ignore current bundle in case it has binary dependencies in it
+    #bundle_gemfile = ENV['BUNDLE_GEMFILE']
+    #bundle_path = ENV['BUNDLE_PATH']    
+    #if bundle_gemfile.nil? || bundle_path.nil?
       new_env['BUNDLE_GEMFILE'] = nil
       new_env['BUNDLE_PATH'] = nil
-    else
-      new_env['BUNDLE_GEMFILE'] = bundle_gemfile
-      new_env['BUNDLE_PATH'] = bundle_path    
-    end  
+    #else
+    #  new_env['BUNDLE_GEMFILE'] = bundle_gemfile
+    #  new_env['BUNDLE_PATH'] = bundle_path    
+    #end  
     
     return new_env
   end
