c88a4cf76f353137cd558972cbcefe6597cb2823
Andrew Parker <Andrew.Parker@nrel.gov>
2019-01-22 09:26:02 -0700

    Expands OfficeDetailed tests and adds to circleci

--- test/doe_prototype/test_large_office_detailed.rb
+++ test/doe_prototype/test_large_office_detailed.rb
@@ -3,8 +3,8 @@ require_relative '../helpers/create_doe_prototype_helper'
 
 class TestLargeOfficeDetailed < CreateDOEPrototypeBuildingTest
   building_types = ['LargeOfficeDetailed']
-  templates = ['90.1-2007']
-  climate_zones = ['ASHRAE 169-2006-6A']
+  templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2007','90.1-2013']
+  climate_zones = ['ASHRAE 169-2006-3B']
   epw_files = ['USA_FL_Miami.Intl.AP.722020_TMY3.epw'] # not used for ASHRAE/DOE archetypes, but required for call
   create_models = true
   run_models = true
