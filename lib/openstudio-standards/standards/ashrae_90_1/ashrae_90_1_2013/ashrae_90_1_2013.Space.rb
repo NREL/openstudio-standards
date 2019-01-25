60f71f4084a0407275d07de22e10fec4b83cc044
Jeremy L <jeremy.lerond@pnnl.gov>
2019-01-25 09:26:16 -0800

    Fix Optional not initialized issue
    Some of the models generated by the tests do not include space types hence triggering a `Optional not initialized` error.

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2013/ashrae_90_1_2013.Space.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2013/ashrae_90_1_2013.Space.rb
@@ -71,19 +71,21 @@ class ASHRAE9012013 < ASHRAE901
     end
 
     # Retail spaces exception (3) to Section 9.4.1.1(e)
-    case space.spaceType.get().standardsSpaceType.to_s
-    # Retail standalone
-    # req_sec_ctrl set to true to create a second reference point
-    when 'Core_Retail'
-      req_pri_ctrl = false
-      req_sec_ctrl = true
-    when 'Entry', 'Front_Retail', 'Point_of_Sale'
-      req_pri_ctrl = false
-      req_sec_ctrl = false
-    # Strip mall
-    when 'Strip mall - type 1', 'Strip mall - type 2', 'Strip mall - type 3', 'Strip mall - type 0A', 'Strip mall - type 0B'
-      req_pri_ctrl = false
-      req_sec_ctrl = false
+    if space.spaceType.is_initialized	
+      case space.spaceType.get.standardsSpaceType.to_s
+      # Retail standalone
+      # req_sec_ctrl set to true to create a second reference point
+      when 'Core_Retail'
+        req_pri_ctrl = false
+        req_sec_ctrl = true
+      when 'Entry', 'Front_Retail', 'Point_of_Sale'
+        req_pri_ctrl = false
+        req_sec_ctrl = false
+      # Strip mall
+      when 'Strip mall - type 1', 'Strip mall - type 2', 'Strip mall - type 3', 'Strip mall - type 0A', 'Strip mall - type 0B'
+        req_pri_ctrl = false
+        req_sec_ctrl = false
+      end
     end
 
     return [req_top_ctrl, req_pri_ctrl, req_sec_ctrl]
