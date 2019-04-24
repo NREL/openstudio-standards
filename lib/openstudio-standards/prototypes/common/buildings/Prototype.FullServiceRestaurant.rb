b2793932cb202afc18ad4ece2a4cca004c7d8ee3
Jeremy L <jeremy.lerond@pnnl.gov>
2019-04-24 13:44:39 -0700

    Full Service Restaurant
    Full service restaurant.

--- lib/openstudio-standards/prototypes/common/buildings/Prototype.FullServiceRestaurant.rb
+++ lib/openstudio-standards/prototypes/common/buildings/Prototype.FullServiceRestaurant.rb
@@ -24,6 +24,73 @@ module FullServiceRestaurant
     return true
   end
 
+  def model_custom_daylighting_tweaks(building_type, climate_zone, prototype_input, model)
+    OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', 'Adjusting daylight sensor positions and fractions')
+
+    adjustments = [
+                    { '90.1-2010' => { 'Dining' => { 'sensor_1_frac' => 0.135,
+                                                     'sensor_2_frac' => 0.135,
+                                                     'sensor_1_xyz' => [1.9812, 1.9812, 0.762],
+                                                     'sensor_2_xyz' => [20.574, 1.9812, 0.762],
+																									 },
+                                      },
+                      '90.1-2013' => { 'Dining' => { 'sensor_1_frac' => 0.25,
+                                                     'sensor_2_frac' => 0.25,
+                                                     'sensor_1_xyz' => [2.6548, 2.6548, 0.762],
+                                                     'sensor_2_xyz' => [19.9539, 2.6548, 0.762],
+																												},
+                                      },
+										}
+                  ]
+
+    # Adjust daylight sensors in each space
+    model.getSpaces.each do |space|
+      if adjustments[0].keys.include? (template)
+        if adjustments[0][template].keys.include? (space.name.to_s)
+          adj = adjustments[0][template][space.name.to_s]
+          next if space.thermalZone.empty?
+          zone = space.thermalZone.get
+          next if space.spaceType.empty?
+          spc_type = space.spaceType.get
+          next if spc_type.standardsSpaceType.empty?
+          stds_spc_type = spc_type.standardsSpaceType.get
+          # Adjust the primary sensor
+          if adj['sensor_1_frac'] && zone.primaryDaylightingControl.is_initialized
+            OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', "For #{zone.name}: Adjusting primary daylight sensor to control #{adj['sensor_1_frac']} of the lighting.")
+            zone.setFractionofZoneControlledbyPrimaryDaylightingControl(adj['sensor_1_frac'])
+            pri_ctrl = zone.primaryDaylightingControl.get
+            if adj['sensor_1_xyz']
+              x = adj['sensor_1_xyz'][0]
+              y = adj['sensor_1_xyz'][1]
+              z = adj['sensor_1_xyz'][2]
+              OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', "For #{zone.name}: Adjusting primary daylight sensor position to [#{x}, #{y}, #{z}].")
+              pri_ctrl.setPositionXCoordinate(x)
+              pri_ctrl.setPositionYCoordinate(y)
+              pri_ctrl.setPositionZCoordinate(z)
+            end
+          end
+          # Adjust the secondary sensor
+          if adj['sensor_2_frac'] && zone.secondaryDaylightingControl.is_initialized
+            OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', "For #{zone.name}: Adjusting secondary daylight sensor to control #{adj['sensor_2_frac']} of the lighting.")
+            zone.setFractionofZoneControlledbySecondaryDaylightingControl(adj['sensor_2_frac'])
+            sec_ctrl = zone.secondaryDaylightingControl.get
+            if adj['sensor_2_xyz']
+              x = adj['sensor_2_xyz'][0]
+              y = adj['sensor_2_xyz'][1]
+              z = adj['sensor_2_xyz'][2]
+              OpenStudio.logFree(OpenStudio::Info, 'openstudio.model.Model', "For #{zone.name}: Adjusting secondary daylight sensor position to [#{x}, #{y}, #{z}].")
+              sec_ctrl.setPositionXCoordinate(x)
+              sec_ctrl.setPositionYCoordinate(y)
+              sec_ctrl.setPositionZCoordinate(z)
+            end
+          end
+        end
+      end
+    end
+
+    return true
+  end
+  
   # add hvac
 
   def add_door_infiltration(climate_zone, model)
