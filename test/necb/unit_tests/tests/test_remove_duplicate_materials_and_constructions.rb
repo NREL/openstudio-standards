05e9d9ae315b044995063ac7e45cf16c64fc1c94
phylroy <phylroy.lopez@canada.ca>
2019-05-27 19:37:51 +0000

    changes to test files

---
+++ test/necb/unit_tests/tests/test_remove_duplicate_materials_and_constructions.rb
@@ -0,0 +1,23 @@
+require_relative '../../helpers/minitest_helper'
+require_relative '../../helpers/create_doe_prototype_helper'
+require 'json'
+
+
+class TestRemoveDuplicateModelObjects < CreateDOEPrototypeBuildingTest
+
+  def test_remove_duplicate_model_objects
+    # read a sample model called fsr model present within the test/necb/models/fsr.osm
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    osm_path = File.join(File.dirname(__FILE__), 'resources', 'fsr.osm')
+    path = OpenStudio::Path.new(osm_path)
+    model = translator.loadModel(path)
+    model = model.get
+
+    old_number_of_objects = model.getModelObjects.length
+    new_model = BTAP::FileIO::remove_duplicate_materials_and_constructions(model)
+    new_number_of_objects = new_model.getModelObjects.length
+
+    puts "Number of objects removed: #{old_number_of_objects - new_number_of_objects}"
+    assert((old_number_of_objects - new_number_of_objects > 0))
+  end
+end
