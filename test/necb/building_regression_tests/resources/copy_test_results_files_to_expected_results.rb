4bd7598b8034f6136b10012ec19ea242784a1cf7
phylroy <phylroy.lopez@gmail.com>
2019-06-27 21:45:07 +0000

    updated regression files

--- test/necb/building_regression_tests/resources/copy_test_results_files_to_expected_results.rb
+++ test/necb/building_regression_tests/resources/copy_test_results_files_to_expected_results.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 
-folder_path = File.dirname(__FILE__)
+folder_path = "#{File.dirname(__FILE__)}/../expected_results/"
+puts folder_path
 Dir.glob(folder_path + "/*test_result.osm").sort.each do |f|
   new_file = f.gsub("test_result.osm","expected_result.osm")
   FileUtils.cp(f,new_file )
