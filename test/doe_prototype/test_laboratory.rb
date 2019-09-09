690ec20d896a4b14195fa2129c49a66a3b72298d
Kaiyu Sun <ksun@lbl.gov>
2019-09-09 16:26:53 -0700

    - Minor update on the data center models based on the CFD modeling results.
    !!!Note: A bug was just found on the ITE object in EnergyPlus, and is being fixed. Current data center prototype models would terminate with severe errors due to the bug. It is not practical to catch up with the Sep 2019 release (next week), but if a patch version will be released later, we'll be able to make it. Worst scenario is the 2020 March release. Before the bug is fixed and released, the data center prototype models should be disabled temporarily.

--- test/doe_prototype/test_laboratory.rb
+++ test/doe_prototype/test_laboratory.rb
@@ -6,11 +6,11 @@ class TestLaboratory < CreateDOEPrototypeBuildingTest
   templates = ['90.1-2004','90.1-2007','90.1-2010','90.1-2013']
   # templates = ['90.1-2004']
   # climate_zones = ['ASHRAE 169-2013-2A']
-  # climate_zones = ['ASHRAE 169-2013-2A','ASHRAE 169-2013-3B','ASHRAE 169-2013-5A','ASHRAE 169-2013-8A']
-  climate_zones = ['ASHRAE 169-2013-1A', 'ASHRAE 169-2013-2A','ASHRAE 169-2013-2B',
-                   'ASHRAE 169-2013-3A', 'ASHRAE 169-2013-3B', 'ASHRAE 169-2013-3C', 'ASHRAE 169-2013-4A',
-                   'ASHRAE 169-2013-4B', 'ASHRAE 169-2013-4C', 'ASHRAE 169-2013-5A', 'ASHRAE 169-2013-5B',
-                   'ASHRAE 169-2013-6A', 'ASHRAE 169-2013-6B', 'ASHRAE 169-2013-7A', 'ASHRAE 169-2013-8A']
+  climate_zones = ['ASHRAE 169-2013-2A','ASHRAE 169-2013-3B','ASHRAE 169-2013-5A','ASHRAE 169-2013-8A']
+  # climate_zones = ['ASHRAE 169-2013-1A', 'ASHRAE 169-2013-2A','ASHRAE 169-2013-2B',
+  #                  'ASHRAE 169-2013-3A', 'ASHRAE 169-2013-3B', 'ASHRAE 169-2013-3C', 'ASHRAE 169-2013-4A',
+  #                  'ASHRAE 169-2013-4B', 'ASHRAE 169-2013-4C', 'ASHRAE 169-2013-5A', 'ASHRAE 169-2013-5B',
+  #                  'ASHRAE 169-2013-6A', 'ASHRAE 169-2013-6B', 'ASHRAE 169-2013-7A', 'ASHRAE 169-2013-8A']
   epw_files = ['USA_FL_Miami.Intl.AP.722020_TMY3.epw'] # not used for ASHRAE/DOE archetypes, but required for call
   create_models = true
   run_models = true
