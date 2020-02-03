5c7cc87996f91c9cf01fab3ebeccc11f4fd252a7
Chris Kirney <chris.kirney@canada.ca>
2020-02-03 20:25:27 +0000

    Finalizing envelope components for BTAP1980TO2010 and updating regression and envelope tests and associated expected results.

--- test/necb/unit_tests/tests/test_necb_constructions_fdwr.rb
+++ test/necb/unit_tests/tests/test_necb_constructions_fdwr.rb
@@ -17,7 +17,7 @@ class NECB_Constructions_FDWR_Tests < Minitest::Test
       'CAN_NU_Resolute.AP.719240_CWEC2016.epw' # CZ 8HDD = 12570
   ]
   #Set Compliance vintage
-  Templates = ['NECB2011','NECB2015','NECB2017', 'BTAPPRE1980']
+  Templates = ['NECB2011','NECB2015','NECB2017', 'BTAPPRE1980', 'BTAP1980TO2010']
 
   # Create scaffolding to create a model with windows, then reset to appropriate values.
   # Will require large windows and constructions that have high U-values.    
