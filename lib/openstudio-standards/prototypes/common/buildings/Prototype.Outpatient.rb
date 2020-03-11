b55463ea1e9c0b51d21a2efd6b3ca1db65c43184
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2020-03-11 15:58:16 -0700

    Move building specific methods from standards to building specific files under prototype.

--- lib/openstudio-standards/prototypes/common/buildings/Prototype.Outpatient.rb
+++ lib/openstudio-standards/prototypes/common/buildings/Prototype.Outpatient.rb
@@ -400,4 +400,70 @@ module Outpatient
 
     return true
   end
+
+  def air_terminal_single_duct_vav_reheat_apply_initial_prototype_damper_position(air_terminal_single_duct_vav_reheat, zone_oa_per_area)
+    # Minimum damper position
+    # Based on AIA 2001 ventilation requirements
+    # See Section 5.2.2.16 in Thornton et al. 2010
+    # https://www.energycodes.gov/sites/default/files/documents/BECP_Energy_Cost_Savings_STD2010_May2011_v00.pdf
+    if template == '90.1-2004' || template == '90.1-2007'
+      min_damper_position = 0.3
+      init_mdp = {
+        'FLOOR 2 CONFERENCE TOILET' => 1.0,
+        'FLOOR 2 EXAM 1' => 1.0,
+        'FLOOR 2 EXAM 2' => 1.0,
+        'FLOOR 2 EXAM 3' => 1.0,
+        'FLOOR 2 EXAM 4' => 1.0,
+        'FLOOR 2 EXAM 5' => 1.0,
+        'FLOOR 2 EXAM 6' => 1.0,
+        'FLOOR 2 EXAM 7' => 1.0,
+        'FLOOR 2 EXAM 8' => 1.0,
+        'FLOOR 2 EXAM 9' => 1.0,
+        'FLOOR 2 RECEPTION TOILET' => 1.0,
+        'FLOOR 2 WORK TOILET' => 1.0,
+        'FLOOR 3 LOUNGE TOILET' => 1.0,
+        'FLOOR 3 OFFICE TOILET' => 1.0,
+        'FLOOR 3 PHYSICAL THERAPY 1' => 1.0,
+        'FLOOR 3 PHYSICAL THERAPY 2' => 1.0,
+        'FLOOR 3 PHYSICAL THERAPY TOILET' => 1.0,
+        'FLOOR 3 STORAGE 1' => 1.0,
+        'FLOOR 3 TREATMENT' => 1.0
+      }
+    elsif template == '90.1-2010' || template == '90.1-2013'
+      min_damper_position = 0.2
+      init_mdp = {
+        'FLOOR 2 CONFERENCE TOILET' => 1.0,
+        'FLOOR 2 EXAM 1' => 0.51,
+        'FLOOR 2 EXAM 2' => 1.0,
+        'FLOOR 2 EXAM 3' => 1.0,
+        'FLOOR 2 EXAM 4' => 0.64,
+        'FLOOR 2 EXAM 5' => 0.69,
+        'FLOOR 2 EXAM 6' => 0.94,
+        'FLOOR 2 EXAM 7' => 1.0,
+        'FLOOR 2 EXAM 8' => 0.93,
+        'FLOOR 2 EXAM 9' => 1.0,
+        'FLOOR 2 RECEPTION TOILET' => 1.0,
+        'FLOOR 2 WORK TOILET' => 1.0,
+        'FLOOR 3 LOUNGE TOILET' => 1.0,
+        'FLOOR 3 OFFICE TOILET' => 1.0,
+        'FLOOR 3 PHYSICAL THERAPY 1' => 0.69,
+        'FLOOR 3 PHYSICAL THERAPY 2' => 0.83,
+        'FLOOR 3 PHYSICAL THERAPY TOILET' => 1.0,
+        'FLOOR 3 STORAGE 1' => 1.0,
+        'FLOOR 3 TREATMENT' => 0.81
+      }
+    end
+
+    airlp = air_terminal_single_duct_vav_reheat.airLoopHVAC.get
+    init_mdp.each do |zn_name, mdp|
+      if air_terminal_single_duct_vav_reheat.name.to_s.upcase.strip.include? zn_name.to_s.strip
+        min_damper_position = mdp
+      end
+    end
+
+    # Set the minimum flow fraction
+    air_terminal_single_duct_vav_reheat.setConstantMinimumAirFlowFraction(min_damper_position)
+
+    return true
+  end
 end
