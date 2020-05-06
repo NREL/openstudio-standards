63e0135c10e9a4a470cda74791989de29214266c
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2020-05-06 12:42:57 -0700

    Add test for 90.1-2019 PRM. The test create a DOE prototype building and its baseline. The only check currently performed is to make sure that the baseline is correctly created. More will be implemented in the future.

---
+++ test/90_1_prm/test_create_prototype_baseline_building.rb
@@ -0,0 +1,71 @@
+require_relative '../helpers/minitest_helper'
+require_relative '../helpers/create_doe_prototype_helper'
+
+class DOEPrototypeBaseline < CreateDOEPrototypeBuildingTest
+
+  def self.generate_prototype_model_and_baseline(building_type, template, climate_zone)
+      # Initialize weather file, necessary but not used
+      epw_file = 'USA_FL_Miami.Intl.AP.722020_TMY3.epw'
+
+      # Create output folder if it doesn't already exist
+      @test_dir = "#{Dir.pwd}/output"
+      if !Dir.exists?(@test_dir)
+        Dir.mkdir(@test_dir)
+      end
+
+      # Define model name and run folder if it doesn't already exist
+      model_name = "#{building_type}-#{template}-#{climate_zone}"
+      run_dir = "#{@test_dir}/#{model_name}"
+      if !Dir.exists?(run_dir)
+        Dir.mkdir(run_dir)
+      end  
+
+      # Create the prototype
+      prototype_creator = Standard.build("#{template}_#{building_type}")
+      model = prototype_creator.model_create_prototype_model(climate_zone, epw_file, run_dir)
+
+      # Specify all standards building and space types
+      # TODO: Currently using the same one for all cases
+      #       but we plan on updating it using a spreadsheet
+      #       that Doug developed
+      bldg = model.building.get()
+      bldg.setStandardsBuildingType("All Other")
+      model.getSpaceTypes.sort.each do |spc|
+        spc.setStandardsBuildingType("All Other")
+        spc.setStandardsSpaceType("office - whole building")
+      end
+
+      # Save prototype OSM file
+      osm_path = OpenStudio::Path.new("#{run_dir}/#{model_name}.osm")
+      model.save(osm_path, true)
+
+      # Translate prototype model to an IDF file
+      forward_translator = OpenStudio::EnergyPlus::ForwardTranslator.new
+      idf_path = OpenStudio::Path.new("#{run_dir}/#{model_name}.idf")
+      idf = forward_translator.translateModel(model)
+      idf.save(idf_path,true)
+
+      # Initialize 90.1-2019 PRM Standard class
+      prototype_creator = Standard.build("90.1-PRM-2019")
+
+      # Create baseline model
+      model_baseline = prototype_creator.model_create_prm_baseline_building(model, building_type, climate_zone, nil, "#{run_dir}-Baseline", false)
+      return model_baseline, model
+    end
+
+  def test_create_prototype_baseline_building
+      # Define prototypes to be generated
+      @templates = ['90.1-2013']
+      @building_types = ['SmallOffice']
+      @climate_zones = ['ASHRAE 169-2013-2A']
+
+      # Generate prototype building models and associated baselines
+      all_comp =  @building_types.product @templates, @climate_zones
+      all_comp.each do |building_type, template, climate_zone|
+        model_baseline, model = DOEPrototypeBaseline.generate_prototype_model_and_baseline(building_type, template, climate_zone)
+        assert(model_baseline,"Baseline model could not be generated for #{building_type}, #{template}, #{climate_zone}")
+
+        # TODO: Load newly created baseline model and create additional assertions
+      end
+  end
+end
