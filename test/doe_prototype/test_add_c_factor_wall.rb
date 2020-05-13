8d8d04c4de58e26c87562d8a9893a40e27f1496e
Gonzalez, Juan <juan.gonzalez@pnnl.gov>
2020-05-13 19:38:22 -0400

    Minor updates to ground FC tests to get them to run with latest updates.

--- test/doe_prototype/test_add_c_factor_wall.rb
+++ test/doe_prototype/test_add_c_factor_wall.rb
@@ -15,15 +15,15 @@ class TestAddCFactorWall < CreateDOEPrototypeBuildingTest
     #Mapping of ASHRAE 90.1 standards + climate zones to their respective assembly C-Factors
     cases = {
         # Format: [CZ, template, OSM path] = C-factor (SI)
-        ['ASHRAE 169-2013-1A', '90.1-2004', hospital_osm_path] => 6.47292,
-        ['ASHRAE 169-2013-2A', '90.1-2007', hospital_osm_path] => 6.47292,
-        ['ASHRAE 169-2013-3A', '90.1-2010', hospital_osm_path] => 6.47292,
-        ['ASHRAE 169-2013-3B', '90.1-2004', hospital_osm_path] => 6.47292,
-        ['ASHRAE 169-2013-4A', '90.1-2013', hospital_osm_path] => 0.67568,
-        ['ASHRAE 169-2013-5A', '90.1-2007', large_hotel_osm_path] => 0.67568,
-        ['ASHRAE 169-2013-6A', '90.1-2013', large_hotel_osm_path] => 0.52238,
-        ['ASHRAE 169-2013-7A', '90.1-2013', large_hotel_osm_path] => 0.35771,
-        ['ASHRAE 169-2013-8A', '90.1-2007', large_hotel_osm_path] => 0.67568
+        ['ASHRAE 169-2013-1A', '90.1-2004', hospital_osm_path, 'LargeHotel'] => 6.47,
+        ['ASHRAE 169-2013-2A', '90.1-2007', hospital_osm_path, 'LargeHotel'] => 6.47,
+        ['ASHRAE 169-2013-3A', '90.1-2010', hospital_osm_path, 'LargeHotel'] => 6.47,
+        ['ASHRAE 169-2013-3B', '90.1-2004', hospital_osm_path, 'LargeHotel'] => 6.47,
+        ['ASHRAE 169-2013-4A', '90.1-2013', hospital_osm_path, 'LargeHotel'] => 0.68,
+        ['ASHRAE 169-2013-5A', '90.1-2007', large_hotel_osm_path, 'Hospital'] => 0.68,
+        ['ASHRAE 169-2013-6A', '90.1-2013', large_hotel_osm_path, 'Hospital'] => 0.52,
+        ['ASHRAE 169-2013-7A', '90.1-2013', large_hotel_osm_path, 'Hospital'] => 0.36,
+        ['ASHRAE 169-2013-8A', '90.1-2007', large_hotel_osm_path, 'Hospital'] => 0.68
     }
 
     #Below grade wall height of both Large Hotel and Hospital is the same at 2.439 meters
@@ -35,6 +35,7 @@ class TestAddCFactorWall < CreateDOEPrototypeBuildingTest
       climate_zone = parameters[0]
       template = parameters[1]
       osm_path = parameters[2]
+      building_type = parameters[3]
 
       #load the example OSM ready for c-factor constructions
       translator = OpenStudio::OSVersion::VersionTranslator.new
@@ -45,13 +46,13 @@ class TestAddCFactorWall < CreateDOEPrototypeBuildingTest
       standard = Standard.build(template)
 
       #Set c-factor constructions
-      standard.model_set_below_grade_wall_constructions(model, climate_zone)
+      standard.model_set_below_grade_wall_constructions(model, building_type, climate_zone)
 
       #parse the modified model for the C-Factor constructions (it should be the only CFactor construction)
       c_factor_construction = model.getCFactorUndergroundWallConstructions[0]
 
       c_factor_height =  c_factor_construction.height.round(3)
-      c_factor_generated = c_factor_construction.getCFactor.value.round(5)
+      c_factor_generated = c_factor_construction.getCFactor.value.round(2)
 
 
       asserts = {
@@ -59,8 +60,9 @@ class TestAddCFactorWall < CreateDOEPrototypeBuildingTest
           'C_Factor' => [c_factor_generated, c_factor_standard]
       }
       asserts.each do |assert_key, assert_content|
-        assert(assert_content[0].to_s.to_f == assert_content[1].to_s.to_f, "#{climate_zone} #{template} - #{assert_key} - #{assert_content[0]}:#{assert_content[1]}")
+
         puts "#{climate_zone} #{template} - #{assert_key} - Generated = #{assert_content[0]}:  Expected = #{assert_content[1]}"
+        assert(assert_content[0].to_s.to_f == assert_content[1].to_s.to_f, "#{climate_zone} #{template} - #{assert_key} - #{assert_content[0]}:#{assert_content[1]}")
       end
     end
 
--- test/doe_prototype/test_add_f_factor_floor.rb
+++ test/doe_prototype/test_add_f_factor_floor.rb
@@ -14,15 +14,15 @@ class TestAddFFactorFloor < CreateDOEPrototypeBuildingTest
     #Mapping of ASHRAE 90.1 standards + climate zones to their respective assembly F-Factor parameters
     cases = {
         # Format: [CZ, template, OSM path]                          => [F-factor (SI), Area (m2), Perimeter (m)]
-        ['ASHRAE 169-2013-1A', '90.1-2004', small_office_osm]       => [1.26363, 113.45, 27.69],
-        ['ASHRAE 169-2013-2A', '90.1-2007', small_office_osm]       => [1.26363, 113.45, 27.69],
-        ['ASHRAE 169-2013-3A', '90.1-2010', small_office_osm]       => [1.26363, 113.45, 27.69],
-        ['ASHRAE 169-2013-3B', '90.1-2004', small_office_osm]       => [1.26363, 113.45, 27.69],
-        ['ASHRAE 169-2013-4A', '90.1-2013', small_office_osm]       => [0.90012, 113.45, 27.69],
-        ['ASHRAE 169-2013-5A', '90.1-2007', retail_stand_alone_osm] => [1.26363, 379.89, 68.27],
-        ['ASHRAE 169-2013-6A', '90.1-2013', retail_stand_alone_osm] => [0.88281, 379.89, 68.27],
-        ['ASHRAE 169-2013-7A', '90.1-2013', retail_stand_alone_osm] => [0.88281, 379.89, 68.27],
-        ['ASHRAE 169-2013-8A', '90.1-2004', retail_stand_alone_osm] => [0.93474, 379.89, 68.27]
+        ['ASHRAE 169-2013-1A', '90.1-2004', small_office_osm, 'SmallOffice']       => [1.26, 113.45, 27.69],
+        ['ASHRAE 169-2013-2A', '90.1-2007', small_office_osm, 'SmallOffice']       => [1.26, 113.45, 27.69],
+        ['ASHRAE 169-2013-3A', '90.1-2010', small_office_osm, 'SmallOffice']       => [1.26, 113.45, 27.69],
+        ['ASHRAE 169-2013-3B', '90.1-2004', small_office_osm, 'SmallOffice']       => [1.26, 113.45, 27.69],
+        ['ASHRAE 169-2013-4A', '90.1-2013', small_office_osm, 'SmallOffice']       => [0.90, 113.45, 27.69],
+        ['ASHRAE 169-2013-5A', '90.1-2007', retail_stand_alone_osm, 'Retail'] => [1.26, 379.89, 68.27],
+        ['ASHRAE 169-2013-6A', '90.1-2013', retail_stand_alone_osm, 'Retail'] => [0.88, 379.89, 68.27],
+        ['ASHRAE 169-2013-7A', '90.1-2013', retail_stand_alone_osm, 'Retail'] => [0.88, 379.89, 68.27],
+        ['ASHRAE 169-2013-8A', '90.1-2004', retail_stand_alone_osm, 'Retail'] => [0.93, 379.89, 68.27]
     }
 
     cases.each do |input_parameters, expected_parameters|
@@ -31,6 +31,9 @@ class TestAddFFactorFloor < CreateDOEPrototypeBuildingTest
       climate_zone = input_parameters[0]
       template = input_parameters[1]
       osm_path = input_parameters[2]
+      building_type = input_parameters[3]
+
+      #puts "Checking cz: #{climate_zone}, template: #{template}, osm: #{osm_path}, bldg_type = #{building_type}"
 
       #Parse expected F-Factor construction parameters
       standard_f_factor = expected_parameters[0]
@@ -46,10 +49,10 @@ class TestAddFFactorFloor < CreateDOEPrototypeBuildingTest
       standard = Standard.build(template)
 
       #Set f-factor constructions
-      standard.model_set_floor_constructions(model, climate_zone)
+      standard.model_set_floor_constructions(model, building_type, climate_zone)
 
       #parse the modified model for the F-Factor constructions
-      expected_name = "Foundation F #{f_factor.round(2).to_s} Perim #{perimeter.round(2).to_s} Area #{area.round(2).to_s}".gsub('.','')
+      expected_name = "Foundation F #{standard_f_factor.round(2).to_s} Perim #{standard_perimeter.round(2).to_s} Area #{standard_area.round(2).to_s}".gsub('.','')
       f_factor_construction = model.getFFactorGroundFloorConstructionByName(expected_name)
 
       #Ensure that the f_factor
@@ -58,7 +61,7 @@ class TestAddFFactorFloor < CreateDOEPrototypeBuildingTest
 
       f_factor_construction = f_factor_construction.get
 
-      generated_f_factor = f_factor_construction.getFFactor.value.round(5)
+      generated_f_factor = f_factor_construction.getFFactor.value.round(2)
       generated_area = f_factor_construction.area.round(2)
       generated_perimeter = f_factor_construction.perimeterExposed.round(2)
 
@@ -68,8 +71,10 @@ class TestAddFFactorFloor < CreateDOEPrototypeBuildingTest
           'Perimeter' => [generated_perimeter, standard_perimeter]
       }
       asserts.each do |assert_key, assert_content|
-        assert(assert_content[0].to_s.to_f == assert_content[1].to_s.to_f, "#{climate_zone} #{template} - #{assert_key} - #{assert_content[0]}:#{assert_content[1]}")
+
         #puts "#{climate_zone} #{template} - #{assert_key} - Generated = #{assert_content[0]}:  Expected = #{assert_content[1]}"
+        assert(assert_content[0].to_s.to_f == assert_content[1].to_s.to_f, "#{climate_zone} #{template} - #{assert_key} - #{assert_content[0]}:#{assert_content[1]}")
+
       end
     end
 
