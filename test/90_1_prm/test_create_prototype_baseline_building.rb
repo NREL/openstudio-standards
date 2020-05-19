d4e8984ab29594e5c4c64938ff0a6c40fe7900e0
Doug Maddox <douglas.maddox@pnnl.gov>
2020-05-19 17:11:49 -0400

    Add json file with lpd space type mapping between prototypes and LPD lookup table.

--- test/90_1_prm/test_create_prototype_baseline_building.rb
+++ test/90_1_prm/test_create_prototype_baseline_building.rb
@@ -3,9 +3,12 @@ require_relative '../helpers/create_doe_prototype_helper'
 
 class DOEPrototypeBaseline < CreateDOEPrototypeBuildingTest
 
-  def self.generate_prototype_model_and_baseline(building_type, template, climate_zone, hvac_building_type = 'All others', wwr_building_type = 'All others', swh_building_type = 'All others', lpd_space_types)
+  def self.generate_prototype_model_and_baseline(building_type, template, climate_zone, hvac_building_type = 'All others', wwr_building_type = 'All others', swh_building_type = 'All others')
       # Initialize weather file, necessary but not used
       epw_file = 'USA_FL_Miami.Intl.AP.722020_TMY3.epw'
+
+      # Set folder for JSON files related to tests
+      @json_dir = "#{Dir.pwd}/data"
 
       # Create output folder if it doesn't already exist
       @test_dir = "#{Dir.pwd}/output"
@@ -46,10 +49,11 @@ class DOEPrototypeBaseline < CreateDOEPrototypeBuildingTest
       prototype_creator = Standard.build("90.1-PRM-2019")
 
       # Convert standardSpaceType string for each space to values expected for prm creation
+      lpd_space_types = JSON.parse(File.read("#{@json_dir}/lpd_space_types.json"))
       model.getSpaceTypes.sort.each do |space_type|
         next if space_type.floorArea == 0
         standards_space_type = if space_type.standardsSpaceType.is_initialized
-          space_type.standardsSpaceType.get
+        space_type.standardsSpaceType.get
         end
         std_bldg_type = space_type.standardsBuildingType.get
         bldg_type_space_type = std_bldg_type + space_type.standardsSpaceType.get
@@ -67,6 +71,9 @@ class DOEPrototypeBaseline < CreateDOEPrototypeBuildingTest
       @templates = ['90.1-2013']
       @building_types = ['SmallOffice','MidriseApartment']
       @climate_zones = ['ASHRAE 169-2013-2A']
+
+      # Set folder for JSON files related to tests
+      @json_dir = "#{Dir.pwd}/data"
 
       wwr_building_types = {
         'HighriseApartment' => 'All others',
@@ -163,225 +170,12 @@ class DOEPrototypeBaseline < CreateDOEPrototypeBuildingTest
         'Warehouse' => 'false'
       }
 
-      lpd_space_types = {
-        'AnyPlenum' => 'plenum',
-        'FullServiceRestaurantKitchen' => 'kitchen',
-        'FullServiceRestaurantDining' => 'dining - family',
-        'FullServiceRestaurantAttic' => 'attic - unoccupied',
-        'HighriseApartmentApartment' => 'apartment - hardwired',
-        'HighriseApartmentApartment_topfloor_NS' => 'apartment - hardwired',
-        'HighriseApartmentApartment_topfloor_WE' => 'apartment - hardwired',
-        'HighriseApartmentCorridor' => 'corridor - all other',
-        'HighriseApartmentCorridor_topfloor' => 'corridor - all other',
-        'HighriseApartmentOffice' => 'office - enclosed <= 250 sf',
-        'HospitalBasement' => 'office - whole building',
-        'HospitalCorridor' => 'corridor - hospital',
-        'HospitalDining' => 'dining - all other',
-        'HospitalER_Exam' => 'emergency room',
-        'HospitalER_NurseStn' => 'nurses station',
-        'HospitalER_Trauma' => 'emergency room',
-        'HospitalER_Triage' => 'emergency room',
-        'HospitalICU_NurseStn' => 'nurses station',
-        'HospitalICU_Open' => 'recovery',
-        'HospitalICU_PatRm' => 'patient room',
-        'HospitalLab' => 'laboratory',
-        'HospitalLobby' => 'lobby - all other',
-        'HospitalNurseStn' => 'nurses station',
-        'HospitalOR' => 'operating room',
-        'HospitalPatCorridor' => 'corridor - hospital',
-        'HospitalPatRoom' => 'patient room',
-        'HospitalPhysTherapy' => 'physical therapy',
-        'HospitalRadiology' => 'laboratory',
-        'HospitalKitchen' => 'kitchen',
-        'HospitalOffice' => 'office - enclosed <= 250 sf',
-        'LaboratoryLab with fume hood' => 'laboratory',
-        'LaboratoryEquipment corridor' => 'corridor - all other',
-        'LaboratoryOpen lab' => 'laboratory',
-        'LaboratoryOffice' => 'office - open',
-        'LargeDataCenterHighITEStandaloneDataCenter' => 'office - open',
-        'LargeDataCenterLowITEStandaloneDataCenter' => 'office - open',
-        'LargeHotelCafe' => 'dining - bar/lounge/leisure',
-        'LargeHotelCorridor' => 'corridor - all other',
-        'LargeHotelGuestRoom' => 'guest room',
-        'LargeHotelLaundry' => 'laundry/washing',
-        'LargeHotelLobby' => 'lobby - hotel',
-        'LargeHotelMechanical' => 'electrical/mechanical',
-        'LargeHotelStorage' => 'storage 50 to 1000 sf - all other',
-        'LargeHotelKitchen' => 'kitchen',
-        'LargeHotelBanquet' => 'dining - bar/lounge/leisure',
-        'LargeHotelBasement' => 'office - whole building',
-        'LargeHotelCorridor2' => 'corridor - all other',
-        'LargeHotelGuestRoom2' => 'guest room',
-        'LargeHotelGuestRoom3' => 'guest room',
-        'LargeHotelGuestRoom4' => 'guest room',
-        'LargeHotelRetail' => 'retail - whole building',
-        'LargeHotelRetail2' => 'retail - whole building',
-        'LargeOfficeRetail' => 'sales',
-        'MidriseApartmentApartment' => 'apartment - hardwired',
-        'MidriseApartmentApartment_topfloor_NS' => 'apartment - hardwired',
-        'MidriseApartmentApartment_topfloor_WE' => 'apartment - hardwired',
-        'MidriseApartmentCorridor' => 'corridor - all other',
-        'MidriseApartmentCorridor_topfloor' => 'corridor - all other',
-        'MidriseApartmentOffice' => 'office - enclosed <= 250 sf',
-        'OfficeWholeBuilding - Sm Office' => 'office - whole building',
-        'OfficeWholeBuilding - Md Office' => 'office - whole building',
-        'OfficeWholeBuilding - Lg Office' => 'office - whole building',
-        'OfficeVending' => 'corridor - all other',
-        'OfficeStorage' => 'storage 50 to 1000 sf - all other',
-        'OfficeStair' => 'stairwell',
-        'OfficeRestroom' => 'restroom - all other',
-        'OfficePrintRoom' => 'office - enclosed <= 250 sf',
-        'OfficeOpenOffice' => 'office - open',
-        'OfficeLobby' => 'lobby - all other',
-        'OfficeIT_Room' => 'office - enclosed <= 250 sf',
-        'OfficeElec/MechRoom' => 'electrical/mechanical',
-        'OfficeCorridor' => 'corridor - all other',
-        'OfficeConference' => 'conference/meeting/multipurpose',
-        'OfficeClosedOffice' => 'office - enclosed <= 250 sf',
-        'OfficeBreakRoom' => 'lounge/breakroom - all other',
-        'OfficeAttic' => 'attic - unoccupied',
-        'OfficeOfficeLarge Data Center' => 'office - open',
-        'OfficeOfficeLarge Main Data Center' => 'office - open',
-        'OfficeDining' => 'dining - all other',
-        'OfficeClassroom' => 'classroom/lecture/training - all other',
-        'OfficeMediumOffice - Storage' => 'storage 50 to 1000 sf - all other',
-        'OfficeMediumOffice - Stair' => 'stairwell',
-        'OfficeMediumOffice - Restroom' => 'restroom - all other',
-        'OfficeMediumOffice - OpenOffice' => 'office - open',
-        'OfficeMediumOffice - Lobby' => 'lobby - all other',
-        'OfficeMediumOffice - Elec/MechRoom' => 'electrical/mechanical',
-        'OfficeMediumOffice - Corridor' => 'corridor - all other',
-        'OfficeMediumOffice - Conference' => 'conference/meeting/multipurpose',
-        'OfficeMediumOffice - ClosedOffice' => 'office - enclosed <= 250 sf',
-        'OfficeMediumOffice - Breakroom' => 'lounge/breakroom - all other',
-        'OfficeMediumOffice - Dining' => 'dining - all other',
-        'OfficeMediumOffice - Classroom' => 'classroom/lecture/training - all other',
-        'OfficeSmallOffice - Attic' => 'attic - unoccupied',
-        'OfficeSmallOffice - Storage' => 'storage 50 to 1000 sf - all other',
-        'OfficeSmallOffice - Stair' => 'stairwell',
-        'OfficeSmallOffice - Restroom' => 'restroom - all other',
-        'OfficeSmallOffice - OpenOffice' => 'office - open',
-        'OfficeSmallOffice - Lobby' => 'lobby - all other',
-        'OfficeSmallOffice - Elec/MechRoom' => 'electrical/mechanical',
-        'OfficeSmallOffice - Corridor' => 'corridor - all other',
-        'OfficeSmallOffice - Conference' => 'conference/meeting/multipurpose',
-        'OfficeSmallOffice - ClosedOffice' => 'office - enclosed <= 250 sf',
-        'OfficeSmallOffice - Breakroom' => 'lounge/breakroom - all other',
-        'OfficeSmallOffice - Dining' => 'dining - all other',
-        'OfficeSmallOffice - Classroom' => 'classroom/lecture/training - all other',
-        'OfficeWholeBuilding - Lg Office-others' => 'office - whole building',
-        'OfficeWholeBuilding - Lg Office-basement' => 'office - whole building',
-        'OutpatientAnesthesia' => 'exam/treatment',
-        'OutpatientBioHazard' => 'storage 50 to 1000 sf - hospital',
-        'OutpatientCafe' => 'dining - all other',
-        'OutpatientCleanWork' => 'exam/treatment',
-        'OutpatientConference' => 'conference/meeting/multipurpose',
-        'OutpatientDressingRoom' => 'office - enclosed <= 250 sf',
-        'OutpatientElec/MechRoom' => 'electrical/mechanical',
-        'OutpatientElevatorPumpRoom' => 'electrical/mechanical',
-        'OutpatientExam' => 'exam/treatment',
-        'OutpatientHall' => 'corridor - hospital',
-        'OutpatientHall_infil' => 'corridor - hospital',
-        'OutpatientIT_Room' => 'office - enclosed <= 250 sf',
-        'OutpatientJanitor' => 'storage 50 to 1000 sf - hospital',
-        'OutpatientLobby' => 'lobby - all other',
-        'OutpatientLockerRoom' => 'lounge/breakroom - healthcare facility',
-        'OutpatientLounge' => 'lounge/breakroom - healthcare facility',
-        'OutpatientMedGas' => 'storage 50 to 1000 sf - hospital',
-        'OutpatientMRI' => 'laboratory',
-        'OutpatientMRI_Control' => 'laboratory',
-        'OutpatientNurseStation' => 'nurses station',
-        'OutpatientOffice' => 'office - enclosed <= 250 sf',
-        'OutpatientOR' => 'operating room',
-        'OutpatientPACU' => 'recovery',
-        'OutpatientPhysicalTherapy' => 'physical therapy',
-        'OutpatientPreOp' => 'patient room',
-        'OutpatientProcedureRoom' => 'emergency room',
-        'OutpatientReception' => 'lobby - all other',
-        'OutpatientStair' => 'corridor - hospital',
-        'OutpatientToilet' => 'restroom - all other',
-        'OutpatientUndeveloped' => 'storage 50 to 1000 sf - hospital',
-        'OutpatientXray' => 'laboratory',
-        'OutpatientSoil Work' => 'exam/treatment',
-        'PrimarySchoolCafeteria' => 'dining - all other',
-        'PrimarySchoolClassroom' => 'classroom/lecture/training - all other',
-        'PrimarySchoolComputerRoom' => 'classroom/lecture/training - all other',
-        'PrimarySchoolCorridor' => 'corridor - all other',
-        'PrimarySchoolGym' => 'gymnsasium exercise area',
-        'PrimarySchoolKitchen' => 'kitchen',
-        'PrimarySchoolLibrary' => 'library - whole building',
-        'PrimarySchoolLobby' => 'lobby - all other',
-        'PrimarySchoolMechanical' => 'electrical/mechanical',
-        'PrimarySchoolOffice' => 'office - enclosed <= 250 sf',
-        'PrimarySchoolRestroom' => 'restroom - all other',
-        'QuickServiceRestaurantDining' => 'dining - family',
-        'QuickServiceRestaurantAttic' => 'attic - unoccupied',
-        'QuickServiceRestaurantKitchen' => 'kitchen',
-        'RetailBack_Space' => 'storage 50 to 1000 sf - all other',
-        'RetailEntry' => 'lobby - all other',
-        'RetailPoint_of_Sale' => 'sales',
-        'RetailRetail' => 'sales',
-        'SecondarySchoolAuditorium' => 'audience seating - auditorium',
-        'SecondarySchoolCafeteria' => 'dining - all other',
-        'SecondarySchoolClassroom' => 'classroom/lecture/training - all other',
-        'SecondarySchoolComputerRoom' => 'classroom/lecture/training - all other',
-        'SecondarySchoolCorridor' => 'corridor - all other',
-        'SecondarySchoolGym' => 'gymnasium playing area',
-        'SecondarySchoolLibrary' => 'library - whole building',
-        'SecondarySchoolLobby' => 'lobby - all other',
-        'SecondarySchoolOffice' => 'office - enclosed <= 250 sf',
-        'SecondarySchoolRestroom' => 'restroom - all other',
-        'SecondarySchoolKitchen' => 'kitchen',
-        'SecondarySchoolMechanical' => 'electrical/mechanical',
-        'SmallDataCenterHighITEComputerRoom' => 'office - open',
-        'SmallDataCenterLowITEComputerRoom' => 'office - open',
-        'SmallHotelCorridor' => 'corridor - all other',
-        'SmallHotelCorridor4' => 'corridor - all other',
-        'SmallHotelElec/MechRoom' => 'electrical/mechanical',
-        'SmallHotelElevatorCore' => 'elevator core',
-        'SmallHotelElevatorCore4' => 'elevator core',
-        'SmallHotelExercise' => 'gymnsasium exercise area',
-        'SmallHotelGuestRoom123Occ' => 'guest room',
-        'SmallHotelGuestRoom123Vac' => 'guest room',
-        'SmallHotelGuestRoom4Occ' => 'guest room',
-        'SmallHotelGuestRoom4Vac' => 'guest room',
-        'SmallHotelLaundry' => 'laundry/washing',
-        'SmallHotelMechanical' => 'electrical/mechanical',
-        'SmallHotelMeeting' => 'conference/meeting/multipurpose',
-        'SmallHotelOffice' => 'office - enclosed <= 250 sf',
-        'SmallHotelPublicRestroom' => 'restroom - all other',
-        'SmallHotelStaffLounge' => 'lounge/breakroom - all other',
-        'SmallHotelStair' => 'stairwell',
-        'SmallHotelStair4' => 'stairwell',
-        'SmallHotelStorage' => 'storage 50 to 1000 sf - all other',
-        'SmallHotelStorage4Front' => 'storage 50 to 1000 sf - all other',
-        'SmallHotelStorage4Rear' => 'storage 50 to 1000 sf - all other',
-        'SmallHotelGuestLounge' => 'lobby - hotel',
-        'StripMallStrip mall - type 1' => 'retail - whole building',
-        'StripMallStrip mall - type 2' => 'retail - whole building',
-        'StripMallStrip mall - type 3' => 'retail - whole building',
-        'SuperMarketMeeting' => 'conference/meeting/multipurpose',
-        'SuperMarketDining' => 'dining - cafeteria/fast food',
-        'SuperMarketRestroom' => 'restroom - all other',
-        'SuperMarketElec/MechRoom' => 'electrical/mechanical',
-        'SuperMarketCorridor' => 'corridor - all other',
-        'SuperMarketDeli' => 'kitchen',
-        'SuperMarketBakery' => 'kitchen',
-        'SuperMarketVestibule' => 'lobby - all other',
-        'SuperMarketSales' => 'sales',
-        'SuperMarketProduce' => 'sales',
-        'SuperMarketDryStorage' => 'storage 50 to 1000 sf - all other',
-        'SuperMarketOffice' => 'office - enclosed <= 250 sf',
-        'WarehouseOffice' => 'office - enclosed <= 250 sf',
-        'WarehouseFine' => 'warehouse - fine storage',
-        'WarehouseBulk' => 'warehouse - bulk storage'
-      }
 
       all_comp =  @building_types.product @templates, @climate_zones
       all_comp.each do |building_type, template, climate_zone|
 
         # Generate prototype building models and associated baselines
-        model_baseline, model = DOEPrototypeBaseline.generate_prototype_model_and_baseline(building_type, template, climate_zone, hvac_building_types[building_type], wwr_building_types[building_type], swh_building_types[building_type],lpd_space_types)
+        model_baseline, model = DOEPrototypeBaseline.generate_prototype_model_and_baseline(building_type, template, climate_zone, hvac_building_types[building_type], wwr_building_types[building_type], swh_building_types[building_type])
         assert(model_baseline,"Baseline model could not be generated for #{building_type}, #{template}, #{climate_zone}.")
 
         # Load baseline model
