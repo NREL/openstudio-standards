27093036f41d87d95cfef5c9b908b619462c99ad
Ye <yunyang.ye@pnnl.gov>
2020-06-15 11:30:30 -0700

    [90.1-AppG] Update lighting in baseline

--- lib/openstudio-standards/standards/Standards.Model.rb
+++ lib/openstudio-standards/standards/Standards.Model.rb
@@ -60,6 +60,7 @@ class Standard
 
     # Modify the internal loads in each space type, keeping user-defined schedules.
     OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.Model', '*** Changing Lighting Loads ***')
+
     model.getSpaceTypes.sort.each do |space_type|
       set_people = false
       set_lights = true
@@ -67,7 +68,11 @@ class Standard
       set_gas_equipment = false
       set_ventilation = false
       set_infiltration = false
-      space_type_apply_internal_loads(space_type, set_people, set_lights, set_electric_equipment, set_gas_equipment, set_ventilation, set_infiltration)
+      if /prm/i =~ template
+        space_type_apply_int_loads_prm(space_type, model)
+      elsif
+        space_type_apply_internal_loads(space_type, set_people, set_lights, set_electric_equipment, set_gas_equipment, set_ventilation, set_infiltration)
+      end
     end
 
     # If any of the lights are missing schedules, assign an always-off schedule to those lights.
