eaba4b60c855b1626830509e8d51e642cd53e80d
carlo <carlo.bianchi@utah.edu>
2020-08-11 21:45:23 +0200

    fixed SetRestockingSchedule for refrigerated cases

--- lib/openstudio-standards/prototypes/common/objects/Prototype.refrigeration.rb
+++ lib/openstudio-standards/prototypes/common/objects/Prototype.refrigeration.rb
@@ -65,15 +65,7 @@ class Standard
         anti_sweat_heater_control = props['anti_sweat_heater_control']
       end
     end
-    if props['restocking_schedule']
-      if props['restocking_schedule'].downcase == 'always off'
-        restocking_sch = model.alwaysOffDiscreteSchedule
-      else
-        restocking_sch = model_add_schedule(model, props['restocking_schedule'])
-      end
-    else
-      restocking_sch = model.alwaysOffDiscreteSchedule
-    end
+
     if props['fractionofantisweatheaterenergytocase']
       fractionofantisweatheaterenergytocase = props['fractionofantisweatheaterenergytocase']
     end
@@ -128,7 +120,19 @@ class Standard
     else
       ref_case.setUnderCaseHVACReturnAirFraction(0)
     end
-    ref_case.setRefrigeratedCaseRestockingSchedule(restocking_sch)
+    #-------------
+    if props['restocking_schedule']
+      if props['restocking_schedule'].downcase == 'always off'
+        # restocking_sch = model.alwaysOffDiscreteSchedule
+        ref_case.resetRefrigeratedCaseRestockingSchedule()
+      else
+        restocking_sch = model_add_schedule(model, props['restocking_schedule'])
+        ref_case.setRefrigeratedCaseRestockingSchedule(restocking_sch)
+      end
+    else
+      ref_case.resetRefrigeratedCaseRestockingSchedule()
+    end
+    #--------------
 
     if props['case_category']
       ref_case_addprops = ref_case.additionalProperties
