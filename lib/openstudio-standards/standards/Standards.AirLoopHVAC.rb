4b7a9c9e2efe1742f00e59b7822b8427a3bccab7
mdahlhausen <matthew.dahlhausen@gmail.com>
2020-09-08 20:15:54 -0600

    heat recovery and economizer changes
    - moves the fixed_dry_bulb_high_limit_shutoff_temp variable to the spreadsheet and .json files, rather than being included in a long if-else statement in the code
    - moves the energy recovery logic to a new sheet

--- lib/openstudio-standards/standards/Standards.AirLoopHVAC.rb
+++ lib/openstudio-standards/standards/Standards.AirLoopHVAC.rb
@@ -1015,53 +1015,12 @@ class Standard
     when 'NoEconomizer'
       return [nil, nil, nil]
     when 'FixedDryBulb'
-      case climate_zone
-      when 'ASHRAE 169-2006-0B',
-          'ASHRAE 169-2006-1B',
-          'ASHRAE 169-2006-2B',
-          'ASHRAE 169-2006-3B',
-          'ASHRAE 169-2006-3C',
-          'ASHRAE 169-2006-4B',
-          'ASHRAE 169-2006-4C',
-          'ASHRAE 169-2006-5B',
-          'ASHRAE 169-2006-5C',
-          'ASHRAE 169-2006-6B',
-          'ASHRAE 169-2006-7B',
-          'ASHRAE 169-2006-8A',
-          'ASHRAE 169-2006-8B',
-          'ASHRAE 169-2013-0B',
-          'ASHRAE 169-2013-1B',
-          'ASHRAE 169-2013-2B',
-          'ASHRAE 169-2013-3B',
-          'ASHRAE 169-2013-3C',
-          'ASHRAE 169-2013-4B',
-          'ASHRAE 169-2013-4C',
-          'ASHRAE 169-2013-5B',
-          'ASHRAE 169-2013-5C',
-          'ASHRAE 169-2013-6B',
-          'ASHRAE 169-2013-7B',
-          'ASHRAE 169-2013-8A',
-          'ASHRAE 169-2013-8B'
-        drybulb_limit_f = 75
-      when 'ASHRAE 169-2006-5A',
-          'ASHRAE 169-2006-6A',
-          'ASHRAE 169-2006-7A',
-          'ASHRAE 169-2013-5A',
-          'ASHRAE 169-2013-6A',
-          'ASHRAE 169-2013-7A'
-        drybulb_limit_f = 70
-      when 'ASHRAE 169-2006-0A',
-          'ASHRAE 169-2006-1A',
-          'ASHRAE 169-2006-2A',
-          'ASHRAE 169-2006-3A',
-          'ASHRAE 169-2006-4A',
-          'ASHRAE 169-2013-0A',
-          'ASHRAE 169-2013-1A',
-          'ASHRAE 169-2013-2A',
-          'ASHRAE 169-2013-3A',
-          'ASHRAE 169-2013-4A'
-        drybulb_limit_f = 65
-      end
+      search_criteria = {
+          'template' => template,
+          'climate_zone' => climate_zone
+      }
+      econ_limits = model_find_object(standards_data['economizers'], search_criteria)
+      drybulb_limit_f = econ_limits['fixed_dry_bulb_high_limit_shutoff_temp']
     when 'FixedEnthalpy'
       enthalpy_limit_btu_per_lb = 28
     when 'FixedDewPointAndDryBulb'
