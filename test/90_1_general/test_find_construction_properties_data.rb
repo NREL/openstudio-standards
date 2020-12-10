d3beacbb7ec63915db02a671b4866e950b30ebda
XXX <ajszilasi@gmail.com>
2020-12-10 16:14:21 -0700

    Adding own test script for speed constructions

--- test/90_1_general/test_find_construction_properties_data.rb
+++ test/90_1_general/test_find_construction_properties_data.rb
@@ -5,7 +5,7 @@ class TestFindConstructionPropertiesData < Minitest::Test
   def test_find_construction_properties_data
 
     # for now this just runs for single building type and climate zone, but could sweep across larger selection
-    template = '90.1-2013'
+	templates = ['90.1-2007', '90.1-2010', '90.1-2013']
     standard_building_type = 'LargeHotel'
     standard_space_type = 'GuestRoom'
     intended_surface_type = 'ExteriorWall'
@@ -25,15 +25,25 @@ class TestFindConstructionPropertiesData < Minitest::Test
     # set climate zone
     climateZones = model.getClimateZones
     climateZones.setClimateZone("ASHRAE",test_climate_zone)
+	
+	
+	templates.each do |template|
 
-    # lookup standards data for space type
-    data = standard.space_type_get_construction_properties(space_type, intended_surface_type,standards_construction_type)
+		# lookup standards data for space type
+		data = standard.space_type_get_construction_properties(space_type, intended_surface_type,standards_construction_type)
+		if data.nil? then puts "#{space_type}  #{intended_surface_type} #{standards_construction_type} data is nil qaqc measure wont work with this " end
 
-    # gather specific internal load values for testing
-    u_value = data['assembly_maximum_u_value']
+		# gather specific internal load values for testing
+		u_value = data['assembly_maximum_u_value']
+		
+		space_type_const_properties['ExteriorRoof']['u_value']
+		space_type_const_properties['ExteriorWall']['u_value']
+		space_type_const_properties[intended_surface_type]['u_value']
+		space_type_const_properties[intended_surface_type]['shgc']
 
-    # check various internal loads. This has ip values
-    assert_in_delta(u_value.to_f, 0.064)
+		# check various internal loads. This has ip values
+		assert_in_delta(u_value.to_f, 0.064)
+	end
 
   end
 
