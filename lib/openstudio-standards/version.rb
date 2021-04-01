170384072557711aae8c82121a0a157509310fec
ckirney <36897585+ckirney@users.noreply.github.com>
2021-04-01 13:13:08 -0400

    Changing version back for nrcan use

--- lib/openstudio-standards/version.rb
+++ lib/openstudio-standards/version.rb
@@ -13,5 +13,7 @@ module OpenstudioStandards
     end
     return 'git-not-found-on-this-system'
   end
-  VERSION = '0.2.13.rc2'.freeze
+  #this should be updated to 0.2.12 when merging to 3.2.0
+  #VERSION = '0.2.13.rc2'.freeze
+  VERSION = '0.2.11'.freeze
 end
