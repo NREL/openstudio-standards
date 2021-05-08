0f6b474a7acf02d64d8cbd6fedd234066777ed3e
phylroy <phylroy.lopez@canada.ca>
2021-05-08 17:36:17 +0000

    adding better string float trans

--- lib/openstudio-standards/standards/necb/ECMS/loads.rb
+++ lib/openstudio-standards/standards/necb/ECMS/loads.rb
@@ -2,8 +2,15 @@ class ECMS
 
   # Occupancy
   def scale_occupancy_loads(model: , scale: 'NECB_Default' )
+    ##### Remove leading or trailing whitespace in case users add them in inputs
+    if scale.instance_of?(String)
+      scale = scale.strip
+    end
     return model if scale == 'NECB_Default' or scale.nil?
-    scale = scale.to_f
+    ##### Convert a string to a float
+    if scale.instance_of?(String)
+      scale = scale.to_f
+    end
     if scale == 0.0
       model.getPeoples.sort.each {|people| people.remove}
       model.getPeopleDefinitions.sort.each {|people| people.remove}
@@ -16,8 +23,15 @@ class ECMS
 
   # Electrical
   def scale_electrical_loads(model:, scale: 'NECB_Default')
+    ##### Remove leading or trailing whitespace in case users add them in inputs
+    if scale.instance_of?(String)
+      scale = scale.strip
+    end
     return model if scale == 'NECB_Default' or scale.nil?
-    scale = scale.to_f
+    ##### Convert a string to a float
+    if scale.instance_of?(String)
+      scale = scale.to_f
+    end
     if scale == 0.0
       model.getElectricEquipments.sort.each {|item| item.remove}
       model.getElectricEquipmentDefinitions.sort.each {|item| item.remove}
@@ -30,8 +44,15 @@ class ECMS
 
   # Outdoor Air
   def scale_oa_loads(model: , scale:'NECB_Default')
+    ##### Remove leading or trailing whitespace in case users add them in inputs
+    if scale.instance_of?(String)
+      scale = scale.strip
+    end
     return model if scale == 'NECB_Default' or scale.nil?
-    scale = scale.to_f
+    ##### Convert a string to a float
+    if scale.instance_of?(String)
+      scale = scale.to_f
+    end
     if scale == 0.0
       model.getDesignSpecificationOutdoorAirs.sort.each {|item| item.remove}
     else
@@ -46,8 +67,15 @@ class ECMS
 
   # Infiltration
   def scale_infiltration_loads(model: , scale: 'NECB_Default')
+    ##### Remove leading or trailing whitespace in case users add them in inputs
+    if scale.instance_of?(String)
+      scale = scale.strip
+    end
     return model if scale == 'NECB_Default' or scale.nil?
-    scale = scale.to_f
+    ##### Convert a string to a float
+    if scale.instance_of?(String)
+      scale = scale.to_f
+    end
     if scale == 0.0
       model.getSpaceInfiltrationDesignFlowRates.sort.each {|item| item.remove}
     else
