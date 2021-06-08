d1fcf842a3481705a4608d0eae923e62d9e66011
plopez <phylroy.lopez@canada.ca>
2021-06-08 17:35:45 +0000

    added cli to standards

--- utilities/btap_cli/tests/btap_cli_local_osm_test.rb
+++ utilities/btap_cli/tests/btap_cli_local_osm_test.rb
@@ -2,15 +2,18 @@ require 'minitest/autorun'
 require 'openstudio'
 
 #if running from openstudio-standards.. load local standards
-if Dir.exist?('../../../lib/openstudio-standards.rb')
-  require_relative '../../../lib/openstudio-standards.rb'
+os_standards_local_lib_path = '../../../lib/openstudio-standards.rb'
+if Dir.exist?(os_standards_local_lib_path )
+  require_relative os_standards_local_lib_path
 else
   require 'openstudio-standards'
 end
 
 #load costing files if we are in btap_costing repository
-resource_folder = File.join(__dir__, '..', '..', 'measures/btap_results/resources')
+resource_folder = File.join(__dir__, '..','..', '..', 'measures/btap_results/resources')
+
 if Dir.exist?(resource_folder)
+  puts('Loading Costing libs')
   require_relative File.join(resource_folder, 'os_lib_reporting')
   require_relative File.join(resource_folder, 'os_lib_schedules')
   require_relative File.join(resource_folder, 'os_lib_helper_methods')
@@ -19,7 +22,6 @@ if Dir.exist?(resource_folder)
   require_relative File.join(resource_folder, 'ventilation_costing.rb')
   require_relative File.join(resource_folder, 'envelope_costing.rb')
   require_relative File.join(resource_folder, 'lighting_costing.rb')
-  require_relative File.join(resource_folder, 'heating_cooling_costing.rb')
   require_relative File.join(resource_folder, 'heating_cooling_costing.rb')
   require_relative File.join(resource_folder, 'shw_costing.rb')
   require_relative File.join(resource_folder, 'btap_data.rb')
