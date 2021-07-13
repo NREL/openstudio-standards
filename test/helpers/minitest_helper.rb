e61abc9d817394b44240f385ad363af553c135d0
Chris Kirney <chris.kirney@canada.ca>
2021-07-13 19:21:29 +0000

    Committing changes suggested by rubocop inculuding a pump power fix.

--- test/helpers/minitest_helper.rb
+++ test/helpers/minitest_helper.rb
@@ -49,8 +49,9 @@ end
 # Set the output reporting format based on the run environment
 if ENV['RM_INFO'] || ENV['TEAMCITY_RAKE_RUNNER_MODE'] # RubyMine
   puts "Running tests from RubyMine, using RubyMine test reporter."
-  ENV.delete('RM_INFO') # Delete this environment variable because it forces use of only RubyMineReporter
-  Minitest::Reporters.use! [Minitest::Reporters::RubyMineReporter.new]
+  #ENV.delete('RM_INFO') # Delete this environment variable because it forces use of only RubyMineReporter
+  Minitest::Reporters.use! [Minitest::Reporters::RubyMineReporter.new] unless ENV['RM_INFO']
+  #Minitest::Reporters.use! [Minitest::Reporters::RubyMineReporter.new]
 elsif ENV['JENKINS_HOME'] # Jenkins
   puts "Running tests from Jenkins, using JUnit XML test reporter and console-based test reporter."
   Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new(reports_dir = "test/reports", empty = false)]
