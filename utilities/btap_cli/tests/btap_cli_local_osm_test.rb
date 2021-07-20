db47bd5302772979baa54350e5f21b4d8e0f3204
Chris Kirney <chris.kirney@canada.ca>
2021-07-20 18:20:50 +0000

    Fixing remaining rubocop errors (except for a few I didn't want to touch for fear of breaking something).

--- utilities/btap_cli/tests/btap_cli_local_osm_test.rb
+++ utilities/btap_cli/tests/btap_cli_local_osm_test.rb
@@ -1,18 +1,16 @@
 require_relative '../libs.rb'
 class BTAP_CLI_Test < Minitest::Test
-
-  def test_cli_local_osm()
-    input_folder = File.join(__dir__,'..','input')
-    input_folder_cache = File.join(__dir__,'..','input_cache')
-    output_folder = File.join(__dir__,'..','output')
+  def test_cli_local_osm
+    input_folder = File.join(__dir__, '..', 'input')
+    input_folder_cache = File.join(__dir__, '..', 'input_cache')
+    output_folder = File.join(__dir__, '..', 'output')
     # Make sure temp folder is always clean.
     FileUtils.rm_rf(input_folder) if Dir.exist?(input_folder)
     FileUtils.rm_rf(input_folder_cache) if Dir.exist?(input_folder_cache)
     FileUtils.rm_rf(output_folder) if Dir.exist?(output_folder)
     FileUtils.mkdir_p(input_folder)
-    FileUtils.cp(File.join(__dir__, 'run_options_local_osm.yml'), File.join(input_folder,'run_options.yml'))
-    FileUtils.cp(File.join(__dir__, 'LocalFullServiceRestaurant.osm'), File.join(input_folder,'LocalFullServiceRestaurant.osm'))
+    FileUtils.cp(File.join(__dir__, 'run_options_local_osm.yml'), File.join(input_folder, 'run_options.yml'))
+    FileUtils.cp(File.join(__dir__, 'LocalFullServiceRestaurant.osm'), File.join(input_folder, 'LocalFullServiceRestaurant.osm'))
     BTAPDatapoint.new(input_folder: input_folder, output_folder: output_folder, input_folder_cache: File.join(__dir__, 'input_cache'))
   end
 end
-
