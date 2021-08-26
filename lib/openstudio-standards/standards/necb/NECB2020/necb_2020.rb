110883b36293c3cc429a69470c721b1f384b8d13
MacdonaldI <iain.macdonald@nrc-cnrc.gc.ca>
2021-08-26 15:56:34 -0400

    update to infiltration setting - convert from value at 75Pa and from overall area to above grade area

--- lib/openstudio-standards/standards/necb/NECB2020/necb_2020.rb
+++ lib/openstudio-standards/standards/necb/NECB2020/necb_2020.rb
@@ -84,5 +84,92 @@ class NECB2020 < NECB2017
 
     OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.SpaceType', "#{space_type.name} set LPD to #{lighting_per_area_led_lighting} W/ft^2.")
   end
+  
+  # Set the infiltration rate for this space to include
+  # the impact of air leakage requirements in the standard.
+  #
+  # Note that this is significantly different for NECB 2020 compared to previous codes.
+  #  The value is now specified at 75 Pa normalised by entire building surface area (previously 5 Pa 
+  #  and for above grade surfaces only). Need to convert to 5 Pa and for the different surface area.
+  #
+  # @return [Double] true if successful, false if not
+  # @todo handle doors and vestibules
+  def space_apply_infiltration_rate(space)
+  
+    # Remove infiltration rates set at the space type.
+    infiltration_data = @standards_data['infiltration']
+    unless space.spaceType.empty?
+      space.spaceType.get.spaceInfiltrationDesignFlowRates.each(&:remove)
+    end
+    # Remove infiltration rates set at the space object.
+    space.spaceInfiltrationDesignFlowRates.each(&:remove)
+
+    # Don't create an object if there is no exterior wall area.
+    exterior_wall_and_roof_and_subsurface_area = space_exterior_wall_and_roof_and_subsurface_area(space)
+    if exterior_wall_and_roof_and_subsurface_area <= 0.0
+      OpenStudio.logFree(OpenStudio::Info, 'openstudio.Standards.Model', "For #{template}, no exterior wall area was found in #{space.name}; no infiltration will be added.")
+      return true
+    end
+	
+    # Calculate total area of above and below grade envelope area in the entire model.
+    totalAreaBuildingEnvelope = 0.0
+    totalAboveGradeArea = 0.0
+	
+	space.model.getSpaces.each do |modelspace|
+	  multiplier = modelspace.multiplier
+	  modelspace.surfaces.each do |surface|
+	    if surface.outsideBoundaryCondition == "Outdoors" then
+		  area = surface.grossArea * multiplier
+          totalAreaBuildingEnvelope += area
+          totalAboveGradeArea += area
+		elsif surface.outsideBoundaryCondition == "Ground" then
+		  area = surface.grossArea * multiplier
+          totalAreaBuildingEnvelope += area
+		end
+	  end
+	end
+	
+	# Get infiltration rate from standards and convert to value at 5 Pa applied to all above grade surfaces.
+    infil_75Pa_all_surf = self.get_standards_constant('infiltration_rate_m3_per_s_per_m2')
+    infil_5Pa_above_grade = infil_75Pa_all_surf * ((5.0 / 75.0) ** (flow_exponent)) * totalAreaBuildingEnvelope / totalAboveGradeArea
+    OpenStudio.logFree(OpenStudio::Debug, 'openstudio.Standards.Space', "For #{space.name}, adj infil = #{infil_5Pa_above_grade.round(5)} m^3/s*m^2.")
+
+    # Get any infiltration schedule already assigned to this space or its space type
+    # If not, the always on schedule will be applied.
+    infil_sch = nil
+    unless space.spaceInfiltrationDesignFlowRates.empty?
+      old_infil = space.spaceInfiltrationDesignFlowRates[0]
+      if old_infil.schedule.is_initialized
+        infil_sch = old_infil.schedule.get
+      end
+    end
+
+    if infil_sch.nil? && space.spaceType.is_initialized
+      space_type = space.spaceType.get
+      unless space_type.spaceInfiltrationDesignFlowRates.empty?
+        old_infil = space_type.spaceInfiltrationDesignFlowRates[0]
+        if old_infil.schedule.is_initialized
+          infil_sch = old_infil.schedule.get
+        end
+      end
+    end
+
+    if infil_sch.nil?
+      infil_sch = space.model.alwaysOnDiscreteSchedule
+    end
+
+    # Create an infiltration rate object for this space.
+    infiltration = OpenStudio::Model::SpaceInfiltrationDesignFlowRate.new(space.model)
+    infiltration.setName("#{space.name} Infiltration")
+    infiltration.setFlowperExteriorSurfaceArea(infil_5Pa_above_grade)
+    infiltration.setSchedule(infil_sch)
+    infiltration.setConstantTermCoefficient(self.get_standards_constant('infiltration_constant_term_coefficient'))
+    infiltration.setTemperatureTermCoefficient(self.get_standards_constant('infiltration_constant_term_coefficient'))
+    infiltration.setVelocityTermCoefficient(self.get_standards_constant('infiltration_velocity_term_coefficient'))
+    infiltration.setVelocitySquaredTermCoefficient(self.get_standards_constant('infiltration_velocity_squared_term_coefficient'))
+    infiltration.setSpace(space)
+
+    return true
+  end
 
 end
