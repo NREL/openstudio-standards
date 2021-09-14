0fd7df82ad708956f4248099c4916ad833e7d4fd
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2021-09-14 08:14:55 -0700

    Missing formatting issue.

--- lib/openstudio-standards/standards/Standards.HeatExchangerSensLat.rb
+++ lib/openstudio-standards/standards/Standards.HeatExchangerSensLat.rb
@@ -43,44 +43,44 @@ class Standard
   # climate zones a 50% ERR at typical condition leads a ERR > 50%,
   # the ERR is thus scaled down.
   #
-  # @param err [float] Enthalpy Recovery Ratio (ERR)
+  # @param enthalpy_recovery_ratio [float] Enthalpy Recovery Ratio (ERR)
   # @param climate_zone [String] climate zone
   # @return [float] adjusted ERR
-  def enthalpy_recovery_ratio_design_to_typical_adjustment(err, climate_zone)
+  def enthalpy_recovery_ratio_design_to_typical_adjustment(enthalpy_recovery_ratio, climate_zone)
     if climate_zone.include? '2B'
-      err /= 0.65 / 0.55
+      enthalpy_recovery_ratio /= 0.65 / 0.55
     elsif climate_zone.include? '3B'
-      err /= 0.62 / 0.55
+      enthalpy_recovery_ratio /= 0.62 / 0.55
     end
 
-    return err
+    return enthalpy_recovery_ratio
   end
 
-  # Calculate a heat exchanger's effectiveness for a specific ERR and design basis.
+  # Calculate a heat exchanger's effectiveness for a specific ERR and design conditions.
   # Regressions were determined based available manufacturer data.
   #
   # @param enthalpy_recovery_ratio [float] Enthalpy Recovery Ratio (ERR)
   # @param design_conditions [String] design_conditions for effectiveness calculation, either 'cooling' or 'heating'
   # @return [Array] heating and cooling heat exchanger effectiveness at 100% and 75% nominal airflow
   def heat_exchanger_air_to_air_sensible_and_latent_enthalpy_recovery_ratio_to_effectiveness(enthalpy_recovery_ratio, design_conditions)
-    case basis
+    case design_conditions
       when 'cooling'
-        full_htg_sens_eff = (20.707 * err**2 + 41.354 * err + 40.755) / 100
-        full_htg_lat_eff = (127.45 * err - 18.625) / 100
-        part_htg_sens_eff = (-0.1214 * err + 1.111) * full_htg_sens_eff
-        part_htg_lat_eff = (-0.3405 * err + 1.2732) * full_htg_lat_eff
-        full_cool_sens_eff = (70.689 * err + 30.789) / 100
-        full_cool_lat_eff = (48.054 * err**2 + 83.082 * err - 12.881) / 100
-        part_cool_sens_eff = (-0.1214 * err + 1.111) * full_cool_sens_eff
-        part_cool_lat_eff = (-0.3982 * err  + 1.3151) * full_cool_lat_eff
+        full_htg_sens_eff = (20.707 * enthalpy_recovery_ratio**2 + 41.354 * enthalpy_recovery_ratio + 40.755) / 100
+        full_htg_lat_eff = (127.45 * enthalpy_recovery_ratio - 18.625) / 100
+        part_htg_sens_eff = (-0.1214 * enthalpy_recovery_ratio + 1.111) * full_htg_sens_eff
+        part_htg_lat_eff = (-0.3405 * enthalpy_recovery_ratio + 1.2732) * full_htg_lat_eff
+        full_cool_sens_eff = (70.689 * enthalpy_recovery_ratio + 30.789) / 100
+        full_cool_lat_eff = (48.054 * enthalpy_recovery_ratio**2 + 83.082 * enthalpy_recovery_ratio - 12.881) / 100
+        part_cool_sens_eff = (-0.1214 * enthalpy_recovery_ratio + 1.111) * full_cool_sens_eff
+        part_cool_lat_eff = (-0.3982 * enthalpy_recovery_ratio  + 1.3151) * full_cool_lat_eff
       when 'heating'
-        full_htg_sens_eff = err
+        full_htg_sens_eff = enthalpy_recovery_ratio
         full_htg_lat_eff = 0.0
-        part_htg_sens_eff = (-0.1214 * err + 1.111) * full_htg_sens_eff
+        part_htg_sens_eff = (-0.1214 * enthalpy_recovery_ratio + 1.111) * full_htg_sens_eff
         part_htg_lat_eff = 0.0
-        full_cool_sens_eff = err * (70.689 * err + 30.789) / (20.707 * err**2 + 41.354 * err + 40.755)
+        full_cool_sens_eff = enthalpy_recovery_ratio * (70.689 * enthalpy_recovery_ratio + 30.789) / (20.707 * enthalpy_recovery_ratio**2 + 41.354 * enthalpy_recovery_ratio + 40.755)
         full_cool_lat_eff = 0.0
-        part_cool_sens_eff = (-0.1214 * err + 1.111) * full_cool_sens_eff
+        part_cool_sens_eff = (-0.1214 * enthalpy_recovery_ratio + 1.111) * full_cool_sens_eff
         part_cool_lat_eff = 0.0
     end
 
