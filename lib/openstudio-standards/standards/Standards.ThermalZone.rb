af8963076950978be4463e0393df0c36e569e486
mdahlhausen <matthew.dahlhausen@gmail.com>
2021-09-21 12:27:53 -0600

    fix failing tests

--- lib/openstudio-standards/standards/Standards.ThermalZone.rb
+++ lib/openstudio-standards/standards/Standards.ThermalZone.rb
@@ -1117,7 +1117,14 @@ class Standard
       elsif equip.to_ZoneHVACLowTempRadiantVarFlow.is_initialized
         equip = equip.to_ZoneHVACLowTempRadiantVarFlow.get
         clg_coil = equip.coolingCoil
-        if clg_coil.to_CoilCoolingLowTempRadiantVarFlow.is_initialized
+        if clg_coil.model.version > OpenStudio::VersionString.new('3.1.0')
+          if clg_coil.is_initialized
+            clg_coil = clg_coil.get
+          else
+            clg_coil = nil
+          end
+        end
+        if !clg_coil.nil? && clg_coil.to_CoilCoolingLowTempRadiantVarFlow.is_initialized
           clg_coil = clg_coil.to_CoilCoolingLowTempRadiantVarFlow.get
           if clg_coil.coolingControlTemperatureSchedule.is_initialized
             clg_sch = clg_coil.coolingControlTemperatureSchedule.get
