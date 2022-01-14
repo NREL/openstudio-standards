c71f6d76520079055138dbc0e24a327355a6e113
Weili Xu <weili.xu@pnnl.gov>
2022-01-14 10:08:26 -0800

    patch fix - add code to extract sub climate zone code (A) from the climate zone list for PRM routine

--- lib/openstudio-standards/standards/ashrae_90_1_prm/ashrae_90_1_prm_2019/ashrae_90_1_prm_2019.Model.rb
+++ lib/openstudio-standards/standards/ashrae_90_1_prm/ashrae_90_1_prm_2019/ashrae_90_1_prm_2019.Model.rb
@@ -137,4 +137,17 @@ class ASHRAE901PRM2019 < ASHRAE901PRM
     srr_lim = 3.0
     return srr_lim
   end
+
+  # Determine which climate zone to use.
+  # Uses the most specific climate zone set for most
+  # climate zones, except for ClimateZone 3, which
+  # uses the least specific climate zone.
+  def model_get_climate_zone_set_from_list(model, possible_climate_zone_sets)
+    climate_zone_set = if possible_climate_zone_sets.include? 'ClimateZone 3'
+                         possible_climate_zone_sets.max
+                       else
+                         possible_climate_zone_sets.min
+                       end
+    return climate_zone_set
+  end
 end
