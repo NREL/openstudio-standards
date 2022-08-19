dbdadd071d09b4b86ec5e10dc721474b09445aa3
edwardv720 <vuong@ds.nrc.ca>
2022-08-19 15:11:28 -0400

    update SHW rules to NECB2020

--- lib/openstudio-standards/standards/necb/NECB2020/service_water_heating.rb
+++ lib/openstudio-standards/standards/necb/NECB2020/service_water_heating.rb
@@ -7,7 +7,7 @@ class NECB2020
   #
   # @return [Bool] true if successful, false if not
   #
-  # This was modified in PCF 1630
+  # NECB2020 uses a different procedure calculate gas water heater efficiencies (compared to previous NECB)
   #
   def water_heater_mixed_apply_efficiency(water_heater_mixed)
     # Get the capacity of the water heater
@@ -34,7 +34,7 @@ class NECB2020
       volume_m3 = volume_m3.get
     end
     volume_gal = OpenStudio.convert(volume_m3, 'm^3', 'gal').get
-
+    volume_litre = OpenStudio.convert(volume_m3, 'm^3', 'L').get
     # Get the heater fuel type
     fuel_type = water_heater_mixed.heaterFuelType
     unless fuel_type == 'NaturalGas' || fuel_type == 'Electricity' || fuel_type == 'FuelOilNo2'
@@ -46,21 +46,23 @@ class NECB2020
     # Calculate the energy factor (EF)
     # From PNNL http://www.energycodes.gov/sites/default/files/documents/PrototypeModelEnhancements_2014_0.pdf
     # Appendix A: Service Water Heating
-	# and modified by PCF 1630 as noted below.
+	  # and modified by PCF 1630 as noted below.
     water_heater_eff = nil
     ua_btu_per_hr_per_f = nil
     sl_btu_per_hr = nil
+    q_load_btu_per_hr = nil
+    uef = nil
     case fuel_type
     when 'Electricity'
-      volume_l_per_s = volume_m3 * 1000
+      volume_litre_per_s = volume_m3 * 1000
       if capacity_btu_per_hr <= OpenStudio.convert(12, 'kW', 'Btu/hr').get
         # Fixed water heater efficiency per PNNL
         water_heater_eff = 1
         # Calculate the max allowable standby loss (SL)
-        sl_w = if volume_l_per_s < 270
-                 40 + 0.2 * volume_l_per_s # assume bottom inlet
+        sl_w = if volume_litre_per_s < 270
+                 40 + 0.2 * volume_litre_per_s # assume bottom inlet
                else
-                 0.472 * volume_l_per_s - 33.5
+                 0.472 * volume_litre_per_s - 33.5
                  # assume bottom inlet
                end
         sl_btu_per_hr = OpenStudio.convert(sl_w, 'W', 'Btu/hr').get
@@ -73,56 +75,107 @@ class NECB2020
       # Calculate the skin loss coefficient (UA)
       ua_btu_per_hr_per_f = sl_btu_per_hr / 70
     when 'NaturalGas'
-      volume_l = volume_m3 / 1000
-      if capacity_btu_per_hr <= 75_000
-        # Fixed water heater thermal efficiency per PNNL
-        water_heater_eff = 0.82
-		
-        # Calculate the minimum Energy Factor (EF) (This was introduced in PCF 1630)
-        if volume_l < 68
-          uef = 0.5982 - 0.0005 * volume_l
-          ef = 1.0005 * uef + 0.0019
-        elsif volume_l >= 68 and volume_l < 193
-          uef = 0.6483 - 0.00045 * volume_l
-          ef = 1.0005 * uef + 0.0019
-        elsif volume_l >= 193 and volume_l < 284
-          uef = 0.692 - 0.00034 * volume_l
-          ef = 1.0005 * uef + 0.0019
+      # Performance requirements from NECB2020 Table 6.2.2.1 Gas-fired storage type
+      
+      
+       
+      # Performance requirement based on FHR and volume
+      # Water heater parameters derived using the procedure described by:
+      #   Maguire, J., & Roberts, D. (2020). DERIVING SIMULATION PARAMETERS FOR STORAGE-TYPE WATER HEATERS 
+      #   USING RATINGS DATA PRODUCED FROM THE UNIFORM ENERGY FACTOR TEST PROCEDURE. 2020 Building Performance 
+      #   Analysis Conference and SimBuild co-organized by ASHRAE and IBPSA-USA (pp. 325-331). Chicago: ASHRAE.
+      #   https://www.ashrae.org/file%20library/conferences/specialty%20conferences/2020%20building%20performance/papers/d-bsc20-c039.pdf
+
+      # Assume fhr = peak demand flow
+      tank_param = auto_size_shw_capacity(model:water_heater_mixed.model, shw_scale: 'NECB_Default')
+      fhr = tank_param['loop_peak_flow_rate_SI']
+      fhr = fhr * 3600000
+      puts "fhr #{fhr}"
+      puts "volume_litre #{volume_litre}"
+      puts "capacity_w #{capacity_w}"
+      if capacity_w <= 22000 and volume_litre >= 76 and volume_litre < 208
+        if fhr < 68
+          uef = 0.3456 - 0.00053*volume_litre
+          puts "1"
+        elsif fhr >= 68 and fhr < 193
+          uef = 0.5982 - 0.00050*volume_litre
+          puts "2"
+        elsif fhr >= 193 and fhr < 284
+          uef = 0.6483 - 0.00045*volume_litre
+          puts "3"
+        elsif fhr >= 284 
+          uef = 0.6920 - 0.00034*volume_litre
+          puts "4"
         end
-		
-        # Calculate the Recovery Efficiency (RE)
-        # based on a fixed capacity of 75,000 Btu/hr
-        # and a fixed volume of 40 gallons by solving
-        # this system of equations:
-        # ua = (1/.95-1/re)/(67.5*(24/41094-1/(re*cap)))
-        # 0.82 = (ua*67.5+cap*re)/cap
-        cap = 75_000.0
-        re = (Math.sqrt(6724 * ef**2 * cap**2 + 40_409_100 * ef**2 * cap - 28_080_900 * ef * cap + 29_318_000_625 * ef**2 - 58_636_001_250 * ef + 29_318_000_625) + 82 * ef * cap + 171_225 * ef - 171_225) / (200 * ef * cap)
-        
-		# Calculate the skin loss coefficient (UA)
-        # based on the actual capacity.
-        ua_btu_per_hr_per_f = (water_heater_eff - re) * capacity_btu_per_hr / 67.5
-		
-      # This capacity band was introduced in PCF 1630
-      elsif capacity_btu_per_hr > 75_000 and capacity_btu_per_hr < 103977 and volume_l < 454
+
+        # Assume thermal efficiency  (Maguire and Roberts, 2020)
         water_heater_eff = 0.82
-        uef = 0.8107 - 0.00021 * volume_l
-        ef = 1.0005 * uef + 0.0019
-        cap = 103977
-        re = (Math.sqrt(6724 * ef**2 * cap**2 + 40_409_100 * ef**2 * cap - 28_080_900 * ef * cap + 29_318_000_625 * ef**2 - 58_636_001_250 * ef + 29_318_000_625) + 82 * ef * cap + 171_225 * ef - 171_225) / (200 * ef * cap)
+
+        #Estimate recovery efficiency
+        capacity_btu_per_hr = OpenStudio.convert(capacity_w, 'W', 'Btu/hr').get
+        if volume_gal < 18
+          q_load_btu_per_hr = 5561
+        elsif volume_gal >= 18 and volume_gal < 51
+          q_load_btu_per_hr = 21131
+        elsif volume_gal >= 51 and volume_gal < 75
+          q_load_btu_per_hr = 30584
+        elsif volume_gal >= 75
+          q_load_btu_per_hr = 46710
+        end
+        puts "volume_gal #{volume_gal}"
+        puts "uef #{uef}"
+        re = water_heater_eff + q_load_btu_per_hr*(uef-water_heater_eff)/(24*capacity_btu_per_hr*uef)
+        # Calculate UA
+        ua_btu_per_hr_per_f = (water_heater_eff-re)*q_load_btu_per_hr/(125-67.5)  
+
+      elsif capacity_w <= 22000 and volume_litre >= 208 and volume_litre < 380
+        if fhr < 68
+          uef = 0.6470 - 0.00016*volume_litre
+          puts "5"
+        elsif fhr >= 68 and fhr < 193
+          uef = 0.7689 - 0.00013*volume_litre
+          puts "this"
+        elsif fhr >= 193 and fhr < 284
+          uef = 0.7897 - 0.00011*volume_litre
+          puts "6"
+        elsif fhr >= 284 
+          uef = 0.8072 - 0.00008*volume_litre
+          puts "7"
+        end
+
+        # Assume thermal efficiency  (Maguire and Roberts, 2020)
+        water_heater_eff = 0.82
+
+        #Estimate recovery efficiency
+        capacity_btu_per_hr = OpenStudio.convert(capacity_w, 'W', 'Btu/hr').get
         
-		# Calculate the skin loss coefficient (UA)
-        # based on the actual capacity.
-        ua_btu_per_hr_per_f = (water_heater_eff - re) * capacity_btu_per_hr / 67.5
-      else
-        # Thermal efficiency (PCF 1630 update)
-        et = 0.9
-        sl_w = 0.84 * capacity_btu_per_hr / 3412.412 / 0.234 + 16.57 * (volume_l ** 0.5)
-        sl_btu_per_hr = sl_w * 3.412
-        # Calculate the skin loss coefficient (UA)
-        ua_btu_per_hr_per_f = (sl_btu_per_hr * et) / 70
-        # Calculate water heater efficiency
-        water_heater_eff = (ua_btu_per_hr_per_f * 70 + capacity_btu_per_hr * et) / capacity_btu_per_hr
+        if volume_gal < 18
+          q_load_btu_per_hr = 5561
+        elsif volume_gal >= 18 and volume_gal < 51
+          q_load_btu_per_hr = 21131
+        elsif volume_gal >= 51 and volume_gal < 75
+          q_load_btu_per_hr = 30584
+        elsif volume_gal >= 75
+          q_load_btu_per_hr = 46710
+        end
+        puts "volume_gal #{volume_gal}"
+        puts "uef #{uef}"
+        re = water_heater_eff + q_load_btu_per_hr*(uef-water_heater_eff)/(24*capacity_btu_per_hr*uef)
+        # Calculate UA
+        ua_btu_per_hr_per_f = (water_heater_eff-re)*q_load_btu_per_hr/(125-67.5)      
+
+      elsif capacity_w > 22000 and capacity_w <= 30500 and volume_litre <= 454 
+        # NOTE: 454L in this case, refers to manufacturer stated volume.
+        # Assume manufacturer rated volume = actual tank volume (value used in EnergyPlus)
+        uef = 0.8107 - 0.00021*volume_litre
+        ua_btu_per_hr_per_f = sl_btu_per_hr / 70
+        water_heater_eff = 0.8
+      else # all other water heaters
+        sl_w = 0.84*(1.25*capacity_w + 16.57*(volume_litre**0.5))
+        sl_btu_per_hr = OpenStudio.convert(sl_w, 'W', 'Btu/hr').get
+        puts "sl_btu_per_hr #{sl_btu_per_hr}"
+        ua_btu_per_hr_per_f = sl_btu_per_hr / 70
+        water_heater_eff = 0.9
       end
     end
 
