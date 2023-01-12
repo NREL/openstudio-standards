2dd908da13d7e7670cc9035511c6ccbd668f6e97
Andrew Parker <Andrew.Parker@nrel.gov>
2023-01-12 10:16:29 -0700

    Removes autosizing and hardsizing methods that have been in OS C++ API since OS 2.4.1

--- lib/openstudio-standards/hvac_sizing/Siz.ThermalZone.rb
+++ lib/openstudio-standards/hvac_sizing/Siz.ThermalZone.rb
@@ -2,48 +2,20 @@
 # open the class to add methods to return sizing values
 class OpenStudio::Model::ThermalZone
 
-  # Sets all auto-sizeable fields to autosize
-  def autosize
-    self.autosizeDesignOutdoorAirFlowRate
-  end
-
-  # Takes the values calculated by the EnergyPlus sizing routines
-  # and puts them into this object model in place of the autosized fields.
-  # Must have previously completed a run with sql output for this to work.
-  def applySizingValues
-
-    # In OpenStudio, the design OA flow rates are calculated by the
-    # Controller:OutdoorAir object associated with this system.
-    # Therefore, this property will be retrieved from that object's sizing values
-    air_loop = self.airLoopHVAC
-    if air_loop.airLoopHVACOutdoorAirSystem.is_initialized
-      controller_oa = air_loop.airLoopHVACOutdoorAirSystem.get.getControllerOutdoorAir
-      # get the max oa flow rate from the controller:outdoor air sizing
-      maximum_outdoor_air_flow_rate = controller_oa.autosizedMaximumOutdoorAirFlowRate
-      if maximum_outdoor_air_flow_rate.is_initialized
-        self.setDesignOutdoorAirFlowRate(maximum_outdoor_air_flow_rate.get)
-        # Set the OA flow method to "ZoneSum" to avoid severe errors
-        # in the fully hard-sized model.
-        self.setSystemOutdoorAirMethod("ZoneSum")
-      end
-    end
-    
-  end
-
   # returns the autosized maximum outdoor air flow rate as an optional double
   def autosizedMaximumOutdoorAirFlowRate
 
-    return self.model.getAutosizedValue(self, 'Maximum Outdoor Air Flow Rate', 'm3/s')
-    
+    return self.getAutosizedValue('Maximum Outdoor Air Flow Rate', 'm3/s')
+
   end
-  
+
   # returns the autosized minimum outdoor air flow rate as an optional double
   def autosizedMinimumOutdoorAirFlowRate
 
-    return self.model.getAutosizedValue(self, 'Minimum Outdoor Air Flow Rate', 'm3/s')
-    
+    return self.getAutosizedValue('Minimum Outdoor Air Flow Rate', 'm3/s')
+
   end
-  
+
   # returns the autosized cooling design air flow rate as an optional double
   def autosizedCoolingDesignAirFlowRate
 
@@ -52,29 +24,29 @@ class OpenStudio::Model::ThermalZone
     name = self.name.get.upcase
 
     sql = self.model.sqlFile
-    
+
     if sql.is_initialized
       sql = sql.get
-    
+
       # In E+ 8.4, (OS 1.9.3 onward) the table name changed
       table_name = nil
       if self.model.version < OpenStudio::VersionString.new('1.9.3')
         table_name = 'Zone Cooling'
       else
         table_name = 'Zone Sensible Cooling'
-      end  
-    
-      query = "SELECT Value 
+      end
+
+      query = "SELECT Value
               FROM tabulardatawithstrings
-              WHERE ReportName='HVACSizingSummary' 
-              AND ReportForString='Entire Facility' 
+              WHERE ReportName='HVACSizingSummary'
+              AND ReportForString='Entire Facility'
               AND TableName='#{table_name}'
               AND ColumnName='User Design Air Flow'
               AND RowName='#{name}'
               AND Units='m3/s'"
 
       val = sql.execAndReturnFirstDouble(query)
-      
+
       if val.is_initialized
         result = OpenStudio::OptionalDouble.new(val.get)
       else
@@ -97,29 +69,29 @@ class OpenStudio::Model::ThermalZone
     name = self.name.get.upcase
 
     sql = self.model.sqlFile
-    
+
     if sql.is_initialized
       sql = sql.get
-    
+
       # In E+ 8.4, (OS 1.9.3 onward) the table name changed
       table_name = nil
       if self.model.version < OpenStudio::VersionString.new('1.9.3')
         table_name = 'Zone Heating'
       else
         table_name = 'Zone Sensible Heating'
-      end    
-    
-      query = "SELECT Value 
+      end
+
+      query = "SELECT Value
               FROM tabulardatawithstrings
-              WHERE ReportName='HVACSizingSummary' 
-              AND ReportForString='Entire Facility' 
+              WHERE ReportName='HVACSizingSummary'
+              AND ReportForString='Entire Facility'
               AND TableName='#{table_name}'
               AND ColumnName='User Design Air Flow'
               AND RowName='#{name}'
               AND Units='m3/s'"
 
       val = sql.execAndReturnFirstDouble(query)
-      
+
       if val.is_initialized
         result = OpenStudio::OptionalDouble.new(val.get)
       else
@@ -136,35 +108,35 @@ class OpenStudio::Model::ThermalZone
 
   # returns the calculated cooling design load as an optional double
   def coolingDesignLoad
-  
+
     result = OpenStudio::OptionalDouble.new
 
     name = self.name.get.upcase
 
     sql = self.model.sqlFile
-    
+
     if sql.is_initialized
       sql = sql.get
-    
+
       # In E+ 8.4, (OS 1.9.3 onward) the table name changed
       table_name = nil
       if self.model.version < OpenStudio::VersionString.new('1.9.3')
         table_name = 'Zone Cooling'
       else
         table_name = 'Zone Sensible Cooling'
-      end    
-    
-      query = "SELECT Value 
+      end
+
+      query = "SELECT Value
               FROM tabulardatawithstrings
-              WHERE ReportName='HVACSizingSummary' 
-              AND ReportForString='Entire Facility' 
+              WHERE ReportName='HVACSizingSummary'
+              AND ReportForString='Entire Facility'
               AND TableName='#{table_name}'
               AND ColumnName='User Design Load'
               AND RowName='#{name}'
               AND Units='W'"
 
       val = sql.execAndReturnFirstDouble(query)
-      
+
       if val.is_initialized
         floor_area_no_multiplier_m2 = self.floorArea
         floor_area_m2 = floor_area_no_multiplier_m2 * self.multiplier
@@ -178,10 +150,10 @@ class OpenStudio::Model::ThermalZone
       OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', 'Model has no sql file containing results, cannot lookup data.')
     end
 
-    return result  
-  
+    return result
+
   end
-  
+
   def designAirFlowRate
 
     result = OpenStudio::OptionalDouble.new
@@ -189,17 +161,17 @@ class OpenStudio::Model::ThermalZone
     name = self.name.get.upcase
 
     sql = self.model.sqlFile
-    
+
     if sql.is_initialized
       sql = sql.get
-      
+
       table_name = 'Zone Sizing Information'
 
       # Get zone row name
       query = "SELECT RowName
               FROM tabulardatawithstrings
-              WHERE ReportName='Initialization Summary' 
-              AND ReportForString='Entire Facility' 
+              WHERE ReportName='Initialization Summary'
+              AND ReportForString='Entire Facility'
               AND TableName='#{table_name}'
               AND ColumnName='Zone Name'
               AND Value='#{name}'"
@@ -217,32 +189,32 @@ class OpenStudio::Model::ThermalZone
         end
         htg_des_air_flow_rate = 0
         clg_des_air_flow_rate = 0
-        
+
         # Get zone cooling design flow rate
-        query = "SELECT Value 
+        query = "SELECT Value
                 FROM tabulardatawithstrings
-                WHERE ReportName='Initialization Summary' 
-                AND ReportForString='Entire Facility' 
+                WHERE ReportName='Initialization Summary'
+                AND ReportForString='Entire Facility'
                 AND TableName='#{table_name}'
                 AND ColumnName='User Des Air Flow Rate {m3/s}'
                 AND RowName='#{clg_id}'"
         val = sql.execAndReturnFirstDouble(query)
-        if val.is_initialized  
+        if val.is_initialized
           clg_des_air_flow_rate = val
         else
           OpenStudio::logFree(OpenStudio::Warn, "openstudio.model.Model", "Data not found for query: #{query}")
         end
 
         # Get zone heating design flow rate
-        query = "SELECT Value 
+        query = "SELECT Value
                 FROM tabulardatawithstrings
-                WHERE ReportName='Initialization Summary' 
-                AND ReportForString='Entire Facility' 
+                WHERE ReportName='Initialization Summary'
+                AND ReportForString='Entire Facility'
                 AND TableName='#{table_name}'
                 AND ColumnName='User Des Air Flow Rate {m3/s}'
                 AND RowName='#{htg_id}'"
         val = sql.execAndReturnFirstDouble(query)
-        if val.is_initialized  
+        if val.is_initialized
           htg_des_air_flow_rate = val
         else
           OpenStudio::logFree(OpenStudio::Warn, "openstudio.model.Model", "Data not found for query: #{query}")
@@ -263,16 +235,16 @@ class OpenStudio::Model::ThermalZone
 
   # returns the calculated heating design load as an optional double
   def heatingDesignLoad
-  
+
     result = OpenStudio::OptionalDouble.new
 
     name = self.name.get.upcase
 
     sql = self.model.sqlFile
-    
+
     if sql.is_initialized
       sql = sql.get
-      
+
       # In E+ 8.4, (OS 1.9.3 onward) the table name changed
       table_name = nil
       if self.model.version < OpenStudio::VersionString.new('1.9.3')
@@ -280,18 +252,18 @@ class OpenStudio::Model::ThermalZone
       else
         table_name = 'Zone Sensible Heating'
       end
-      
-      query = "SELECT Value 
+
+      query = "SELECT Value
               FROM tabulardatawithstrings
-              WHERE ReportName='HVACSizingSummary' 
-              AND ReportForString='Entire Facility' 
+              WHERE ReportName='HVACSizingSummary'
+              AND ReportForString='Entire Facility'
               AND TableName='#{table_name}'
               AND ColumnName='User Design Load'
               AND RowName='#{name}'
               AND Units='W'"
 
       val = sql.execAndReturnFirstDouble(query)
-      
+
       if val.is_initialized
         floor_area_no_multiplier_m2 = self.floorArea
         floor_area_m2 = floor_area_no_multiplier_m2 * self.multiplier
@@ -305,8 +277,8 @@ class OpenStudio::Model::ThermalZone
       OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', 'Model has no sql file containing results, cannot lookup data.')
     end
 
-    return result  
-  
+    return result
+
   end
 
   # Determine the zone heating fuels, including
@@ -314,14 +286,14 @@ class OpenStudio::Model::ThermalZone
   # the air loops serving the zone, and any plant loops
   # serving those air loops.
   #
-  # return [Array<String>] An array. Possible values are 
+  # return [Array<String>] An array. Possible values are
   # Electricity, NaturalGas, Propane, PropaneGas, FuelOilNo1, FuelOilNo2,
-  # Coal, Diesel, Gasoline, DistrictCooling, DistrictHeating, 
+  # Coal, Diesel, Gasoline, DistrictCooling, DistrictHeating,
   # and SolarEnergy.
   def heating_fuels
-  
+
     fuels = []
-    
+
     # Special logic for models imported from Sefaira.
     # In this case, the fuels are listed as a comment
     # above the Zone object.
@@ -338,7 +310,7 @@ class OpenStudio::Model::ThermalZone
         fuels.uniq.sort
       end
     end
-    
+
     # Check the zone hvac heating fuels
     fuels += self.model.zone_equipment_heating_fuels(self)
 
@@ -348,9 +320,9 @@ class OpenStudio::Model::ThermalZone
     OpenStudio::logFree(OpenStudio::Debug, 'openstudio.model.Model', "For #{name}, heating fuels = #{fuels.uniq.sort.join(', ')}.")
 
     return fuels.uniq.sort
-    
+
   end
- 
+
   # Determine the zone cooling fuels, including
   # any fuels used by zone equipment, reheat terminals,
   # the air loops serving the zone, and any plant loops
@@ -358,12 +330,12 @@ class OpenStudio::Model::ThermalZone
   #
   # return [Array<String>] An array. Possible values are
   # Electricity, NaturalGas, Propane, PropaneGas, FuelOilNo1, FuelOilNo2,
-  # Coal, Diesel, Gasoline, DistrictCooling, DistrictHeating, 
+  # Coal, Diesel, Gasoline, DistrictCooling, DistrictHeating,
   # and SolarEnergy.
   def cooling_fuels
-  
+
     fuels = []
-    
+
     # Check the zone hvac cooling fuels
     fuels += self.model.zone_equipment_cooling_fuels(self)
 
@@ -373,7 +345,7 @@ class OpenStudio::Model::ThermalZone
     OpenStudio::logFree(OpenStudio::Debug, 'openstudio.model.Model', "For #{name}, cooling fuels = #{fuels.uniq.sort.join(', ')}.")
 
     return fuels.uniq.sort
-    
-  end  
-  
+
+  end
+
 end
