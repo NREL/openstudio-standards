0c742bf3cc1e326edb885c5e71276c8c88e156af
amylebar <41447070+amylebar@users.noreply.github.com>
2023-02-13 10:20:52 -0700

    update/add DEER AirLoopHVAC.rb and ThermalZone.rb

---
+++ lib/openstudio-standards/standards/deer/deer_2015/deer_2015.ThermalZone.rb
@@ -0,0 +1,21 @@
+class DEER2015 < DEER
+    # @!group ThermalZone
+  
+    # Determine the area and occupancy level limits for
+    # demand control ventilation.
+    #
+    # @param thermal_zone [OpenStudio::Model::ThermalZone] the thermal zone
+    # @return [Array<Double>] the minimum area, in m^2
+    # and the minimum occupancy density in m^2/person.  Returns nil
+    # if there is no requirement.
+    def thermal_zone_demand_control_ventilation_limits(thermal_zone)
+      min_area_ft2 = nil # No minimum area
+      min_ft2_per_occ = 40
+      
+      # Convert to SI
+      min_m2_per_occ = OpenStudio.convert(min_ft2_per_occ, 'ft^2', 'm^2').get
+  
+      return [min_area_ft2, min_m2_per_occ]
+    end
+  end
+end
