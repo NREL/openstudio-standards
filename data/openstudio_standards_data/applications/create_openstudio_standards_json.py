cd7a0497f613789d2746d973dd115f5e31e85917
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2023-03-23 10:43:48 -0700

    Black.

--- data/openstudio_standards_data/applications/create_openstudio_standards_json.py
+++ data/openstudio_standards_data/applications/create_openstudio_standards_json.py
@@ -13,7 +13,9 @@ from query.fetch.database_table import (
 from query.fetch.template import fetch_templates
 
 
-def create_openstudio_standards_space_data_json(conn: sqlite3.Connection, json_data_dir: str):
+def create_openstudio_standards_space_data_json(
+    conn: sqlite3.Connection, json_data_dir: str
+):
     """
     Extract OpenSutdio Standards building data to a json file.
     :param conn: database connection
