401485cfa00a8936f6dc006e1b7fd181e1c1603e
Weili Xu <weili.xu@pnnl.gov>
2023-03-28 09:06:14 -0700

    fix typo in the script and data files

--- data/openstudio_standards_data/database_tables/level_2_lighting_space_types.py
+++ data/openstudio_standards_data/database_tables/level_2_lighting_space_types.py
@@ -7,7 +7,7 @@ RECORD_HELP = """
 Must provide a tuple that contains:
 lighting_space_type_name: TEXT (unique)
 level_3_lighting_definition_table: TEXT
-level_3_lighting_defintion_id: id from level_3_lighting_definition index
+level_3_lighting_definition_id: id from level_3_lighting_definition index
 """
 
 LIGHT_SUBSPACE_TABLE = """
@@ -15,21 +15,21 @@ CREATE TABLE IF NOT EXISTS level_2_lighting_space_types
 (id INTEGER PRIMARY KEY,
 lighting_space_type_name TEXT NOT NULL,
 level_3_lighting_definition_table TEXT NOT NULL,
-level_3_lighting_defintion_id INTEGER NOT NULL,
+level_3_lighting_definition_id INTEGER NOT NULL,
 FOREIGN KEY(lighting_space_type_name) REFERENCES support_lighting_space_type_name_tags(lighting_space_type_name)
 );
 """
 
 INSERT_LIGHT_SUBSPACE = f"""
     INSERT INTO {TABLE_NAME}
-    (lighting_space_type_name, level_3_lighting_definition_table, level_3_lighting_defintion_id)
+    (lighting_space_type_name, level_3_lighting_definition_table, level_3_lighting_definition_id)
     VALUES (?, ?, ?);
 """
 
 RECORD_TEMPLATE = {
     "lighting_space_type_name": "",
     "level_3_lighting_definition_table": "",
-    "level_3_lighting_defintion_id": "",
+    "level_3_lighting_definition_id": "",
 }
 
 
@@ -57,7 +57,7 @@ class LightSubspaceTable(DBOperation):
         record_list = (
             getattr_either("lighting_space_type_name", record),
             getattr_either("level_3_lighting_definition_table", record),
-            getattr_either("level_3_lighting_defintion_id", record),
+            getattr_either("level_3_lighting_definition_id", record),
         )
 
         return record_list
--- data/openstudio_standards_data/database_tables/level_2_ventilation_space_types.py
+++ data/openstudio_standards_data/database_tables/level_2_ventilation_space_types.py
@@ -7,7 +7,7 @@ RECORD_HELP = """
 Must provide a tuple that contains:
 ventilation_space_type_name: TEXT (unique)
 level_3_ventilation_definition_table: TEXT
-level_3_ventilation_defintion_id: id from level_3_ventilation_definition index
+level_3_ventilation_definition_id: id from level_3_ventilation_definition index
 """
 
 VENT_SUBSPACE_TABLE = """
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS level_2_ventilation_space_types
 (id INTEGER PRIMARY KEY,
 ventilation_space_type_name TEXT NOT NULL,
 level_3_ventilation_definition_table TEXT NOT NULL,
-level_3_ventilation_defintion_id INTEGER NOT NULL,
+level_3_ventilation_definition_id INTEGER NOT NULL,
 FOREIGN KEY(ventilation_space_type_name) REFERENCES support_ventilation_space_type_name_tags(ventilation_space_type_name)
 
 );
@@ -23,14 +23,14 @@ FOREIGN KEY(ventilation_space_type_name) REFERENCES support_ventilation_space_ty
 
 INSERT_VENT_SUBSPACE = f"""
     INSERT INTO {TABLE_NAME}
-    (ventilation_space_type_name, level_3_ventilation_definition_table, level_3_ventilation_defintion_id)
+    (ventilation_space_type_name, level_3_ventilation_definition_table, level_3_ventilation_definition_id)
     VALUES (?, ?, ?);
 """
 
 RECORD_TEMPLATE = {
     "ventilation_space_type_name": "",
     "level_3_ventilation_definition_table": "",
-    "level_3_ventilation_defintion_id": "",
+    "level_3_ventilation_definition_id": "",
 }
 
 
@@ -59,7 +59,7 @@ class VentSubspaceTable(DBOperation):
         record_list = (
             getattr_either("ventilation_space_type_name", record),
             getattr_either("level_3_ventilation_definition_table", record),
-            getattr_either("level_3_ventilation_defintion_id", record),
+            getattr_either("level_3_ventilation_definition_id", record),
         )
 
         return record_list
