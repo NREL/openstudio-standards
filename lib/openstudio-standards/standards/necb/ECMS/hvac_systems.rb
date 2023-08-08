938e66b7b423f7f15faa5ca58921051229a17206
Kim <janghyun.kim@nrel.gov>
2023-08-08 13:26:17 -0600

    removing if for zero

--- lib/openstudio-standards/standards/necb/ECMS/hvac_systems.rb
+++ lib/openstudio-standards/standards/necb/ECMS/hvac_systems.rb
@@ -255,9 +255,6 @@ class ECMS
       end
     end
     max_net_vert_distance = max_vert_distance + min_vert_distance
-    if max_net_vert_distance.zero?()
-      max_net_vert_distance = 0.000001
-    end
 
     return max_equiv_distance, max_net_vert_distance
   end
