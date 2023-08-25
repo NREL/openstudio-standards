89be23d89326bd78bf9a74eb3505455d235d166c
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2023-08-25 16:31:19 -0600

    remove getAutosizedValueFromEquipmentSummary
    remove getAutosizedValueFromEquipmentSummary method from Siz.Model.rb

--- lib/openstudio-standards/hvac_sizing/Siz.Model.rb
+++ lib/openstudio-standards/hvac_sizing/Siz.Model.rb
@@ -21,43 +21,6 @@ class OpenStudio::Model::Model
   # Component quantity methods
   require_relative 'Siz.HVACComponent'
 
-  # A helper method to get component sizes from the Equipment Summary of the TabularDataWithStrings Report
-  # returns the autosized value as an optional double
-  def getAutosizedValueFromEquipmentSummary(object, table_name, value_name, units)
-
-    result = OpenStudio::OptionalDouble.new
-
-    name = object.name.get.upcase
-
-    sql = self.sqlFile
-
-    if sql.is_initialized
-      sql = sql.get
-
-      #SELECT * FROM ComponentSizes WHERE CompType = 'Coil:Heating:Gas' AND CompName = "COIL HEATING GAS 3" AND Description = "Design Size Nominal Capacity"
-      query = "Select Value FROM TabularDataWithStrings WHERE
-      ReportName = 'EquipmentSummary' AND
-      TableName = '#{table_name}' AND
-      RowName = '#{name}' AND
-      ColumnName = '#{value_name}' AND
-      Units = '#{units}'"
-
-      val = sql.execAndReturnFirstDouble(query)
-
-      if val.is_initialized
-        result = OpenStudio::OptionalDouble.new(val.get)
-      else
-        # @todo comment following line (debugging new HVACsizing objects right now)
-        # OpenStudio::logFree(OpenStudio::Warn, "openstudio.model.Model", "QUERY ERROR: Data not found for query: #{query}")
-      end
-    else
-      OpenStudio::logFree(OpenStudio::Error, 'openstudio.model.Model', 'Model has no sql file containing results, cannot lookup data.')
-    end
-
-    return result
-  end
-
-
   # Helper function to output the fan power for each fan in the model
   # @param [String] csv_path: if given, will output a csv file
   # @return [Array of Hash] each row is a fan, with its name, type, rated watts per cfm, and the airloop or hvac component or zonehvac component it serves
