aae98d8806a6cd51e397a96c1be95caaf81edb30
Carlos Duarte <cduarte@berkeley.edu>
2023-08-31 15:14:32 -0700

    test to verify lockout hours for afternoon shutoff and all day radiant operation

--- test/os_stds_methods/test_radiant_controls.rb
+++ test/os_stds_methods/test_radiant_controls.rb
@@ -37,7 +37,7 @@ class TestRadiantControls < Minitest::Test
        hot_water_loop_type: 'LowTemperature', climate_zone: 'ASHRAE 169-2013-5B', model_name: 'basic_2_story_office_no_hvac_20WWR',
        radiant_type: 'ceiling', use_zone_occupancy_for_control: false, radiant_availability_type: 'precool',
        custom_variables_output: [['Zone Radiant HVAC Heating Rate', '*', 'Hourly'], ['Zone Radiant HVAC Cooling Rate', '*', 'Hourly']],
-       unmet_hrs_htg: 500.0, unmet_hrs_clg: 1500.0}
+       unmet_hrs_htg: 200.0, unmet_hrs_clg: 1500.0}
 
     standard = Standard.build('90.1-2013')
 
@@ -149,4 +149,237 @@ class TestRadiantControls < Minitest::Test
     assert(errs.empty?, "Radiant slab system model failed with errors: #{errs}")
   end
 
+  def test_radiant_afternoon_shutoff_hours_schedule
+    arguments = {model_test_name: 'radiant_afternoon_shutoff_hours_schedule', main_heat_fuel: 'NaturalGas', cool_fuel: 'Electricity',
+       hot_water_loop_type: 'LowTemperature', climate_zone: 'ASHRAE 169-2013-5B', model_name: 'basic_2_story_office_no_hvac_20WWR',
+       radiant_type: 'ceiling', use_zone_occupancy_for_control: false, radiant_availability_type: 'afternoon_shutoff',
+       custom_variables_output: [['Zone Radiant HVAC Heating Rate', '*', 'Hourly'], ['Zone Radiant HVAC Cooling Rate', '*', 'Hourly']],
+       unmet_hrs_htg: 150.0, unmet_hrs_clg: 1250.0}
+
+    standard = Standard.build('90.1-2013')
+
+    test_return = model_radiant_system_test(arguments)
+    model = test_return['model']
+    errs = test_return['errs']
+
+    # radiant lockout hours for precool operation
+    radiant_lockout_start_time = 15.0
+    radiant_lockout_end_time = 22.0
+
+    start_hour = radiant_lockout_start_time.to_i
+    start_minute = ((radiant_lockout_start_time % 1) * 60).to_i
+    end_hour = radiant_lockout_end_time.to_i
+    end_minute = ((radiant_lockout_end_time % 1) * 60).to_i
+
+    # get array of hours to test
+    if end_hour > start_hour
+      time_OFF = ((start_hour + 1)..end_hour).to_a
+      time_ON = ((end_hour + 1)..24).to_a + ((1)..(start_hour)).to_a
+    elsif start_hour > end_hour
+      time_OFF = ((start_hour + 1)..24).to_a + (1..end_hour).to_a
+      time_ON = ((end_hour + 1)..(start_hour)).to_a
+    else
+      time_OFF = []
+      time_ON = (1..24).to_a
+    end
+
+    unless start_minute == 0
+      time_OFF << start_hour + (start_minute + 1).fdiv(60)
+    end
+
+    unless end_minute == 0
+      time_ON << end_hour + (end_minute + 1).fdiv(60)
+    end
+
+    # get availability schedule for all radiant loops
+    control_errs = []
+    radiant_loops = model.getZoneHVACLowTempRadiantVarFlows
+    if radiant_loops.length == 0
+      # check constant flow radiant loops
+      radiant_loops = model.getZoneHVACLowTempRadiantConstFlows
+    end
+
+    # test each radiant loop schedule against each time ON/OFF array to verify
+    # correct definition of object
+    correct_schedule = []
+    radiant_loops.each do |radiant_loop|
+      availability_sch = radiant_loop.availabilitySchedule.to_ScheduleRuleset.get
+      day_schedule = availability_sch.defaultDaySchedule
+
+      # test radiant OFF operation
+      time_OFF.each do |time|
+        hour = time.to_i
+        minute = ((time % 1) * 60).to_i
+
+        test_time = OpenStudio::Time.new(0, hour, minute, 0)
+        correct_schedule << (day_schedule.getValue(test_time) == 0)
+      end
+
+      # test radiant ON operation
+      time_ON.each do |time|
+        hour = time.to_i
+        minute = ((time % 1) * 60).to_i
+
+        test_time = OpenStudio::Time.new(0, hour, minute, 0)
+        correct_schedule << (day_schedule.getValue(test_time) == 1)
+      end
+    end
+
+    unless correct_schedule.all?
+      control_errs << "Model #{arguments[:model_test_name]} has at least one incorrect availability schedule for radiant system in operation type #{arguments[:radiant_availability_type]}"
+    end
+
+    # get each radiant loop operation and test that it stays off during defined lockout hours
+
+    # get simualtion results
+    sql = standard.model_get_sql_file(model)
+    ann_env_pd = standard.model_get_weather_run_period(model)
+
+    radiant_loops.each do |radiant_loop|
+
+      radiant_htg = sql.timeSeries(ann_env_pd, 'Hourly', 'Zone Radiant HVAC Heating Rate', radiant_loop.name.get)
+      radiant_clg = sql.timeSeries(ann_env_pd, 'Hourly', 'Zone Radiant HVAC Cooling Rate', radiant_loop.name.get)
+
+      if radiant_htg.empty? || radiant_clg.empty?
+        OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Could not find heating/cooling timeseries for radiant loop '#{radiant_loop.name.get}'")
+      end
+
+      # verify that heating and cooling does not activate during radiant lockout hours
+      radiant_loop_htg_vector = radiant_htg.get.values.collect{ |val| val}
+      radiant_loop_clg_vector = radiant_clg.get.values.collect{ |val| val}
+      sim_times = radiant_htg.get.dateTimes.collect{ |tm| tm.time.hours + tm.time.minutes.fdiv(60) }
+
+      sim_time_OFF_index = sim_times.each_index.select{|idx| time_OFF.include? sim_times[idx]}
+      htg_during_time_OFF = sim_time_OFF_index.collect{|idx| radiant_loop_htg_vector[idx]}
+      clg_during_time_OFF = sim_time_OFF_index.collect{|idx| radiant_loop_clg_vector[idx]}
+
+      if htg_during_time_OFF.sum() > 0
+        control_errs << "Model #{arguments[:model_test_name]} does heating in radiant loop #{radiant_loop.name.get} during lockout hours"
+      end
+
+      if clg_during_time_OFF.sum() > 0
+        control_errs << "Model #{arguments[:model_test_name]} does cooling in radiant loop #{radiant_loop.name.get} during lockout hours"
+      end
+    end
+
+    errs += control_errs
+    assert(errs.empty?, "Radiant slab system model failed with errors: #{errs}")
+  end
+
+  def test_radiant_all_day_hours_schedule
+    arguments = {model_test_name: 'radiant_all_day_hours_schedule', main_heat_fuel: 'NaturalGas', cool_fuel: 'Electricity',
+       hot_water_loop_type: 'LowTemperature', climate_zone: 'ASHRAE 169-2013-5B', model_name: 'basic_2_story_office_no_hvac_20WWR',
+       radiant_type: 'ceiling', use_zone_occupancy_for_control: false, radiant_availability_type: 'all_day',
+       custom_variables_output: [['Zone Radiant HVAC Heating Rate', '*', 'Hourly'], ['Zone Radiant HVAC Cooling Rate', '*', 'Hourly']],
+       unmet_hrs_htg: 100.0, unmet_hrs_clg: 1500.0}
+
+    standard = Standard.build('90.1-2013')
+
+    test_return = model_radiant_system_test(arguments)
+    model = test_return['model']
+    errs = test_return['errs']
+
+    # radiant lockout hours for precool operation
+    radiant_lockout_start_time = 24.0
+    radiant_lockout_end_time = 24.0
+
+    start_hour = radiant_lockout_start_time.to_i
+    start_minute = ((radiant_lockout_start_time % 1) * 60).to_i
+    end_hour = radiant_lockout_end_time.to_i
+    end_minute = ((radiant_lockout_end_time % 1) * 60).to_i
+
+    # get array of hours to test
+    if end_hour > start_hour
+      time_OFF = ((start_hour + 1)..end_hour).to_a
+      time_ON = ((end_hour + 1)..24).to_a + ((1)..(start_hour)).to_a
+    elsif start_hour > end_hour
+      time_OFF = ((start_hour + 1)..24).to_a + (1..end_hour).to_a
+      time_ON = ((end_hour + 1)..(start_hour)).to_a
+    else
+      time_OFF = []
+      time_ON = (1..24).to_a
+    end
+
+    unless start_minute == 0
+      time_OFF << start_hour + (start_minute + 1).fdiv(60)
+    end
+
+    unless end_minute == 0
+      time_ON << end_hour + (end_minute + 1).fdiv(60)
+    end
+
+    # get availability schedule for all radiant loops
+    control_errs = []
+    radiant_loops = model.getZoneHVACLowTempRadiantVarFlows
+    if radiant_loops.length == 0
+      # check constant flow radiant loops
+      radiant_loops = model.getZoneHVACLowTempRadiantConstFlows
+    end
+
+    # test each radiant loop schedule against each time ON/OFF array to verify
+    # correct definition of object
+    correct_schedule = []
+    radiant_loops.each do |radiant_loop|
+      availability_sch = radiant_loop.availabilitySchedule.to_ScheduleRuleset.get
+      day_schedule = availability_sch.defaultDaySchedule
+
+      # test radiant OFF operation
+      time_OFF.each do |time|
+        hour = time.to_i
+        minute = ((time % 1) * 60).to_i
+
+        test_time = OpenStudio::Time.new(0, hour, minute, 0)
+        correct_schedule << (day_schedule.getValue(test_time) == 0)
+      end
+
+      # test radiant ON operation
+      time_ON.each do |time|
+        hour = time.to_i
+        minute = ((time % 1) * 60).to_i
+
+        test_time = OpenStudio::Time.new(0, hour, minute, 0)
+        correct_schedule << (day_schedule.getValue(test_time) == 1)
+      end
+    end
+
+    unless correct_schedule.all?
+      control_errs << "Model #{arguments[:model_test_name]} has at least one incorrect availability schedule for radiant system in operation type #{arguments[:radiant_availability_type]}"
+    end
+
+    # get each radiant loop operation and test that it stays off during defined lockout hours
+
+    # get simualtion results
+    sql = standard.model_get_sql_file(model)
+    ann_env_pd = standard.model_get_weather_run_period(model)
+
+    radiant_loops.each do |radiant_loop|
+
+      radiant_htg = sql.timeSeries(ann_env_pd, 'Hourly', 'Zone Radiant HVAC Heating Rate', radiant_loop.name.get)
+      radiant_clg = sql.timeSeries(ann_env_pd, 'Hourly', 'Zone Radiant HVAC Cooling Rate', radiant_loop.name.get)
+
+      if radiant_htg.empty? || radiant_clg.empty?
+        OpenStudio.logFree(OpenStudio::Error, 'openstudio.model.Model', "Could not find heating/cooling timeseries for radiant loop '#{radiant_loop.name.get}'")
+      end
+
+      # verify that heating and cooling does not activate during radiant lockout hours
+      radiant_loop_htg_vector = radiant_htg.get.values.collect{ |val| val}
+      radiant_loop_clg_vector = radiant_clg.get.values.collect{ |val| val}
+      sim_times = radiant_htg.get.dateTimes.collect{ |tm| tm.time.hours + tm.time.minutes.fdiv(60) }
+
+      sim_time_OFF_index = sim_times.each_index.select{|idx| time_OFF.include? sim_times[idx]}
+      htg_during_time_OFF = sim_time_OFF_index.collect{|idx| radiant_loop_htg_vector[idx]}
+      clg_during_time_OFF = sim_time_OFF_index.collect{|idx| radiant_loop_clg_vector[idx]}
+
+      if htg_during_time_OFF.sum() > 0
+        control_errs << "Model #{arguments[:model_test_name]} does heating in radiant loop #{radiant_loop.name.get} during lockout hours"
+      end
+
+      if clg_during_time_OFF.sum() > 0
+        control_errs << "Model #{arguments[:model_test_name]} does cooling in radiant loop #{radiant_loop.name.get} during lockout hours"
+      end
+    end
+
+    errs += control_errs
+    assert(errs.empty?, "Radiant slab system model failed with errors: #{errs}")
+  end
 end
