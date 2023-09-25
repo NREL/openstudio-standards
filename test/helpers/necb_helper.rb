feff5fe6d5876f192cf735dba97da3f55fe70e5a
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2023-09-25 13:56:29 -0400

    rename and clean up of run_the_measure, now run_sizing; and minor clean-up edits

--- test/helpers/necb_helper.rb
+++ test/helpers/necb_helper.rb
@@ -1,12 +1,14 @@
 # Additional methods for NECB tests
 require 'fileutils'
 require 'pathname'
+#require 'json'
+#require 'hashdiff'
 
 module NecbHelper
   # Hold an array of the instantiated standards (to save recreating them all the time).
   @@standards = []
 
-# Standard path definitions for NECB testing.
+  # Standard path definitions for NECB testing.
   def define_folders(file_folder)
     @test_folder = File.join(file_folder, '..')
     @root_folder = File.join(@test_folder, '../../..')
@@ -17,7 +19,7 @@ module NecbHelper
   end
 
   def method_output_folder(name="")
-    output_folder = File.join(@top_output_folder,self.class.ancestors[0].to_s.downcase,name.to_s.downcase)
+    output_folder = File.join(@top_output_folder, self.class.ancestors[0].to_s.downcase, name.to_s.downcase)
     FileUtils.rm_rf(output_folder)
     FileUtils.mkdir_p(output_folder)
     return output_folder
@@ -25,6 +27,7 @@ module NecbHelper
 
   def define_std_ranges
     @Templates = ['NECB2011', 'NECB2015', 'NECB2017', 'BTAPPRE1980']
+    @AllTemplates = ['NECB2011', 'NECB2015', 'NECB2017', 'NECB2020', 'BTAPPRE1980', 'BTAP1980TO2010']
     @AllBuildings = [
       'FullServiceRestaurant',
       'LargeHotel',
@@ -50,43 +53,44 @@ module NecbHelper
     ]
   end
 
-# Method to recover existing template (or create on if it has not been instantiated).
+  # Method to recover existing template (or create on if it has not been instantiated).
   def get_standard(template)
     standard = nil
     if @@standards.any? {|std| std.template == template}
       standard = @@standards.select{|std| std.template == template}.first
-      puts standard.class
     else
       standard = Standard.build(template)
-      puts standard.class
       @@standards << standard
     end
+    puts "**** Using template: #{standard.class}"
     return standard
   end
 
 
-# Standard method to run a measure for NECB testing. Parameters:
-#   model - the model object to be operated on
-#   template - version of NECB to use
-#   test_name - unique name of this test (used to create folders for output)
-#   necb_ref_hp = true if the caase is for the NECB reference model using heat pumps
-#   sql_db_vars_map - ???
-#   save_model_versions - logical to trigger saving of osm files before and after standards applied
-  def run_the_measure(model:, 
-                      template: 'NECB2011', 
-                      test_name:, 
-                      necb_ref_hp: false,
-                      sql_db_vars_map: nil, 
-                      save_model_versions: false)
+  # Standard method to run sizing for NECB testing. Parameters:
+  #   model - the model object to be operated on
+  #   template - version of NECB to use
+  #   test_name - unique name of this test (used to create folders for output)
+  #   second_run - true if a second sizing run is required.
+  #   necb_ref_hp = true if the caase is for the NECB reference model using heat pumps
+  #   sql_db_vars_map - ???
+  #   save_model_versions - logical to trigger saving of osm files before and after standards applied
+  def run_sizing(model:, 
+                 template: 'NECB2011', 
+                 test_name:, 
+                 second_run: false,
+                 necb_ref_hp: false,
+                 sql_db_vars_map: nil, 
+                 save_model_versions: false)
 
-    # Ensure we're doing this for NECB.
-    building_type = 'NECB'
-    climate_zone = 'NECB'
+    # Report what we are doing (helps when things go wrong!).
+    puts "**** Running measure for test class: #{self.class.ancestors[0]}"
+    puts "****   from method: #{caller_locations(1,1)[0].label}"
+    puts "****   with scenario name: #{test_name}"
+    puts "****   for template: #{template}"
 
     # Instantiate the required version of standards.
-    puts "*** Passed standard #{template}"
     standard = get_standard(template)
-    puts "### Using standard #{standard.template}"
 
     # Define output folders.
     test_method_name = self.class.ancestors[0].to_s.downcase
@@ -98,16 +102,22 @@ module NecbHelper
       FileUtils.mkdir_p(sizing_dir)
     end
 
-    # Perform a sizing run.
-    if standard.model_run_sizing_run(model, "#{sizing_dir}/SizingRun1") == false
-      puts "could not find sizing run #{sizing_dir}/SizingRun1"
-      assert(false, "Failure in sizing run 1 wile running test: #{self.class.ancestors[0]}")
+    # Save model before sizing.
+    BTAP::FileIO.save_osm(model, "#{models_dir}/pre-sizing.osm") if save_model_versions
+
+    # Perform first sizing run.
+    sizing_folder = "#{sizing_dir}/SR1"
+    if standard.model_run_sizing_run(model, sizing_folder) == false
+      puts "could not find sizing run #{sizing_folder}"
+      assert(false, "Failure in sizing run wile running test: #{self.class.ancestors[0]}")
     else
-      puts "found sizing run #{sizing_dir}/SizingRun1"
+      puts "found sizing run #{sizing_folder}"
     end
 
-    # Save model before applying standard.
-    BTAP::FileIO.save_osm(model, "#{models_dir}/before.osm") if save_model_versions
+    # Apply HVAC assumptions for efficiency etc.
+    # Ensure we're doing this for NECB.
+    building_type = 'NECB'
+    climate_zone = 'NECB'
 
     # Need to set prototype assumptions so that HRV added.
     standard.model_apply_prototype_hvac_assumptions(model, building_type, climate_zone)
@@ -115,21 +125,29 @@ module NecbHelper
     # Apply the HVAC efficiency standard.
     standard.model_apply_hvac_efficiency_standard(model, climate_zone, necb_ref_hp: necb_ref_hp)
 
-    # Do another sizing run after applying the hvac assumptions and efficiency standards 
-    #  to properly apply the pump rules.
-    #if standard.model_run_sizing_run(model, "#{sizing_dir}/SizingRun2") == false
-    #  puts "could not find sizing run #{sizing_dir}/SizingRun2"
-    #  assert(false, "Failure in sizing run 2 wile running test: #{self.class.ancestors[0]}")
-    #else
-    #  puts "found sizing run #{sizing_dir}/SizingRun2"
-    #end
+    # Second sizing run (if requested).
+    if second_run
+      
+      # Save model before sizing run 2.
+      BTAP::FileIO.save_osm(model, "#{models_dir}/pre-sizing2.osm") if save_model_versions
 
-    # Save model after applying standard.
-    BTAP::FileIO.save_osm(model, "#{models_dir}/after.osm") if save_model_versions
+      # Do another sizing run after applying the hvac assumptions and efficiency standards 
+      #  to properly apply the pump rules.
+      sizing_folder = "#{sizing_dir}/SR2"
+      if standard.model_run_sizing_run(model, sizing_folder) == false
+        puts "could not find sizing run #{sizing_folder}"
+        assert(false, "Failure in sizing run wile running test: #{self.class.ancestors[0]}")
+      else
+        puts "found sizing run #{sizing_folder}"
+      end
+    end
+
+    # Save model after sizing.
+    BTAP::FileIO.save_osm(model, "#{models_dir}/post-sizing.osm") if save_model_versions
   end
 
-# Check if two files are identical with some added smarts
-# (used in place of simple ruby methods)
+  # Check if two files are identical with some added smarts
+  # (used in place of simple ruby methods)
   def file_compare(expected_results_file:, test_results_file:, msg: "Files do not match", type: nil)
   
     #if type == "fred" # Place holder for other file compare options. So far just defined the default behaviour.
@@ -146,6 +164,10 @@ module NecbHelper
         same = le.eql?(lt)
         break if !same
       end
+
+      # Close files before assert.
+      fe.close
+      ft.close
       expected_results_file_path=Pathname.new(expected_results_file).cleanpath
       test_results_file_path=Pathname.new(test_results_file).cleanpath
       comp_files_str="  Compare #{expected_results_file_path} with #{test_results_file_path}. File contents differ!"
