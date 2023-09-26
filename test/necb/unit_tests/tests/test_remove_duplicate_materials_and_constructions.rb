7ac98ce69cdea7c925bbb298394088e15f215c3d
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2023-09-26 08:58:59 -0400

    tidy up of load model and assert calls

--- test/necb/unit_tests/tests/test_remove_duplicate_materials_and_constructions.rb
+++ test/necb/unit_tests/tests/test_remove_duplicate_materials_and_constructions.rb
@@ -14,18 +14,15 @@ class NECB_RemoveDuplicateModelObjects_Tests < MiniTest::Test
   end
 
   def test_remove_duplicate_model_objects
-    # read a sample model called fsr model present within the test/necb/models/fsr.osm
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    osm_path = File.join(@resources_folder, 'fsr.osm')
-    path = OpenStudio::Path.new(osm_path)
-    model = translator.loadModel(path)
-    model = model.get
+
+    # Load test model.
+    model = BTAP::FileIO.load_osm(File.join(@resources_folder,"fsr.osm"))
 
     old_number_of_objects = model.getModelObjects.length
     new_model = BTAP::FileIO::remove_duplicate_materials_and_constructions(model)
     new_number_of_objects = new_model.getModelObjects.length
 
-    puts "Number of objects removed: #{old_number_of_objects - new_number_of_objects}"
-    assert((old_number_of_objects - new_number_of_objects > 0))
+    diff = old_number_of_objects - new_number_of_objects
+    assert_equal(17, diff, "test_remove_duplicate_model_objects: expected to remove 17 objects, removed #{diff}.")
   end
 end
