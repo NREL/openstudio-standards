099ab2dce00ce99905a53427705294c7aead477d
srgilani <s.gilani@outlook.com>
2023-11-22 22:54:19 -0500

    Revised the energy_eui_data method in btap_data.rb in a way to exclude district heating and cooling from total EUI and heating and cooling EUI when the HVAC system is GSHP

--- lib/openstudio-standards/standards/necb/common/btap_data.rb
+++ lib/openstudio-standards/standards/necb/common/btap_data.rb
@@ -47,7 +47,7 @@ class BTAPData
     @btap_data.merge!(building_costing_data(cost_result)) unless cost_result.nil?
     @btap_data.merge!(climate_data)
     @btap_data.merge!(service_water_heating_data)
-    @btap_data.merge!(energy_eui_data(model)) #Sara
+    @btap_data.merge!(energy_eui_data(model))
     @btap_data.merge!(energy_peak_data)
     @btap_data.merge!(utility(model))
     @btap_data.merge!(unmet_hours(model))
@@ -1412,46 +1412,40 @@ class BTAPData
      'energy_eui_total_gj_per_m_sq',
      'energy_eui_heat recovery_gj_per_m_sq',
      'energy_eui_water systems_gj_per_m_sq'].each { |end_use| data[end_use] = 0.0 }
+
+    # Check if the HVAC of the model is GSHP
+    plant_loops = model.getPlantLoops
+    model_has_how_many_GSHP = 0.0
+    plant_loops.each do |plantloop|
+      if plantloop.name.to_s.upcase.include? "GLHX"
+        model_has_how_many_GSHP += 1.0
+      end
+    end
+
     # Get E+ End use table from sql
     table = get_sql_table_to_json(@model, 'AnnualBuildingUtilityPerformanceSummary', 'Entire Facility', 'End Uses')['table']
     # Get rid of totals and averages rows.. I want just the
     table.delete_if { |row| !!(row['name'] =~ /Total|Average/) }
-    puts table
     table.each do |row|
-      puts "row is #{row}" #Sara
       # skip name and water_m3 columns
       energy_columns = row.select { |k, v| (k != 'name') && (k != 'water_m3') }
       # Store eui by use name.
       data["energy_eui_#{row['name'].downcase}_gj_per_m_sq"] = energy_columns.inject(0) { |sum, tuple| sum += tuple[1] } / @conditioned_floor_area_m_sq
     end
+
     data['energy_eui_total_gj_per_m_sq'] = 0.0
-    ['natural_gas_GJ', 'electricity_GJ', 'additional_fuel_GJ'].each do |column|
-      puts "column is #{column}" #Sara
+
+    ['natural_gas_GJ', 'electricity_GJ', 'additional_fuel_GJ', 'district_cooling_GJ', 'district_heating_GJ'].each do |column|
       data["energy_eui_#{column.downcase}_per_m_sq"] = table.inject(0) { |sum, row| sum + (row[column].nil? ? 0.0 : row[column]) } / @conditioned_floor_area_m_sq
       data['energy_eui_total_gj_per_m_sq'] += data["energy_eui_#{column.downcase}_per_m_sq"] unless data["energy_eui_#{column.downcase}_per_m_sq"].nil?
     end
-    #Sara
-    plant_loops = model.getPlantLoops
-    model_has_how_many_GSHP = 0.0
-    plant_loops.each do |plantloop|
-      puts "plantloop name is #{plantloop}"
-      if plantloop.name.to_s.upcase.include? "GLHX"
-        puts "does not have GLHX"
-        model_has_how_many_GSHP += 1.0
-      end
+
+    # if the HVAC of the model is GSHP, district heating and cooling must be removed from EUIs for heating and cooling and total EUI
+    if model_has_how_many_GSHP > 0.0
+      data['energy_eui_heating_gj_per_m_sq'] -= data['energy_eui_district_heating_gj_per_m_sq']
+      data['energy_eui_cooling_gj_per_m_sq'] -= data['energy_eui_district_cooling_gj_per_m_sq']
+      data['energy_eui_total_gj_per_m_sq'] -= (data['energy_eui_district_heating_gj_per_m_sq'] + data['energy_eui_district_cooling_gj_per_m_sq'])
     end
-    puts "model_has_how_many_GSHP is #{model_has_how_many_GSHP}"
-    if model_has_how_many_GSHP < 1.0
-      puts "Sara"
-      ['district_cooling_GJ', 'district_heating_GJ'].each do |column|
-        puts "column is #{column}"  #Sara
-        data["energy_eui_#{column.downcase}_per_m_sq"] = table.inject(0) { |sum, row| sum + (row[column].nil? ? 0.0 : row[column]) } / @conditioned_floor_area_m_sq
-      end
-    end
-    puts "energy_eui_cooling_gj_per_m_sq is #{data['energy_eui_cooling_gj_per_m_sq']}"
-
-
-
 
     # Get total and net site energy use intensity
     # Note: 'Total Site Energy' is the "gross" energy used by a building.
