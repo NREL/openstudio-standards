a1f81380d264909c48fe3674676f46b7281fe98a
Eric Ringold <ericringold@gmail.com>
2024-01-02 09:32:33 -0700

    require space module

--- lib/openstudio-standards.rb
+++ lib/openstudio-standards.rb
@@ -27,6 +27,9 @@ module OpenstudioStandards
   require_relative 'openstudio-standards/schedules/information'
   require_relative 'openstudio-standards/schedules/parametric'
 
+  # Space Module
+  require_relative 'openstudio-standards/space/space'
+  
   # HVAC Module
   require_relative 'openstudio-standards/hvac/cbecs_hvac'
 
