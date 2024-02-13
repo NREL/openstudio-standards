0296c200ebd754159dbc2215ba45f9222d1f798f
Weili Xu <weili.xu@pnnl.gov>
2024-02-13 09:40:27 -0800

    Add schedule limit type to ruleset schedule simple value adjustment to prevent simulation errors that the schedule value over the lower or upper boundary.

--- lib/openstudio-standards/schedules/modify.rb
+++ lib/openstudio-standards/schedules/modify.rb
@@ -105,6 +105,19 @@ module OpenstudioStandards
     def self.schedule_ruleset_simple_value_adjust(schedule_ruleset, value, modification_type = 'Multiplier')
       # gather profiles
       profiles = []
+      # negative infinity
+      lower_bound = -1.0 / 0.0
+      # positive infinity
+      upper_bound = Float::INFINITY
+      if schedule_ruleset.scheduleTypeLimits.is_initialized
+        scheduleTypeLimits = schedule_ruleset.scheduleTypeLimits.get
+        if scheduleTypeLimits.lowerLimitValue.is_initialized
+          lower_bound = scheduleTypeLimits.lowerLimitValue.get
+        end
+        if scheduleTypeLimits.upperLimitValue.is_initialized
+          upper_bound = scheduleTypeLimits.upperLimitValue.get
+        end
+      end
       default_profile = schedule_ruleset.to_ScheduleRuleset.get.defaultDaySchedule
       profiles << default_profile
       rules = schedule_ruleset.scheduleRules
@@ -120,10 +133,12 @@ module OpenstudioStandards
           case modification_type
           when 'Multiplier', 'Percentage'
             # percentage was used early on but Multiplier is preferable
-            profile.addValue(times[i], sch_value * value)
+            new_value = [lower_bound, [upper_bound, sch_value * value].min].max
+            profile.addValue(times[i], new_value)
           when 'Sum', 'Value'
             # value was used early on but Sum is preferable
-            profile.addValue(times[i], sch_value + value)
+            new_value = [lower_bound, [upper_bound, sch_value + value].min].max
+            profile.addValue(times[i], new_value)
           end
           i += 1
         end
