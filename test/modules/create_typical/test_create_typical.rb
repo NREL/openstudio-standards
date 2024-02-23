c1c5d3d7bee18d2512c7953bb30df3f97def7ec0
gonz102 <juan.gonzalez@pnnl.gov>
2024-02-23 10:11:00 -0500

    Added test for HVAC to zone mapping

--- test/modules/create_typical/test_create_typical.rb
+++ test/modules/create_typical/test_create_typical.rb
@@ -31,4 +31,35 @@ class TestCreateTypical < Minitest::Test
     assert(model.getSpaceTypes.size > 0)
     assert(model.getDefaultConstructionSets.size > 0)
   end
+
+  def test_create_typical_building_from_model_with_hvac_mapping
+    # load model and set up weather file
+    template = '90.1-2013'
+    climate_zone = 'ASHRAE 169-2013-4A'
+    std = Standard.build(template)
+    model = std.safe_load_model("#{File.dirname(__FILE__)}/../../../data/geometry/ASHRAESmallOffice.osm")
+    OpenstudioStandards::Weather.model_set_building_location(model, climate_zone: climate_zone)
+
+    # Read in HVAC to zone mapping with 4 zones served by PTAC and 1 by a PSZ AC
+    current_dir = File.dirname(__FILE__)
+    hvac_zone_json_path = File.join("#{current_dir}",'data','hvac_zone_mapping.json')
+    hvac_zone_json = File.read(hvac_zone_json_path)
+    hvac_mapping_hash = JSON.parse(hvac_zone_json)
+
+    # apply create typical with zone mapping
+    starting_size = model.getModelObjects.size
+    result = @create.create_typical_building_from_model(model, template, climate_zone: climate_zone,
+                                                        user_hvac_mapping: hvac_mapping_hash)
+    ending_size = model.getModelObjects.size
+    ptacs = model.getZoneHVACPackagedTerminalAirConditioners
+    psz_ac = model.getAirLoopHVACUnitarySystems
+
+    # Check that JSON specs were applied
+    assert(result)
+    assert(starting_size < ending_size)
+    assert(ptacs.length==4)
+    assert(psz_ac.length==1)
+
+  end
+
 end
