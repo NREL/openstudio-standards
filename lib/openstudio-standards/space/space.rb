1a202fce23b9dca08765a08c02c3c758fd92b511
Eric Ringold <ericringold@gmail.com>
2024-04-08 16:03:48 -0600

    avoid imprecise floating point math causing occ threshold to not trigger

--- lib/openstudio-standards/space/space.rb
+++ lib/openstudio-standards/space/space.rb
@@ -394,7 +394,7 @@ module OpenstudioStandards
       # get nested array of 8760 values of the total occupancy at each hour of each schedule
       all_schedule_hourly_occ = []
       occ_schedules_num_occ.each do |occ_sch, num_occ|
-        all_schedule_hourly_occ << OpenstudioStandards::Schedules.schedule_get_hourly_values(occ_sch).map { |i| i * num_occ }
+        all_schedule_hourly_occ << OpenstudioStandards::Schedules.schedule_get_hourly_values(occ_sch).map { |i| (i * num_occ).round(4) }
       end
 
       # total occupancy from all people
