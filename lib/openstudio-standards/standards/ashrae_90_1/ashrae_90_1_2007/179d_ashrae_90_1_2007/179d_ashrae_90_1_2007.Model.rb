28a512adc523c1e388050bd0f43cf528e919fc44
Julien Marrec <julien.marrec@gmail.com>
2024-04-18 12:58:43 +0200

    Rubocop

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.Model.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.Model.rb
@@ -1,23 +1,22 @@
 class ACM179dASHRAE9012007
   def self.__model_get_primary_building_type(model)
-
     building_types = {}
 
     building = model.getBuilding
     building_level_bt = nil
     if building.standardsBuildingType.is_initialized
-      building_level_bt =building.standardsBuildingType.get
-      OpenStudio.logFree(OpenStudio::Debug, "openstudio.standards.Model", "found Building level standardsBuildingType = '#{building_level_bt}'")
+      building_level_bt = building.standardsBuildingType.get
+      OpenStudio.logFree(OpenStudio::Debug, 'openstudio.standards.Model', "found Building level standardsBuildingType = '#{building_level_bt}'")
     end
-
 
     model.getSpaceTypes.sort.each do |space_type|
       # populate hash of building types
       if !space_type.standardsBuildingType.is_initialized
         next
       end
+
       bldg_type = space_type.standardsBuildingType.get
-      OpenStudio.logFree(OpenStudio::Debug, "openstudio.standards.Model", "found building type for Space Type '#{space_type.name}' = '#{bldg_type}'")
+      OpenStudio.logFree(OpenStudio::Debug, 'openstudio.standards.Model', "found building type for Space Type '#{space_type.name}' = '#{bldg_type}'")
       if building_types.key?(bldg_type)
         building_types[bldg_type] += space_type.floorArea
       else
@@ -27,23 +26,23 @@ class ACM179dASHRAE9012007
 
     if building_types.empty?
       if building_level_bt.nil?
-        OpenStudio.logFree(OpenStudio::Error, "openstudio.standards.Model", "Cannot identify a single building type in model, none of your #{model.getSpaceTypes.size} SpaceTypes have a standardsBuildingType assigned and neither does the Building")
-        raise "No Primary Building Type found"
+        OpenStudio.logFree(OpenStudio::Error, 'openstudio.standards.Model', "Cannot identify a single building type in model, none of your #{model.getSpaceTypes.size} SpaceTypes have a standardsBuildingType assigned and neither does the Building")
+        raise 'No Primary Building Type found'
       else
-        OpenStudio.logFree(OpenStudio::Info, "openstudio.standards.Model", "No area determination based on space types found, using Building level standardsBuildingType = '#{building_level_bt}'")
+        OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.Model', "No area determination based on space types found, using Building level standardsBuildingType = '#{building_level_bt}'")
         return building_level_bt
       end
     end
 
-    space_type_level_bt = building_types.max_by{|k, v| v}.first
+    space_type_level_bt = building_types.max_by { |_, v| v }.first
     if !building_level_bt.nil?
       if building_level_bt != space_type_level_bt
-        OpenStudio.logFree(OpenStudio::Warn, "openstudio.standards.Model", "The Building has standardsBuildingType '#{building_level_bt}' while the area determination based on space types has '#{space_type_level_bt}'. Preferring the Space Type one")
+        OpenStudio.logFree(OpenStudio::Warn, 'openstudio.standards.Model', "The Building has standardsBuildingType '#{building_level_bt}' while the area determination based on space types has '#{space_type_level_bt}'. Preferring the Space Type one")
       end
       return space_type_level_bt
     end
 
-    OpenStudio.logFree(OpenStudio::Info, "openstudio.standards.Model", "Building doesn't have a standardsBuildingType, using the area determination based on space types = '#{space_type_level_bt}'")
+    OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.Model', "Building doesn't have a standardsBuildingType, using the area determination based on space types = '#{space_type_level_bt}'")
     return space_type_level_bt
   end
 
