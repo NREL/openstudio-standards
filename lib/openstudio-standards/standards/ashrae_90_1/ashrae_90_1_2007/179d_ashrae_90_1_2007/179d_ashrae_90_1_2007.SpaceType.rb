18edd2fe633350bd7b8741e96ec608b201f731af
Julien Marrec <julien.marrec@gmail.com>
2024-04-25 15:49:25 +0200

    For anything we don't define (Ventilation, exhaust, lighting control), use a vanilla 90.1-2007 instance and merge into ours

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.SpaceType.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.SpaceType.rb
@@ -24,6 +24,15 @@ class ACM179dASHRAE9012007
       space_type_properties = {}
     end
 
+    # This merges the ventilation, exhaust and lighting controls
+    data2007 = @std_2007.space_type_get_standards_data(space_type)
+    if data2007.nil?
+      OpenStudio.logFree(OpenStudio::Warn, 'openstudio.standards.SpaceType', "Space type properties from ASHRAE 90.1-2007 lookup failed")
+    else
+      space_type_properties = data2007.merge(space_type_properties)
+      space_type_properties['space_type_2007'] = data2007['space_type']
+    end
+
     return space_type_properties
   end
 end
