2fdab0b96a00170860b69da9b1dd60baebf069e0
Julien Marrec <julien.marrec@gmail.com>
2024-05-07 23:38:19 +0200

    Unbrick the code and change return type

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.SubSurface.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.SubSurface.rb
@@ -2,13 +2,13 @@
 
 class ACM179dASHRAE9012007
   def sub_surface_get_window_property(sub_surface)
-    sql = sub_surface.model.sqlFile.get
-    if !sql.is_initialized
+    sql_file = sub_surface.model.sqlFile
+    if !sql_file.is_initialized
       OpenStudio.logFree(OpenStudio::Error, 'openstudio.standards.SubSurface', 'Model has no sql file containing results, cannot lookup data.')
       return nil
     end
+    sql_file = sql_file.get
 
-    window_property = {}
     sub_surface_name = sub_surface.name.to_s
 
     # get SHGC
@@ -17,18 +17,35 @@ class ACM179dASHRAE9012007
     # get U-value
     window_u_value = sub_surface.assemblyUFactor.get
 
-    # get opening area
+    # get opening area, including the frame (with is added to the sub_surface vertices)
+    window_area = sub_surface.roughOpeningArea
+    window_area *= sub_surface.multiplier
+    space_ = sub_surface.space
+    if space_.is_initialized
+      window_area *= space_.get.multiplier
+    end
     var_val_query = "SELECT Value FROM TabularDataWithStrings WHERE ReportName = 'EnvelopeSummary' AND ReportForString = 'Entire Facility' AND TableName = 'Exterior Fenestration' AND RowName = '#{sub_surface_name.upcase}' AND ColumnName = 'Area of Multiplied Openings' AND Units = 'm2'"
-    val = sqlFile.execAndReturnFirstDouble(var_val_query)
+    val = sql_file.execAndReturnFirstDouble(var_val_query)
     window_area = val.to_f.round(3)
+
     # get surface type
-    surface_type = sub_surface.surface.surfaceType
+    surface_type = nil
+    surface_ = sub_surface.surface
+    if surface_.is_initialized
+      surface_type = surface_.get.surfaceType
+    end
+
     # get window type
     window_type = sub_surface.subSurfaceType
-    # add window information to hash
-    window_property[surface_type][window_type][sub_surface_name]['area_m2'] = window_area
-    window_property[surface_type][window_type][sub_surface_name]['shgc'] = window_shgc
-    window_property[surface_type][window_type][sub_surface_name]['u_value'] = window_u_value
+
+    window_property = {
+      'name' => sub_surface_name,
+      'window_type' => window_type,
+      'surface_type' => surface_type,
+      'area_m2' => window_area,
+      'shgc' => window_shgc,
+      'u_value' => window_u_value
+    }
     return window_property
   end
 end
