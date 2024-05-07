a96c258ed3f55b0ba32a45a14e90b86f1d2e1f19
Julien Marrec <julien.marrec@gmail.com>
2024-05-07 22:59:10 +0200

    Use SDK methods

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.SubSurface.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.SubSurface.rb
@@ -5,19 +5,16 @@ class ACM179dASHRAE9012007
             OpenStudio.logFree(OpenStudio::Error, 'openstudio.standards.SubSurface', 'Model has no sql file containing results, cannot lookup data.')
             return nil
         end
-    
+
         window_property = {}
         sub_surface_name = sub_surface.name.to_s
+
         # get SHGC
-        # Note that the SQL Queries can be swapped out for SDK functions
-        # https://github.com/NREL/OpenStudio/blob/develop/src/model/SubSurface.cpp#L1305-L1348
-        var_val_query = "SELECT Value FROM TabularDataWithStrings WHERE ReportName = 'EnvelopeSummary' AND ReportForString = 'Entire Facility' AND TableName = 'Exterior Fenestration' AND RowName = '#{sub_surface_name.upcase}' AND ColumnName = 'Assembly SHGC'"
-        val = sqlFile.execAndReturnFirstDouble(var_val_query)
-        window_shgc = val.to_f.round(3)
+        window_shgc = sub_surface.assemblySHGC.get
+
         # get U-value
-        var_val_query = "SELECT Value FROM TabularDataWithStrings WHERE ReportName = 'EnvelopeSummary' AND ReportForString = 'Entire Facility' AND TableName = 'Exterior Fenestration' AND RowName = '#{sub_surface_name.upcase}' AND ColumnName = 'Assembly U-Factor' AND Units = 'W/m2-K'"
-        val = sqlFile.execAndReturnFirstDouble(var_val_query)
-        window_u_value = val.to_f.round(3)
+        window_u_value = sub_surface.assemblyUFactor.get
+
         # get opening area
         var_val_query = "SELECT Value FROM TabularDataWithStrings WHERE ReportName = 'EnvelopeSummary' AND ReportForString = 'Entire Facility' AND TableName = 'Exterior Fenestration' AND RowName = '#{sub_surface_name.upcase}' AND ColumnName = 'Area of Multiplied Openings' AND Units = 'm2'"
         val = sqlFile.execAndReturnFirstDouble(var_val_query)
@@ -30,6 +27,6 @@ class ACM179dASHRAE9012007
         window_property[surface_type][window_type][sub_surface_name]['area_m2'] = window_area
         window_property[surface_type][window_type][sub_surface_name]['shgc'] = window_shgc
         window_property[surface_type][window_type][sub_surface_name]['u_value'] = window_u_value
-        return window_property   
+        return window_property
     end
 end
