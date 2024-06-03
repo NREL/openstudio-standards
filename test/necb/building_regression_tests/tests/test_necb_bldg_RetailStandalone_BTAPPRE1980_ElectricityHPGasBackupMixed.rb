4e8864e7d05ce67efcad8da6e33b1a7d562e5b8c
Nicholas Pneumaticos <nicholas.pneumaticos@nrcan-rncan.gc.ca>
2024-06-03 12:56:09 -0400

    Formatting, removing unnessecary require, adjusting print statement

--- test/necb/building_regression_tests/tests/test_necb_bldg_RetailStandalone_BTAPPRE1980_ElectricityHPGasBackupMixed.rb
+++ test/necb/building_regression_tests/tests/test_necb_bldg_RetailStandalone_BTAPPRE1980_ElectricityHPGasBackupMixed.rb
@@ -1,23 +1,29 @@
 require_relative '../../../helpers/minitest_helper'
-require_relative '../../../helpers/create_doe_prototype_helper'
 require_relative '../../../helpers/compare_models_helper'
 require_relative '../resources/regression_helper'
 
 class Test_RetailStandalone_BTAPPRE1980_ElectricityHPGasBackupMixed < NECBRegressionHelper
-def setup()
-super()
-end
-def test_BTAPPRE1980_RetailStandalone_regression_ElectricityHPGasBackupMixed()
-result, diff = create_model_and_regression_test(building_type: 'RetailStandalone',primary_heating_fuel: 'ElectricityHPGasBackupMixed', epw_file:  'CAN_AB_Calgary.Intl.AP.718770_CWEC2020.epw',template: 'BTAPPRE1980', run_simulation: false)
-if result == false
-puts "JSON terse listing of diff-errors."
-puts diff
-puts "Pretty listing of diff-errors for readability."
-puts JSON.pretty_generate( diff )
-puts "You can find the saved json diff file here test/necb/regression_models/RetailStandalone-BTAPPRE1980-ElectricityHPGasBackupMixed_CAN_AB_Calgary.Intl.AP.718770_CWEC2020_diffs.json"
-puts "outputing errors here. "
-puts diff["diffs-errors"] if result == false
-end
-assert(result, diff)
-end
+  def setup()
+    super()
+  end
+
+  def test_BTAPPRE1980_RetailStandalone_regression_ElectricityHPGasBackupMixed()
+    result, diff = create_model_and_regression_test(
+      building_type:        'RetailStandalone',
+      primary_heating_fuel: 'ElectricityHPGasBackupMixed', 
+      epw_file:             'CAN_AB_Calgary.Intl.AP.718770_CWEC2020.epw',
+      template:             'BTAPPRE1980', 
+      run_simulation:       false
+    )
+    if result == false
+      puts "JSON terse listing of diff-errors."
+      puts diff
+      puts "Pretty listing of diff-errors for readability."
+      puts JSON.pretty_generate( diff )
+      puts "You can find the saved json diff file here test/necb/building_regression_tests/expected_results/RetailStandalone-BTAPPRE1980-ElectricityHPGasBackupMixed_CAN_AB_Calgary.Intl.AP.718770_CWEC2020_diffs.json"
+      puts "outputing errors here. "
+      puts diff["diffs-errors"] if result == false
+    end
+    assert(result, diff)
+  end
 end
