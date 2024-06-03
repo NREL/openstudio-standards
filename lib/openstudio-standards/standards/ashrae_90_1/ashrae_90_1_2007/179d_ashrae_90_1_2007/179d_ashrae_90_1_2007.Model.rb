a62fd1e2972ed33f674142dca1cf2694ae6f6f7c
brianlball <brian.ball@nrel.gov>
2024-06-03 14:03:38 -0400

    move model_prm_baseline_system_number from 90.1-2007 into 179D subclass

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.Model.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.Model.rb
@@ -128,4 +128,42 @@ class ACM179dASHRAE9012007
 
     return results
   end
+
+  def model_prm_baseline_system_number(_model, _climate_zone, area_type, _fuel_type, area_ft2, num_stories, custom)
+    OpenStudio.logFree(OpenStudio::Info, 'openstudio.Model.prm', '179d: Heat Storage area applied as 90.1-2007 with addenda dn')
+    sys_num = nil
+
+    # Set the area limit
+    limit_ft2 = 25_000
+
+    # Customization for Xcel EDA.
+    # No special retail category
+    # for regular 90.1-2010.
+    if custom != 'Xcel Energy CO EDA' && (area_type == 'retail')
+      area_type = 'nonresidential'
+    end
+
+    case area_type
+    when 'residential'
+      sys_num = '1_or_2'
+    when 'nonresidential'
+      # nonresidential and 3 floors or less and <25,000 ft2
+      if num_stories <= 3 && area_ft2 < limit_ft2
+        sys_num = '3_or_4'
+      # nonresidential and 4 or 5 floors or 5 floors or less and 25,000 ft2 to 150,000 ft2
+      elsif ((num_stories == 4 || num_stories == 5) && area_ft2 < limit_ft2) || (num_stories <= 5 && (area_ft2 >= limit_ft2 && area_ft2 <= 150_000))
+        sys_num = '5_or_6'
+      # nonresidential and more than 5 floors or >150,000 ft2
+      elsif num_stories >= 5 || area_ft2 > 150_000
+        sys_num = '7_or_8'
+      end
+    when 'heatedonly'
+      sys_num = '9_or_10'
+    when 'retail'
+      # Should only be hit by Xcel EDA
+      sys_num = '3_or_4'
+    end
+
+    return sys_num
+  end
 end
