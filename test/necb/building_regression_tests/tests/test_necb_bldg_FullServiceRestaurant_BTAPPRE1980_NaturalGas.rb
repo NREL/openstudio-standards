302faefa4279837a1373a5856e269cf563626b89
Nicholas Pneumaticos <nicholas.pneumaticos@nrcan-rncan.gc.ca>
2024-06-07 15:37:12 -0400

    Removed redundant require 'compare_models_helper.rb'

--- test/necb/building_regression_tests/tests/test_necb_bldg_FullServiceRestaurant_BTAPPRE1980_NaturalGas.rb
+++ test/necb/building_regression_tests/tests/test_necb_bldg_FullServiceRestaurant_BTAPPRE1980_NaturalGas.rb
@@ -1,5 +1,4 @@
 require_relative '../../../helpers/minitest_helper'
-require_relative '../../../helpers/compare_models_helper'
 require_relative '../resources/regression_helper'
 
 class Test_FullServiceRestaurant_BTAPPRE1980_NaturalGas < NECBRegressionHelper
