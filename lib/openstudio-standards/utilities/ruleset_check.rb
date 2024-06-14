65c897fd460436ef21c9f13872b4b06cf360ceaf
Weili Xu <weili.xu@pnnl.gov>
2024-06-14 12:18:14 -0700

    add output variable

--- lib/openstudio-standards/utilities/ruleset_check.rb
+++ lib/openstudio-standards/utilities/ruleset_check.rb
@@ -22,14 +22,19 @@ module OpenStudioStandards
       end
       return true
     end
-
+    
     def self.tag_spaces(model, ruleset_space_rule)
       42
     end
 
+    # Revise the output variables to prepare data export for RPD generation
+    #
+    # @param model [OpenStudio::Model::Model]
+    # @return [Boolean] returns true if successful, false if not
     def self.export_json_output(model)
       # clear out the output, output:variable, output:schedule, outputcontrol:table:style and output:table:summaryreports
       # reformt it a different set.
+      # reset output json
       output_json = model.getOutputJSON
       unless output_json
         output_json = OpenStudio::Model::OutputJSON.new(model)
@@ -39,12 +44,14 @@ module OpenStudioStandards
       output_json.setOutputCBOR(false)
       output_json.setOutputMessagePack(false)
 
+      # reset output schedule
       output_schedules = model.getOutputSchedules
       unless output_schedules
         output_schedules = OpenStudio::Model::OutputSchedules.new(model)
       end
       output_schedules.setKeyField('Hourly')
 
+      # reset output control table style
       outputcontrol_table_style = model.getOutputControlTableStyle
       unless outputcontrol_table_style
         outputcontrol_table_style = OpenStudio::Model::OutputControlTableStyle.new(model)
@@ -52,12 +59,19 @@ module OpenStudioStandards
       outputcontrol_table_style.setColumnSeparator('HTML')
       outputcontrol_table_style.setUnitConversion('None')
 
+      # reset output table summary reports
       output_table_summaryreports = model.getOutputTableSummaryReports
       unless output_table_summaryreports
         output_table_summaryreports = OpenStudio::Model::OutputTableSummaryReports.new(model)
       end
       output_table_summaryreports.removeAllSummaryReports
-      outputcontrol_table_style.addSummaryReport('AllSummaryAndMonthly')
+      output_table_summaryreports.addSummaryReport('AllSummaryAndMonthly')
+
+      # remove all output variables
+      model.getOutputVariables.each {|ivar| ivar.remove}
+      output_variable = OpenStudio::Model::OutputVariable.new('Schedule Value', model)
+      output_variable.setReportingFrequency('hourly')
+      output_variable.setKeyValue('*')
     end
   end
 end
