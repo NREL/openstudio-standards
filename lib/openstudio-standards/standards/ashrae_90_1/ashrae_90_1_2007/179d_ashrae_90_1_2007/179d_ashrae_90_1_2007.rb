0981b3bfc330a157a802a9232dd27a9a74a29997
JanghyunJK <janghyun.kim@nrel.gov>
2024-07-26 10:14:36 -0600

    override incorrect effs for packaged heat pumps

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.rb
@@ -55,6 +55,7 @@ class ACM179dASHRAE9012007 < ASHRAE9012007
     end
 
     # override values in 90.1-2007 jsons that are no longer correct compared to the latest 90.1-2007
+    # packaged ac units
     @standards_data['unitary_acs'].each do |info|
       if info['cooling_type'] == 'AirCooled' &&
          info['heating_type'] == 'All Other' &&
@@ -99,7 +100,7 @@ class ACM179dASHRAE9012007 < ASHRAE9012007
         info['minimum_integrated_energy_efficiency_ratio'] = nil
       end
     end
-
+    # boilers
     @standards_data['boilers'].each do |info|
       if info['fluid_type'] == 'Hot Water' &&
          almost_equal?(300000.0, info['minimum_capacity']) &&
@@ -109,7 +110,6 @@ class ACM179dASHRAE9012007 < ASHRAE9012007
         info['minimum_combustion_efficiency'] = nil
       end
     end
-
     @standards_data['boilers'].each do |info|
       if info['fluid_type'] == 'Hot Water' &&
          almost_equal?(2500000.01, info['minimum_capacity']) &&
@@ -117,6 +117,63 @@ class ACM179dASHRAE9012007 < ASHRAE9012007
         info['minimum_thermal_efficiency'] = nil
         info['minimum_annual_fuel_utilization_efficiency'] = nil
         info['minimum_combustion_efficiency'] = 0.82
+      end
+    end
+    # packaged heat pumps
+    @standards_data['heat_pumps'].each do |info|
+      if info['cooling_type'] == 'AirCooled' &&
+         info['heating_type'] == 'All Other' &&
+         info['subcategory'] == 'Single Package' &&
+         almost_equal?(65000.0, info['minimum_capacity']) &&
+         almost_equal?(134999.0, info['maximum_capacity'])
+        info['minimum_seasonal_efficiency'] = nil
+        info['minimum_full_load_efficiency'] = 10.8
+        info['minimum_iplv'] = nil
+        info['minimum_integrated_energy_efficiency_ratio'] = nil
+      end
+    end
+    @standards_data['heat_pumps'].each do |info|
+      if info['cooling_type'] == 'AirCooled' &&
+         info['heating_type'] == 'All Other' &&
+         info['subcategory'] == 'Single Package' &&
+         almost_equal?(135000.0, info['minimum_capacity']) &&
+         almost_equal?(239999.0, info['maximum_capacity'])
+        info['minimum_seasonal_efficiency'] = nil
+        info['minimum_full_load_efficiency'] = 10.4
+        info['minimum_iplv'] = nil
+        info['minimum_integrated_energy_efficiency_ratio'] = nil
+      end
+    end
+    @standards_data['heat_pumps'].each do |info|
+      if info['cooling_type'] == 'AirCooled' &&
+         info['heating_type'] == 'All Other' &&
+         info['subcategory'] == 'Single Package' &&
+         almost_equal?(240000.0, info['minimum_capacity']) &&
+         almost_equal?(9999999.0, info['maximum_capacity'])
+        info['minimum_seasonal_efficiency'] = nil
+        info['minimum_full_load_efficiency'] = 9.3
+        info['minimum_iplv'] = nil
+        info['minimum_integrated_energy_efficiency_ratio'] = nil
+      end
+    end
+    @standards_data['heat_pumps_heating'].each do |info|
+      if info['cooling_type'] == 'AirCooled' &&
+         info['subcategory'] == 'Single Package' &&
+         almost_equal?(65000.0, info['minimum_capacity']) &&
+         almost_equal?(134999.0, info['maximum_capacity'])
+        info['minimum_heating_seasonal_performance_factor'] = nil
+        info['minimum_coefficient_of_performance_heating'] = 3.3
+        info['minimum_energy_efficiency_ratio'] = nil
+      end
+    end
+    @standards_data['heat_pumps_heating'].each do |info|
+      if info['cooling_type'] == 'AirCooled' &&
+         info['subcategory'] == 'Single Package' &&
+         almost_equal?(135000.0, info['minimum_capacity']) &&
+         almost_equal?(9999999.0, info['maximum_capacity'])
+        info['minimum_heating_seasonal_performance_factor'] = nil
+        info['minimum_coefficient_of_performance_heating'] = 3.2
+        info['minimum_energy_efficiency_ratio'] = nil
       end
     end
 
