7077051e5f6c2596f7459e456968f4e52211ff6e
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2024-08-01 09:28:49 -0400

    update to output_folder definition

--- test/necb/unit_tests/tests/test_necb_coolingtower_rules.rb
+++ test/necb/unit_tests/tests/test_necb_coolingtower_rules.rb
@@ -38,13 +38,13 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
     # Test cases. Three cases for NG and FuelOil, one for Electric.
     # Results and name are tbd here as they will be calculated in the test.
     test_cases_hash = {:Vintage => ["NECB2011"], 
-                       :chiller_types => ['Scroll', 'Centrifugal', 'Rotary Screw', 'Reciprocating']
+                       :chiller_types => ['Scroll', 'Centrifugal', 'Rotary Screw', 'Reciprocating'],
                        :TestCase => ["Single"], 
                        :TestPars => {:tested_capacity_kW => 1500}}
     new_test_cases = make_test_cases_json(test_cases_hash)
     merge_test_cases!(test_cases, new_test_cases)
     test_cases_hash = {:Vintage => ["NECB2011"], 
-                       :chiller_types => ['Scroll', 'Centrifugal', 'Rotary Screw', 'Reciprocating']
+                       :chiller_types => ['Scroll', 'Centrifugal', 'Rotary Screw', 'Reciprocating'],
                        :TestCase => ["Twin"], 
                        :TestPars => {:tested_capacity_kW => 2500}}
     new_test_cases = make_test_cases_json(test_cases_hash)
@@ -54,6 +54,7 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
     test_results = do_test_cases(test_cases: test_cases, test_pars: test_parameters)
 
     # Write test results.
+    file_root = "#{self.class.name}-#{__method__}".downcase
     test_result_file = File.join(@test_results_folder, "#{file_root}-test_results.json")
     File.write(test_result_file, JSON.pretty_generate(test_results))
 
@@ -93,10 +94,10 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
     total_boiler_cap = test_case[:tested_capacity_kW]
 
     # Set up remaining parameters for test.
-    output_folder = method_output_folder(__method__)
     template = "NECB2011"
     standard = get_standard(template)
     save_intermediate_models = false
+    
 
     first_cutoff_twr_cap = 1750000.0
     tol = 1.0e-3
@@ -112,13 +113,14 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
     chiller_types.each do |chiller_type|
       test_chiller_cap.each do |chiller_cap|
         name = "sys6_#{template}_ChillerType_#{chiller_type}-#{chiller_cap}watts"
-        name.gsub!(/\s+/, "-")
-        puts "***************#{name}***************\n"
+        name_short = "#{chiller_type}_sys6"
+        output_folder = method_output_folder("#{test_name}/#{name_short}")
+        logger.info "Started individual test: #{name}"
 
         # Load model and set climate file.
         model = BTAP::FileIO.load_osm(File.join(@resources_folder,"5ZoneNoHVAC.osm"))
         BTAP::Environment::WeatherFile.new('CAN_ON_Toronto.Pearson.Intl.AP.716240_CWEC2016.epw').set_weather_file(model)
-        BTAP::FileIO.save_osm(model, "#{output_folder}/#{name}-baseline.osm") if save_intermediate_models
+        BTAP::FileIO.save_osm(model, "#{output_folder}/baseline.osm") if save_intermediate_models
 
         hw_loop = OpenStudio::Model::PlantLoop.new(model)
         always_on = model.alwaysOnDiscreteSchedule
@@ -131,11 +133,11 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
                                                                             fan_type: fan_type,
                                                                             hw_loop: hw_loop)
         # Save the model after btap hvac.
-        BTAP::FileIO.save_osm(model, "#{output_folder}/#{name}.hvacrb")
+        BTAP::FileIO.save_osm(model, "#{output_folder}/hvacrb")
         model.getChillerElectricEIRs.each { |ichiller| ichiller.setReferenceCapacity(chiller_cap) }
 
         # Run the measure.
-        run_sizing(model: model, template: template, test_name: name, save_model_versions: save_intermediate_models) if PERFORM_STANDARDS
+        run_sizing(model: model, template: template, save_model_versions: save_intermediate_models, output_dir: output_folder) if PERFORM_STANDARDS
 
         necb2011_refCOP = 5.0
         model.getChillerElectricEIRs.each do |ichiller|
@@ -187,11 +189,13 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
   # power = 0.013 x capacity in kW.
   def test_coolingtower_power
 
+    # General inputs.
+    test_name = test_pars[:test_method]
+    save_intermediate_models = test_pars[:save_intermediate_models]
+
     # Set up remaining parameters for test.
-    output_folder = method_output_folder(__method__)
     template = 'NECB2015'
     standard = get_standard(template)
-    save_intermediate_models = false
 
     # Generate the osm files for all relevant cases to generate the test data for system 6.
     boiler_fueltype = 'Electricity'
@@ -204,13 +208,14 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
 
     chiller_types.each do |chiller_type|
       name = "sys6_#{template}_ChillerType_#{chiller_type}-#{chiller_cap}watts"
-      name.gsub!(/\s+/, "-")
-      puts "***************#{name}***************\n"
+      name_short = "#{chiller_type}_sys6"
+      output_folder = method_output_folder("#{test_name}/#{name_short}")
+      logger.info "Started individual test: #{name}"
 
       # Load model and set climate file.
       model = BTAP::FileIO.load_osm(File.join(@resources_folder,"5ZoneNoHVAC.osm"))
       BTAP::Environment::WeatherFile.new('CAN_ON_Toronto.Pearson.Intl.AP.716240_CWEC2016.epw').set_weather_file(model)
-      BTAP::FileIO.save_osm(model, "#{output_folder}/#{name}-baseline.osm") if save_intermediate_models
+      BTAP::FileIO.save_osm(model, "#{output_folder}/baseline.osm") if save_intermediate_models
 
       hw_loop = OpenStudio::Model::PlantLoop.new(model)
       always_on = model.alwaysOnDiscreteSchedule
@@ -225,7 +230,7 @@ class NECB_HVAC_Cooling_Tower_Tests < Minitest::Test
       model.getChillerElectricEIRs.each { |ichiller| ichiller.setReferenceCapacity(chiller_cap) }
 
       # Run sizing.
-      run_sizing(model: model, template: template, test_name: name, save_model_versions: save_intermediate_models) if PERFORM_STANDARDS
+      run_sizing(model: model, template: template, save_model_versions: save_intermediate_models, output_dir: output_folder) if PERFORM_STANDARDS
 
       refCOP = 5.0
       model.getChillerElectricEIRs.each do |ichiller|
