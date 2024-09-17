48812df5a142bf2496e8fc945da3293177ec1072
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2024-09-17 13:34:44 -0600

    add office detailed space type ratios
    - add small, medium, and large office detailed space type ratios from example models
    - switch large office default to be an else statement in case the building_subtype is not specified

--- test/modules/create_typical/test_space_type_ratios.rb
+++ test/modules/create_typical/test_space_type_ratios.rb
@@ -5,23 +5,76 @@ class TestCreateTypicalSpaceTypeRatios < Minitest::Test
     @create = OpenstudioStandards::CreateTypical
   end
 
-  def test_get_space_types_from_building_type
+  def test_get_space_types_from_building_type_primary_school
     building_type = 'PrimarySchool'
     result = @create.get_space_types_from_building_type(building_type, template: '90.1-2013')
-    assert(result)
+    assert_equal(11, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios)
+  end
 
+  def test_get_space_types_from_building_type_small_office
+    building_type = 'SmallOffice'
+    result = @create.get_space_types_from_building_type(building_type, template: '90.1-2013', whole_building: true)
+    assert_equal(1, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
+
+    result = @create.get_space_types_from_building_type(building_type, whole_building: false)
+    assert_equal(9, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
+  end
+
+  def test_get_space_types_from_building_type_medium_office
     building_type = 'MediumOffice'
     result = @create.get_space_types_from_building_type(building_type, template: '90.1-2013', whole_building: true)
-    assert(result)
+    assert_equal(1, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
 
+    result = @create.get_space_types_from_building_type(building_type, whole_building: false)
+    assert_equal(11, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
+  end
+
+  def test_get_space_types_from_building_type_large_office
     building_type = 'LargeOffice'
     result = @create.get_space_types_from_building_type(building_type, building_subtype: 'largeoffice_datacenter', template: '90.1-2013', whole_building: true)
-    assert(result)
+    assert_equal(3, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
 
+    result = @create.get_space_types_from_building_type(building_type, building_subtype: nil, template: '90.1-2013', whole_building: false)
+    assert_equal(14, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
+
+    result = @create.get_space_types_from_building_type(building_type, building_subtype: nil, template: 'DOE Ref Pre-1980', whole_building: false)
+    assert_equal(12, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
+  end
+
+  def test_get_space_types_from_building_type_warehouse
     building_type = 'Warehouse'
     result = @create.get_space_types_from_building_type(building_type, building_subtype: 'warehouse_bulk20', template: '90.1-2013', whole_building: true)
-    assert(result)
+    assert_equal(3, result.length)
+    sum_of_ratios = 0
+    result.each { |key, value| sum_of_ratios += value[:ratio] }
+    assert_equal(1.0, sum_of_ratios.round(6))
+  end
 
+  def test_get_space_types_from_building_type_fail
     building_type = 'Casa Bonita'
     result = @create.get_space_types_from_building_type(building_type, template: '90.1-2013')
     assert(result == false)
