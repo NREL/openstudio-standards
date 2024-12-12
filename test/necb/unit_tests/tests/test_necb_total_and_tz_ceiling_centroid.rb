f7e426f138e354e5ec1213ec8fee118d4208dad1
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2024-12-12 10:41:14 -0500

    updated test to new format. note unexpected diffs between vintages

--- test/necb/unit_tests/tests/test_necb_total_and_tz_ceiling_centroid.rb
+++ test/necb/unit_tests/tests/test_necb_total_and_tz_ceiling_centroid.rb
@@ -1,5 +1,4 @@
 require_relative '../../../helpers/minitest_helper'
-#require_relative '../../../helpers/create_doe_prototype_helper'
 require_relative '../../../helpers/necb_helper'
 include(NecbHelper)
 
@@ -12,51 +11,120 @@ class NECB_Ceiling_Centroid_Test < Minitest::Test
     define_folders(__dir__)
     define_std_ranges
   end
+  
+  #  Makes use of the template design pattern with the work done by the do_* method below (i.e. 'do_' prepended to the current method name)
+  def test_ceiling_centroid
+    logger.info "Starting suite of tests for: #{__method__}"
+    
+    # Define test parameters that apply to all tests.
+    test_parameters = {test_method: __method__,
+                       save_intermediate_models: true}
 
-  # Additional constant ranges for tests.
-  Epw_files = ['CAN_AB_Calgary.Intl.AP.718770_CWEC2020.epw']
-  Templates = ['NECB2011', 'NECB2015', 'NECB2017']
+    # Define test cases. 
+    test_cases = Hash.new
 
-  # @return [Boolean] true if successful.
-  def test_ceiling_centroid()
+    # Define references (per vintage in this case).
+    #test_cases[:NECB2011] = {:Reference => "tbd"}
+    
+    # Test cases. Three cases for NG and FuelOil, one for Electric.
+    # Results and name are tbd here as they will be calculated in the test.
+    test_cases_hash = {:Vintage => @AllTemplates, 
+                       :EpwFile => ['CAN_AB_Calgary.Intl.AP.718770_CWEC2020.epw'],
+                       :TestCase => ["case-1"], 
+                       :TestPars => {:value => :tbd}}
+    new_test_cases = make_test_cases_json(test_cases_hash)
+    merge_test_cases!(test_cases, new_test_cases)
+      
+    # Create empty results hash and call the template method that runs the individual test cases.
+    test_results = do_test_cases(test_cases: test_cases, test_pars: test_parameters)
 
-    # Set up remaining parameters for test.
-    output_folder = method_output_folder(__method__)
+    # Write test results.
+    file_root = "#{self.class.name}-#{__method__}".downcase
+    test_result_file = File.join(@test_results_folder, "#{file_root}-test_results.json")
+    File.write(test_result_file, JSON.pretty_generate(test_results))
 
-    output_array = []
-    climate_zone = 'none'
-    #Iterate through code versions. It shouldn't make a different but do it anyway.
-    Templates.sort.each do |template|
-      Epw_files.sort.each do |epw_file|
-        model = nil
-        standard = nil
-        # Open the Outpatient model.
-        model = BTAP::FileIO.load_osm(File.join(@resources_folder,"Ceilingtest.osm"))
-        # Set the weather file.
-        weather_file_path = OpenstudioStandards::Weather.get_standards_weather_file_path(epw_file)
-        OpenstudioStandards::Weather.model_set_building_location(model, weather_file_path: weather_file_path)
-        # Get access to the standards class
-        standard = get_standard(template)
-        # Find the centroid of the highest outside ceiling and add it to the output array
-        output_array << standard.find_highest_roof_centre(model)
-        # Go through the thermal zones and find all the conditioned, non-plenum, spaces in the thermal zore.  Sort by
-        # story and find the overall centroid of all the ceilings in the thermal zone on that floor.  Add the result
-        # to the output array.
-        model.getThermalZones.sort.each do |tz|
-          output_array << tz.nameString
-          output_array << standard.thermal_zone_get_centroid_per_floor(tz) unless standard.thermal_zone_get_centroid_per_floor(tz).nil?
-        end
-      end #loop to the next epw_file
-    end #loop to the next Template
-    #Write test report file.
-    test_result_file = File.join(@test_results_folder,'ceiling_test_results.json')
-    File.open(test_result_file, 'w') {|f| f.write(JSON.pretty_generate(output_array)) }
-
-    #Test that the values are correct by doing a file compare.
-    expected_result_file = File.join(@expected_results_folder,'ceiling_test_expected_results.json')
+    # Read expected results. 
+    file_name = File.join(@expected_results_folder, "#{file_root}-expected_results.json")
+    expected_results = JSON.parse(File.read(file_name), {symbolize_names: true})
 
     # Check if test results match expected.
     msg = "Ceiling centroid test results do not match what is expected in test"
-    file_compare(expected_results_file: expected_result_file, test_results_file: test_result_file, msg: msg)
+    compare_results(expected_results: expected_results, test_results: test_results, msg: msg, type: 'json_data')
+    logger.info "Finished suite of tests for: #{__method__}"
+  end
+
+  # @param test_pars [Hash] has the static parameters.
+  # @param test_case [Hash] has the specific test parameters.
+  # @return results of this case.
+  # @note Companion method to test_boiler_efficiency that runs a specific test. Called by do_test_cases in necb_helper.rb.
+  def do_test_ceiling_centroid(test_pars:, test_case:)
+
+    # Debug.
+    logger.debug "test_pars: #{JSON.pretty_generate(test_pars)}"
+    logger.debug "test_case: #{JSON.pretty_generate(test_case)}"
+
+    # Define local variables. These are extracted from the supplied hashes.
+    # General inputs.
+    test_name = test_pars[:test_method]
+    save_intermediate_models = test_pars[:save_intermediate_models]
+    vintage = test_pars[:Vintage]
+    epw_file = test_pars[:EpwFile]
+
+    # Define the test name. 
+    name = "#{vintage}_ceiling_centroid"
+    name_short = "#{vintage}_ceiling_centroid"
+    output_folder = method_output_folder("#{test_name}/#{name_short}")
+    logger.info "Starting individual test: #{name}"
+    results = Hash.new
+
+    # Wrap test in begin/rescue/ensure.
+    begin
+
+      # Load model and set climate file.
+      model = BTAP::FileIO.load_osm(File.join(@resources_folder,"Ceilingtest.osm"))
+      weather_file_path = OpenstudioStandards::Weather.get_standards_weather_file_path(epw_file)
+      OpenstudioStandards::Weather.model_set_building_location(model, weather_file_path: weather_file_path)
+      BTAP::FileIO.save_osm(model, "#{output_folder}/baseline.osm") if save_intermediate_models
+
+      # Get access to the standards class
+      standard = get_standard(vintage)
+
+    rescue => error
+      msg = "#{__FILE__}::#{__method__} #{error.message}"
+      logger.error(msg)
+      return {ERROR: msg}
+    end
+      
+    # Add this test case to results and return the hash.
+    # Find the centroid of the highest outside ceiling
+    roof_cent = standard.find_highest_roof_centre(model)
+    results = {
+      roof_centroid_x_m: roof_cent[:roof_centroid][0].signif(3),
+      roof_centroid_y_m: roof_cent[:roof_centroid][1].signif(3),
+      roof_height_m: roof_cent[:roof_centroid][2].signif(3),
+      roof_area_m2: roof_cent[:roof_area].signif(3)
+    }
+
+    # Go through the thermal zones and find all the conditioned, non-plenum, spaces in the thermal zone.  Sort by
+    # story and find the overall centroid of all the ceilings in the thermal zone on that floor.  Add the result
+    # to the output array.
+    tz_results = []
+    model.getThermalZones.sort.each do |tz|
+      tz_data = standard.thermal_zone_get_centroid_per_floor(tz)
+      next if tz_data[0] == nil
+      tz_results << {
+        name: tz.nameString,
+        story: tz_data[0][:story_name],
+        centroid_x_m: tz_data[0][:centroid][0].signif(3),
+        centroid_y_m: tz_data[0][:centroid][1].signif(3),
+        centroid_z_m: tz_data[0][:centroid][2].signif(3),
+        ceiling_area_m2: tz_data[0][:ceiling_area].signif(3)
+      }
+    end
+    #tz_results.sort_by! { |entry| entry[:thermal_zone] }
+    results[:thermal_zones] = tz_results
+
+    logger.info "Completed individual test: #{name}"
+    return results
   end
 end
