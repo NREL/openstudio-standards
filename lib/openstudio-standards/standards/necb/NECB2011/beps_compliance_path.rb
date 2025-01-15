b82ac41f9ff3f5f3e0d13708e26d4e889a5ffd1d
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2025-01-15 11:04:01 -0500

    moving additional properties code block so they are set for all cases

--- lib/openstudio-standards/standards/necb/NECB2011/beps_compliance_path.rb
+++ lib/openstudio-standards/standards/necb/NECB2011/beps_compliance_path.rb
@@ -301,23 +301,23 @@ class NECB2011
         # ventilation standard is followed.  The mod_ventilation_per_person rate adjusts the per person ventilation rate (from the standard) so that the
         # correct ventilation rate (per the standard) is calculated when using the NECB occupant density (defined as occupancy_per_area in the space_types.json file)
         # Later the method thermal_zone_convert_outdoor_air_to_per_area (in standards) uses the space occupancy which is the NECB figure to get the total vent rate.
-        # Add information about the source to an additional properties object. Used in unit testing but could be useful elsewhere.
         mod_ventilation_per_person = ventilation_per_person * ventilation_occupancy_per_area / occupancy_per_area
         ventilation.setOutdoorAirFlowperPerson(OpenStudio.convert(mod_ventilation_per_person.to_f, 'ft^3/min*person', 'm^3/s*person').get)
-        notes = ventilation.additionalProperties
-        notes.setFeature("Ref OA per area", space_type_properties['ventilation_per_area'].to_f)
-        notes.setFeature("Ref OA per person", space_type_properties['ventilation_per_person'].to_f)
-        notes.setFeature("Ref OA ach", space_type_properties['ventilation_air_changes'].to_f)
-        notes.setFeature("Ref occupancy per 1000ft2", space_type_properties['ventilation_occupancy_rate_people_per_1000ft2'].to_f)
-        notes.setFeature("Ref standard", space_type_properties['ventilation_occupancy_standard'].to_s)
-        notes.setFeature("Ref space type", space_type_properties['ventilation_standard_space_type'].to_s)
         OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.SpaceType', "#{space_type.name} set ventilation per person to #{mod_ventilation_per_person} cfm/person.")
       end
       unless ventilation_ach.zero?
         ventilation.setOutdoorAirFlowAirChangesperHour(ventilation_ach)
         OpenStudio.logFree(OpenStudio::Info, 'openstudio.standards.SpaceType', "#{space_type.name} set ventilation to #{ventilation_ach} ACH.")
       end
-
+      
+      # Add information about the source to an additional properties object. Used in unit testing but could be useful elsewhere.
+      notes = ventilation.additionalProperties
+      notes.setFeature("Ref OA per area", space_type_properties['ventilation_per_area'].to_f)
+      notes.setFeature("Ref OA per person", space_type_properties['ventilation_per_person'].to_f)
+      notes.setFeature("Ref OA ach", space_type_properties['ventilation_air_changes'].to_f)
+      notes.setFeature("Ref occupancy per 1000ft2", space_type_properties['ventilation_occupancy_rate_people_per_1000ft2'].to_f)
+      notes.setFeature("Ref standard", space_type_properties['ventilation_occupancy_standard'].to_s)
+      notes.setFeature("Ref space type", space_type_properties['ventilation_standard_space_type'].to_s)
     elsif set_ventilation && !ventilation_have_info
 
       # All space types must have a design spec OA
