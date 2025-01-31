fbbf229734e17cfd405f66ba94a85cf5cf4b2f55
Macdonald, Iain <iain.macdonald@nrc-cnrc.gc.ca>
2025-01-31 10:55:33 -0500

    WIP updated expected results (old ones were wrong); left output in place; to do: tidy up and expand tests

--- test/necb/unit_tests/tests/test_ecm_erv.rb
+++ test/necb/unit_tests/tests/test_ecm_erv.rb
@@ -45,6 +45,7 @@ class ECM_ERV_Tests < Minitest::Test
         #'Warehouse'
     ]
     @epw_files = ['CAN_AB_Calgary.Intl.AP.718770_CWEC2020.epw']
+    #@epw_files = ['CAN_BC_Vancouver.Intl.AP.718920_CWEC2020.epw']
     @primary_heating_fuels = ['Electricity']
     @erv_packages = ["NECB_Default","NECB_Default_All","Plate-Existing", 'Plate-All','Rotary-All']
 
@@ -58,6 +59,7 @@ class ECM_ERV_Tests < Minitest::Test
           @primary_heating_fuels.sort.each do |primary_heating_fuel|
             @erv_packages.sort.each do |erv_package|
               standard = Standard.build(template)
+              puts "************* #{template}; #{erv_package}; #{epw_file}; #{building_type}; #{primary_heating_fuel}"
               model  = standard.model_create_prototype_model(template:template,
                                                building_type: building_type,
                                                epw_file: epw_file,
@@ -73,6 +75,14 @@ class ECM_ERV_Tests < Minitest::Test
 
               ervs = model.getHeatExchangerAirToAirSensibleAndLatents
               result['number_of_ervs'] = ervs.length
+              ervs.each { |erv| puts "##### #{erv}"}
+              model.getAirLoopHVACs.each do |air_loop_hvac| 
+                puts "####### air loop kW #{standard.calculate_exhaust_heat(air_loop_hvac)}"
+                puts "####### has ERV #{standard.air_loop_hvac_energy_recovery_ventilator_required?(air_loop_hvac, 'NECB')}"
+                erv_comp = air_loop_hvac.components('OS:HeatExchanger:AirToAir:SensibleAndLatent'.to_IddObjectType).first
+                puts "### ERV: #{erv_comp}"
+                #puts "####### components #{air_loop_hvac.components('OS:HeatExchanger:AirToAir:SensibleAndLatent'.to_IddObjectType).first.name}"
+              end
               if ervs.length >0
                 heat_exchanger_air_to_air_sensible_and_latent = ervs[0]
                 result['heatExchangerType'] = heat_exchanger_air_to_air_sensible_and_latent.heatExchangerType()
@@ -90,6 +100,7 @@ class ECM_ERV_Tests < Minitest::Test
                 result['thresholdTemperature'] = heat_exchanger_air_to_air_sensible_and_latent.thresholdTemperature()
                 result['initialDefrostTimeFraction'] = heat_exchanger_air_to_air_sensible_and_latent.initialDefrostTimeFraction().get
               end
+              puts ">> #{result}"
               @test_results_array << result
 
             end #@erv_package.sort.each do |dcv_type|
