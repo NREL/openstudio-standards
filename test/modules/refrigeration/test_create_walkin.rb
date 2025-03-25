c41f9ac195b4201993782b9a3bd63cd23e23279d
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2025-03-25 16:11:38 -0600

    Update test_create_walkin.rb

--- test/modules/refrigeration/test_create_walkin.rb
+++ test/modules/refrigeration/test_create_walkin.rb
@@ -1,6 +1,6 @@
 require_relative '../../helpers/minitest_helper'
 
-class TestRefrigerationWalkIn < Minitest::Test
+class TestRefrigerationCreateWalkIn < Minitest::Test
   def setup
     @refrig = OpenstudioStandards::Refrigeration
   end
@@ -9,20 +9,20 @@ class TestRefrigerationWalkIn < Minitest::Test
     model = OpenStudio::Model::Model.new
     zone = OpenStudio::Model::ThermalZone.new(model)
 
-    # default case
-    case1 = @refrig.create_walkin(model)
+    # default walkin
+    walkin1 = @refrig.create_walkin(model)
 
-    # old case
-    case2 = @refrig.create_walkin(model,
-                                  template: 'old',
-                                  operation_type: 'MT',
-                                  thermal_zone: zone)
+    # old walkin
+    walkin2 = @refrig.create_walkin(model,
+                                    template: 'old',
+                                    operation_type: 'MT',
+                                    thermal_zone: zone)
 
-    # advanced case
-    case3 = @refrig.create_walkin(model,
-                                  template: 'advanced',
-                                  operation_type: 'LT',
-                                  walkin_type: 'Walk-in Freezer - 240SF',
-                                  thermal_zone: zone)
+    # advanced walkin
+    walkin3 = @refrig.create_walkin(model,
+                                    template: 'advanced',
+                                    operation_type: 'LT',
+                                    walkin_type: 'Walk-in Freezer - 240SF',
+                                    thermal_zone: zone)
   end
 end
