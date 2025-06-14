271191d3b9ae4ba45016339f9214bca94a112048
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2025-06-14 17:33:29 -0600

    revert changes to 90.1-2016 ervs

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2016/ashrae_90_1_2016.AirLoopHVAC.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2016/ashrae_90_1_2016.AirLoopHVAC.rb
@@ -417,13 +417,6 @@ class ASHRAE9012016 < ASHRAE901
     # Moisture regime is not needed for climate zone 8
     climate_zone = climate_zone.split('-')[-1]
     climate_zone = '8' if climate_zone.include?('8')
-    # Default to use values for cooling for climate zones 0, 1, 2, and 3 and heating for all others.
-    case climate_zone
-    when '0A', '0B', '1A', '1A', '1B', '2A', '2B', '3A', '3B', '3C'
-      design_conditions = 'Cooling'
-    else
-      design_conditions = 'Heating'
-    end
 
     # Check annual operating hours
     if ann_op_hrs < 8000.0
@@ -438,8 +431,7 @@ class ASHRAE9012016 < ASHRAE901
     search_criteria = {
       'template' => template,
       'climate_zone' => climate_zone,
-      'under_8000_hours' => under_8000_hours,
-      'design_conditions' => design_conditions
+      'under_8000_hours' => under_8000_hours
     }
     energy_recovery_limits = model_find_object(standards_data['energy_recovery'], search_criteria)
     if energy_recovery_limits.nil?
