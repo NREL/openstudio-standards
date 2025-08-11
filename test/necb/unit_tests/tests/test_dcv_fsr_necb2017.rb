aa5fdc5305dc43991ed808ef635ec42bbbd4e94b
plopez <phylroy.lopez@nrcan-rncan.gc.ca>
2025-08-11 20:10:18 +0000

    Add speed test improvements and parallel test runner

---
+++ test/necb/unit_tests/tests/test_dcv_fsr_necb2017.rb
@@ -0,0 +1,13 @@
+require_relative 'helpers/dcv_test_helper'
+
+class NECB_DCV_FSR_2017_Tests < Minitest::Test
+  include DCVTestHelper
+
+  def setup
+    setup_dcv_test
+  end
+
+  def test_dcv_fsr_necb2017
+    run_dcv_test('NECB2017', 'FullServiceRestaurant')
+  end
+end
