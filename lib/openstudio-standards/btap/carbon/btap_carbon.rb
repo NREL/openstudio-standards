19d8c74492779a7785831fbb5516c6da7cf4f19a
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-08-22 17:33:09 +0000

    Fixed and refactored attributes.rb and btap_carbon.rb, carbon analysis now works.

--- lib/openstudio-standards/btap/carbon/btap_carbon.rb
+++ lib/openstudio-standards/btap/carbon/btap_carbon.rb
@@ -1,10 +1,12 @@
 class BTAPCarbon
   def initialize(attributes)
-    @db = []
-    @cp = CommonPaths.instance
-    @attributes = attributes
-    @carbon_report = {}
+    @carbon_database  = []
+    @costing_database = CostingDatabase.instance
+    @cp               = CommonPaths.instance
+    @attributes       = attributes
+    @carbon_report    = {}
 
+    # Build the carbon database.
     data = CSV.read(@cp.carbon_data_path)
 
     1.upto data.length - 1 do |i|
@@ -13,11 +15,11 @@ class BTAPCarbon
       item  = Hash.new
 
       item["materials_opaque_id"] = index.next
-      item["OC23 - Products"]     = index.next
-      item["Quantity"]            = index.next.to_f
-      item["Unit"]                = index.next
+      item["OC23"]                = index.next
+      item["quantity"]            = index.next.to_f
+      item["unit"]                = index.next
 
-      @db << item
+      @carbon_database << item
     end
   end
 
@@ -37,9 +39,17 @@ class BTAPCarbon
           @carbon_report["#{surface_type.underscore}_area_m2"] = \
             (@carbon_report["#{surface_type.underscore}_area_m2"] + surfArea).round(2)
 
+          # Get the carbon emissions for each material in the space.
+          if surface.construction_hash.nil?
+            # TODO: undefined space type
+            emissions = 0.0
+          else
+            emissions = carbon_from_construction(surface.construction_hash)
+          end
+
           # Calculate the carbon emissions
           @carbon_report["#{surface_type.underscore}_carbon"] = \
-            (@carbon_report["#{surface_type.underscore}_carbon"] + 1).round(2)
+            (@carbon_report["#{surface_type.underscore}_carbon"] + emissions * surfArea).round(2)
           
           total_emissions += @carbon_report["#{surface_type.underscore}_carbon"]
         end
@@ -49,4 +59,37 @@ class BTAPCarbon
     @carbon_report["total"] = total_emissions
     return @carbon_report
   end
+end
+
+# Retrieve the carbon emissions given a construction.
+def carbon_from_construction(construction)
+  total_emissions = 0.0
+
+  construction["material_opaque_id_layers"].split(',').each do |material_id|
+
+    # Locate the material entry in the costing database
+    material_costing = @costing_database["raw"]["materials_opaque"].find do |row| 
+      row["materials_opaque_id"] == material_id
+    end
+
+    if material_costing.nil?
+      raise("Error: Could not find material with ID #{material_id} in the costing database.")
+    end
+
+    # Locate the material entry in the carbon database
+    material_carbon = @carbon_database.find do |row| 
+      row["materials_opaque_id"] == material_id
+    end
+
+    if material_carbon.nil?
+      raise("Error: Could not find material with ID #{material_id} in the carbon database.")
+    end
+
+    emissions = 0.0 #TODO: this will be added once the API is made available
+
+    # Convert the units according to the carbon database and multiply by the expected emissions.
+    total_emissions += material_carbon["quantity"] / material_costing["quantity"].to_f # * emissions
+  end
+
+  return total_emissions
 end
