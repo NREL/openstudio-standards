3d246472257467c388f2ae413e10caa320b2eada
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-10-15 14:58:55 +0000

    added BtapResults test helper class

---
+++ test/btap_costing/tests/BtapResults_test_helper.rb
@@ -0,0 +1,28 @@
+require 'fileutils'
+
+# BTAPResultsHelper
+#   Helper methods for the BtapResults suite of tests.
+class BTAPResultsHelper
+  attr_reader :model_cached_path
+  attr_reader :sql_cached_path
+
+  def initialize(test_path)
+    cached_folder      = "#{__dir__}/cache/#{File.basename(test_path, '.rb')}"
+    @model_cached_path = cached_folder + "/output.osm"
+    @sql_cached_path   = cached_folder + "/eplusout.sql"
+  end
+
+  def cache_osm_and_sql(model_path:, sql_path:)
+    FileUtils.cp(model_path, model_cached_path)
+    FileUtils.cp(sql_path, sql_cached_path)
+  end
+
+  def get_analysis(output_folder:, template:)
+    return BTAPNoSimAnalysis.new(
+      model_path:    @model_cached_path, 
+      sql_file_path: @sql_cached_path,
+      output_folder: output_folder,
+      template:      template,
+      datapoint_id:  'test_run')
+  end
+end
