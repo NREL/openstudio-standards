544acd20e77dab2167eb4fd08630901ab0f9c428
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-10-21 14:23:46 +0000

    fixed costing test_run_all_test_locally.rb to run regardless of current directory

--- lib/openstudio-standards/btap/costing/test_run_all_test_locally.rb
+++ lib/openstudio-standards/btap/costing/test_run_all_test_locally.rb
@@ -5,25 +5,25 @@ require 'minitest/autorun'
 require 'json'
 require_relative './parallel_tests'
 require_relative './btap_results/tests/BtapResults_test_helper' # Required for the cached switch
-TestListFile = File.join(File.dirname(__FILE__), 'test_list.txt')
 
 # NOTE: Runs are cached automatically. To run this test with an annual
 # simulation and update the cache, pass the RERUN_CACHED=true environment
 # variable pair to the test file, for example:
 # RERUN_CACHED=true bundle exec ruby [test_file]
-# TODO: This only runs given the correct directory, fix this.
 class RunAllTests < Minitest::Test
   def test_all()
     full_file_list = nil
-    if File.exist?(TestListFile)
-      puts TestListFile
+    test_list_file = File.expand_path(File.join(__dir__, 'test_list.txt'))
+    root_dir       = File.expand_path("../../../../", __dir__)
+    if File.exist?(test_list_file)
+      puts test_list_file
       # load test files from file.
-      full_file_list = File.readlines(TestListFile).shuffle
+      full_file_list = File.readlines(test_list_file).shuffle
+      full_file_list.map! {|file| "#{root_dir}/#{file.strip}"}
       # Select only .rb files that exist
-      full_file_list.select! {|item| item.include?('rb') && File.exist?(File.absolute_path("#{item.strip}"))}
-      full_file_list.map! {|item| File.absolute_path("#{item.strip}")}
+      full_file_list.select! {|file| file.include?('rb') && File.exist?(file)}
     else
-      puts "Could not find list of files to test at #{TestListFile}"
+      puts "Could not find list of files to test at #{test_list_file}"
       return false
     end
     assert(ParallelTests.new.run(full_file_list), "Some tests failed please ensure all test pass and tests have been updated to reflect the changes you expect before issuing a pull request")
